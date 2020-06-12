d1340e538f167734bb74eb2c7378954a4f6d02fc
Fernando Cejas <fcejas@gmail.com>
2020-06-12 14:34:36 +0200

    Refactor infrastructure script with default values.

--- buildSrc/src/main/kotlin/scripts/infrastructure.gradle.kts
+++ buildSrc/src/main/kotlin/scripts/infrastructure.gradle.kts
@@ -1,6 +1,12 @@
 package scripts
 
+import scripts.Variants_gradle.*
 import java.util.*
+
+private object Default {
+    val BUILD_TYPE = BuildTypes.DEBUG.capitalize()
+    val BUILD_FLAVOR = ProductFlavors.DEV.capitalize()
+}
 
 tasks.register("clean", Delete::class){
     delete(rootProject.buildDir)
@@ -13,23 +19,26 @@ tasks.named<Wrapper>("wrapper") {
 
 tasks.register("runUnitTests") {
     description = "Runs all Unit Tests."
-    dependsOn(":app:testDebugUnitTest")
+    dependsOn(":app:test${Default.BUILD_FLAVOR}${Default.BUILD_TYPE}UnitTest")
 }
 
 tasks.register("runAcceptanceTests") {
     description = "Runs all Acceptance Tests in the connected device."
-    dependsOn(":app:connectedAndroidTest")
+    dependsOn(":app:connected${Default.BUILD_FLAVOR}${Default.BUILD_TYPE}AndroidTest")
 }
 
 tasks.register("compileApp") {
-    description = "Compiles the Debug Version of the Wire Android Client."
-    dependsOn(":app:assembleDebug")
+    description = "Compiles the Wire Android Client."
+    dependsOn(":app:assemble${Default.BUILD_FLAVOR}${Default.BUILD_TYPE}")
 }
 
 tasks.register("runApp", Exec::class) {
-    description = "Compiles and runs the Debug Version of the Wire Android Client in the connected device."
-    dependsOn("compileApp", ":app:installDebug")
-    tasks.findByName(":app:installDebug")?.mustRunAfter("compileApp")
+    val compileAppTask = "compileApp"
+    val installAppTask = ":app:install${Default.BUILD_FLAVOR}${Default.BUILD_TYPE}"
+
+    description = "Compiles and runs the Wire Android Client in the connected device."
+    dependsOn(compileAppTask, installAppTask)
+    tasks.findByName(installAppTask)?.mustRunAfter(compileAppTask)
 
     val localProperties = File(project.rootDir, "local.properties")
     if (localProperties.exists()) {
@@ -37,7 +46,10 @@ tasks.register("runApp", Exec::class) {
         localProperties.inputStream().use { properties.load(it) }
         val sdkDir = properties["sdk.dir"]
         val adb = "${sdkDir}/platform-tools/adb"
+        
+        val applicationPackage = "com.wire.android.${Default.BUILD_FLAVOR.toLowerCase()}.${Default.BUILD_TYPE.toLowerCase()}"
+        val launchActivity = "com.wire.android.MainActivity"
 
-        commandLine(adb, "shell", "am", "start", "-n", "com.wire.android/com.wire.android.MainActivity")
+        commandLine(adb, "shell", "am", "start", "-n", "${applicationPackage}/${launchActivity}")
     }
 }

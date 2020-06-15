132ac8a1354b98107bca27bc88d3ed65d22924a7
Gizem Burhanoglu <gizembrh@gmail.com>
2020-06-15 18:01:53 +0200

    Add CreateAccountFragment (#6)
    * Add CreateAccountFragment

--- app/src/main/kotlin/com/wire/android/core/extension/Fragment.kt
+++ app/src/main/kotlin/com/wire/android/core/extension/Fragment.kt
@@ -1,7 +1,14 @@
 package com.wire.android.core.extension
 
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentManager
 import androidx.fragment.app.FragmentTransaction
 
 inline fun FragmentManager.inTransaction(func: FragmentTransaction.() -> FragmentTransaction) =
     beginTransaction().func().commit()
+
+//TODO: Fragment shouldn't be aware of activity's view ids
+fun Fragment.replaceFragment(frameId: Int, fragment: Fragment, addToBackStack: Boolean = true) {
+    (activity as AppCompatActivity).replaceFragment(frameId, fragment, addToBackStack)
+}
--- app/src/main/kotlin/com/wire/android/feature/auth/registration/CreateAccountActivity.kt
+++ app/src/main/kotlin/com/wire/android/feature/auth/registration/CreateAccountActivity.kt
@@ -3,13 +3,14 @@ package com.wire.android.feature.auth.registration
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.wire.android.R
+import com.wire.android.core.extension.replaceFragment
 import kotlinx.android.synthetic.main.activity_create_account.*
 
 class CreateAccountActivity : AppCompatActivity(R.layout.activity_create_account) {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-//        replaceFragment(R.id.activityCreateAccountLayoutContainer, CreateAccountFragment.newInstance(), false)
+        replaceFragment(R.id.createAccountLayoutContainer, CreateAccountFragment.newInstance(), false)
         initBackButton()
     }
 

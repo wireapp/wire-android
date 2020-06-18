30270664221aa7b84d8e2754953ae1ab2a07d5c3
Fernando Cejas <fcejas@gmail.com>
2020-06-18 11:17:30 +0200

    Refactor: move gradle kotlin compile options to compilation script.

--- buildSrc/src/main/kotlin/core/android.gradle.kts
+++ buildSrc/src/main/kotlin/core/android.gradle.kts
@@ -2,4 +2,5 @@ package core
 
 plugins {
     id("com.android.application") apply false
+    id("kotlin-android") apply false
 }
--- buildSrc/src/main/kotlin/scripts/compilation.gradle.kts
+++ buildSrc/src/main/kotlin/scripts/compilation.gradle.kts
@@ -15,4 +15,8 @@ android {
         sourceCompatibility = JavaVersion.VERSION_1_8
         targetCompatibility = JavaVersion.VERSION_1_8
     }
+
+    kotlinOptions {
+        jvmTarget = JavaVersion.VERSION_1_8.toString()
+    }
 }

d879afd21a40733edb41b9f87b5f50636c9459a4
Gizem Burhanoglu <gizembrh@gmail.com>
2020-06-22 17:03:55 +0200

    Change assertion in unit test

--- app/src/test/kotlin/com/wire/android/feature/auth/activation/datasource/remote/ActivationRemoteDataSourceTest.kt
+++ app/src/test/kotlin/com/wire/android/feature/auth/activation/datasource/remote/ActivationRemoteDataSourceTest.kt
@@ -2,10 +2,10 @@ package com.wire.android.feature.auth.activation.datasource.remote
 
 import com.wire.android.UnitTest
 import com.wire.android.any
-import com.wire.android.framework.functional.assertLeft
 import com.wire.android.framework.functional.assertRight
 import com.wire.android.framework.network.connectedNetworkHandler
 import kotlinx.coroutines.runBlocking
+import org.assertj.core.api.Assertions.assertThat
 import org.junit.Before
 import org.junit.Test
 import org.mockito.Mock
@@ -29,26 +29,29 @@ class ActivationRemoteDataSourceTest : UnitTest() {
     }
 
     @Test
-    fun `Given sendEmailActivationCode() is called, when api response is successful, then return success`() = runBlocking {
-        `when`(response.body()).thenReturn(Unit)
-        `when`(response.isSuccessful).thenReturn(true)
-        `when`(activationApi.sendActivationCode(any())).thenReturn(response)
+    fun `Given sendEmailActivationCode() is called, when api response is successful, then return success`() =
+        runBlocking {
+            `when`(response.body()).thenReturn(Unit)
+            `when`(response.isSuccessful).thenReturn(true)
+            `when`(activationApi.sendActivationCode(any())).thenReturn(response)
 
-        val result = activationRemoteDataSource.sendEmailActivationCode(TEST_EMAIL)
+            val result = activationRemoteDataSource.sendEmailActivationCode(TEST_EMAIL)
 
-        verify(activationApi).sendActivationCode(any())
-        result.assertRight()
-    }
+            verify(activationApi).sendActivationCode(any())
+            result.assertRight()
+        }
 
     @Test
-    fun `Given sendEmailActivationCode() is called, when api response fails, then return a failure`() = runBlocking {
-        `when`(response.isSuccessful).thenReturn(false)
-        `when`(activationApi.sendActivationCode(any())).thenReturn(response)
+    fun `Given sendEmailActivationCode() is called, when api response fails, then return a failure`() {
+        runBlocking {
+            `when`(response.isSuccessful).thenReturn(false)
+            `when`(activationApi.sendActivationCode(any())).thenReturn(response)
 
-        val result = activationRemoteDataSource.sendEmailActivationCode(TEST_EMAIL)
+            val result = activationRemoteDataSource.sendEmailActivationCode(TEST_EMAIL)
 
-        verify(activationApi).sendActivationCode(any())
-        result.assertLeft { /* no need to check error mapping here */ }
+            verify(activationApi).sendActivationCode(any())
+            assertThat(result.isLeft).isTrue()
+        }
     }
 
     companion object {

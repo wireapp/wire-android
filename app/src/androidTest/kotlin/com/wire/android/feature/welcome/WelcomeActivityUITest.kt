1f738c9d9d9c9a75c0b91b1bdc04b69547cf195d
Mejdeddine Benzarti <benzarti.mejdeddine@gmail.com>
2020-06-24 18:03:20 +0200

    add ui tests

---
+++ app/src/androidTest/kotlin/com/wire/android/feature/welcome/WelcomeActivityUITest.kt
@@ -0,0 +1,25 @@
+package com.wire.android.feature.welcome
+
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
+import androidx.test.espresso.matcher.ViewMatchers.withId
+import com.wire.android.FunctionalTest
+import com.wire.android.R
+import org.junit.Test
+
+class WelcomeActivityUITest : FunctionalTest(WelcomeActivity::class.java) {
+
+    @Test
+    fun launch_uiElementsVisible() {
+        onView(withId(R.id.welcomeTitleTextView)).check(matches(isDisplayed()))
+        onView(withId(R.id.welcomeCreateAccountButton)).check(matches(isDisplayed()))
+        onView(withId(R.id.welcomeLoginButton)).check(matches(isDisplayed()))
+        onView(withId(R.id.welcomeEnterpriseLoginButton)).check(matches(isDisplayed()))
+    }
+
+    @Test
+    fun launch_uiElementsVisibleInLandscape() = rotateScreen {
+        launch_uiElementsVisible()
+    }
+}

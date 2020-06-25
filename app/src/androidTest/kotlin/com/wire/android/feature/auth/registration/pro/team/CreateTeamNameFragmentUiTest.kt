ac5a6cd82b712fbee9837b6f22618e4aa9400e62
BradleyWire <bradley.wilson@wire.com>
2020-06-25 19:10:20 +0200

    remove test class annotations

--- app/src/androidTest/kotlin/com/wire/android/feature/auth/registration/pro/team/CreateTeamNameFragmentUiTest.kt
+++ app/src/androidTest/kotlin/com/wire/android/feature/auth/registration/pro/team/CreateTeamNameFragmentUiTest.kt
@@ -18,8 +18,6 @@ import org.junit.Before
 import org.junit.Test
 import org.junit.runner.RunWith
 
-@RunWith(AndroidJUnit4::class)
-@LargeTest
 class CreateTeamNameFragmentUiTest : FunctionalTest() {
 
     @Before

bbc74c8347820769dac037f839d9d3a28ec6f09f
BradleyWire <bradley.wilson@wire.com>
2020-06-26 11:18:31 +0200

    Changed teamName test back to activity based functional test

--- app/src/androidTest/kotlin/com/wire/android/feature/auth/registration/pro/team/CreateTeamNameFragmentUiTest.kt
+++ app/src/androidTest/kotlin/com/wire/android/feature/auth/registration/pro/team/CreateTeamNameFragmentUiTest.kt
@@ -1,29 +1,23 @@
 package com.wire.android.feature.auth.registration.pro.team
 
-import androidx.fragment.app.Fragment
-import androidx.fragment.app.FragmentFactory
-import androidx.fragment.app.testing.launchFragmentInContainer
 import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.action.ViewActions.*
 import androidx.test.espresso.assertion.ViewAssertions.matches
 import androidx.test.espresso.matcher.ViewMatchers.*
-import com.wire.android.FunctionalTest
+import com.wire.android.FunctionalActivityTest
 import com.wire.android.R
 import com.wire.android.core.extension.EMPTY
+import com.wire.android.feature.auth.registration.CreateAccountActivity
 import org.hamcrest.CoreMatchers.allOf
 import org.hamcrest.CoreMatchers.not
 import org.junit.Before
 import org.junit.Test
 
-class CreateTeamNameFragmentUiTest : FunctionalTest() {
+class CreateTeamNameFragmentUiTest : FunctionalActivityTest(CreateAccountActivity::class.java) {
 
     @Before
     fun setup() {
-        launchFragmentInContainer<CreateProAccountTeamNameFragment>(factory = object : FragmentFactory() {
-            override fun instantiate(classLoader: ClassLoader, className: String): Fragment {
-                return CreateProAccountTeamNameFragment.newInstance()
-            }
-        }, themeResId = R.style.AppTheme)
+        onView(withId(R.id.createProAccountTitleTextView)).perform(click())
     }
 
     @Test

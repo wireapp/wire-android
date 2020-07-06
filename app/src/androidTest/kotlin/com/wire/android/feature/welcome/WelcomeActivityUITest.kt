efb1179dc287599488e640a90897cb4f581cc2c5
Mejdeddine Benzarti <benzarti.mejdeddine@gmail.com>
2020-07-06 13:09:57 +0200

    update ui tests

--- app/src/androidTest/kotlin/com/wire/android/feature/welcome/WelcomeActivityUITest.kt
+++ app/src/androidTest/kotlin/com/wire/android/feature/welcome/WelcomeActivityUITest.kt
@@ -4,6 +4,7 @@ import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.assertion.ViewAssertions.matches
 import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
 import androidx.test.espresso.matcher.ViewMatchers.withId
+import androidx.test.espresso.matcher.ViewMatchers.withText
 import com.wire.android.FunctionalTest
 import com.wire.android.R
 import org.junit.Test
@@ -12,10 +13,10 @@ class WelcomeActivityUITest : FunctionalTest(WelcomeActivity::class.java) {
 
     @Test
     fun launch_uiElementsVisible() {
-        onView(withId(R.id.welcomeTitleTextView)).check(matches(isDisplayed()))
-        onView(withId(R.id.welcomeCreateAccountButton)).check(matches(isDisplayed()))
-        onView(withId(R.id.welcomeLoginButton)).check(matches(isDisplayed()))
-        onView(withId(R.id.welcomeEnterpriseLoginButton)).check(matches(isDisplayed()))
+        onView(withText(R.string.welcome_text)).check(matches(isDisplayed()))
+        onView(withText(R.string.welcome_create_account)).check(matches(isDisplayed()))
+        onView(withText(R.string.welcome_login)).check(matches(isDisplayed()))
+        onView(withText(R.string.welcome_enterprise_login)).check(matches(isDisplayed()))
     }
 
     @Test

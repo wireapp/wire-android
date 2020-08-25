63857b65e9f2b8ddfc9e3dc09414f9a9c65cf180
Gizem Burhanoglu <gizembrh@gmail.com>
2020-08-25 10:57:02 +0300

    Migrate ViewPager to ViewPager2 (#49)

--- app/src/main/kotlin/com/wire/android/feature/auth/registration/personal/CreatePersonalAccountFragment.kt
+++ app/src/main/kotlin/com/wire/android/feature/auth/registration/personal/CreatePersonalAccountFragment.kt
@@ -3,6 +3,7 @@ package com.wire.android.feature.auth.registration.personal
 import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.Fragment
+import com.google.android.material.tabs.TabLayoutMediator
 import com.wire.android.R
 import com.wire.android.core.extension.headingForAccessibility
 import kotlinx.android.synthetic.main.fragment_create_personal_account.*
@@ -24,11 +25,15 @@ class CreatePersonalAccountFragment : Fragment(R.layout.fragment_create_personal
             getString(R.string.authentication_tab_layout_title_email),
             getString(R.string.authentication_tab_layout_title_phone)
         )
-        createPersonalAccountViewPager.adapter = CreatePersonalAccountViewPagerAdapter(childFragmentManager, titles)
-        titles.forEachIndexed { index, title ->
-            createPersonalAccountTabLayout.getTabAt(index)?.contentDescription =
-                getString(R.string.create_an_account_tab_content_description, title, index + 1, titles.size)
-        }
+        createPersonalAccountViewPager.adapter = CreatePersonalAccountViewPagerAdapter(this)
+        TabLayoutMediator(createPersonalAccountTabLayout, createPersonalAccountViewPager) { tab, position ->
+            val title = titles[position]
+            tab.text = title
+            tab.contentDescription = getString(
+                R.string.create_an_account_tab_content_description,
+                title, position + 1, titles.size
+            )
+        }.attach()
     }
 
 

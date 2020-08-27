2fb2d1b926066ae2ff4d6497216cbfb991077ff7
Gizem Burhanoglu <gizembrh@gmail.com>
2020-08-27 13:55:22 +0300

    Update email test data

--- app/src/test/kotlin/com/wire/android/feature/auth/login/email/LoginWithEmailViewModelTest.kt
+++ app/src/test/kotlin/com/wire/android/feature/auth/login/email/LoginWithEmailViewModelTest.kt
@@ -115,7 +115,7 @@ class LoginWithEmailViewModelTest : UnitTest() {
     }
 
     companion object {
-        private const val TEST_EMAIL = "email"
+        private const val TEST_EMAIL = "test@wire.com"
         private const val TEST_EMPTY_PASSWORD = ""
         private const val TEST_VALID_PASSWORD = "password"
     }

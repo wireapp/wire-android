7df34c36cd9b2048c9aaea37811493bed780e103
Gizem Burhanoglu <gizembrh@gmail.com>
2020-09-11 14:05:53 +0300

    Add an ApiService method that returns complete retrofit Response

--- app/src/test/kotlin/com/wire/android/core/network/ApiServiceTest.kt
+++ app/src/test/kotlin/com/wire/android/core/network/ApiServiceTest.kt
@@ -1,16 +1,25 @@
 package com.wire.android.core.network
 
 import com.wire.android.UnitTest
-import com.wire.android.core.exception.*
-import com.wire.android.core.extension.EMPTY
+import com.wire.android.core.exception.BadRequest
+import com.wire.android.core.exception.EmptyResponseBody
+import com.wire.android.core.exception.Failure
+import com.wire.android.core.exception.Forbidden
+import com.wire.android.core.exception.InternalServerError
+import com.wire.android.core.exception.NetworkConnection
+import com.wire.android.core.exception.NotFound
+import com.wire.android.core.exception.ServerError
+import com.wire.android.core.exception.TooManyRequests
+import com.wire.android.core.exception.Unauthorized
 import com.wire.android.framework.functional.assertLeft
+import com.wire.android.framework.functional.assertRight
 import kotlinx.coroutines.runBlocking
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.Before
-
 import org.junit.Test
 import org.mockito.Mock
-import org.mockito.Mockito.*
+import org.mockito.Mockito.`when`
+import org.mockito.Mockito.verifyNoInteractions
 import retrofit2.Response
 
 class ApiServiceTest : UnitTest() {
@@ -28,17 +37,18 @@ class ApiServiceTest : UnitTest() {
 
     @Before
     fun setUp() {
+        `when`(networkHandler.isConnected).thenReturn(true)
         apiService = object : ApiService() {
             override val networkHandler: NetworkHandler = this@ApiServiceTest.networkHandler
         }
     }
 
     @Test
-    fun `given no network connection, when request called, returns NetworkConnection failure immediately`() {
+    fun `given rawRequest is called, when there's no network connection, then returns NetworkConnection failure immediately`() {
         runBlocking {
             `when`(networkHandler.isConnected).thenReturn(false)
 
-            val result = apiService.request(default = null, call = responseFunc)
+            val result = apiService.rawRequest(responseFunc)
 
             verifyNoInteractions(responseFunc)
             result.assertLeft { assertThat(it).isEqualTo(NetworkConnection) }
@@ -46,64 +56,93 @@ class ApiServiceTest : UnitTest() {
     }
 
     @Test
-    fun `given no default argument, when response is successful but has no body, returns EmptyResponseBody failure`() {
+    fun `given rawRequest is called, when response is successful, then returns the response`() {
         runBlocking {
-            `when`(networkHandler.isConnected).thenReturn(true)
+            `when`(response.isSuccessful).thenReturn(true)
 
+            val result = apiService.rawRequest(::testCall)
+
+            result.assertRight { assertThat(it).isEqualTo(response) }
+        }
+    }
+
+    @Test
+    fun `given request is called with a default value, when response is successful and has a body, then returns the body`() {
+        runBlocking {
+            `when`(response.isSuccessful).thenReturn(true)
+            `when`(response.body()).thenReturn(TEST_BODY)
+
+            val result = apiService.request(default = null, call = ::testCall)
+
+            result.assertRight { assertThat(it).isEqualTo(TEST_BODY) }
+        }
+    }
+
+    @Test
+    fun `given request is called with a default value, when response is successful but has no body, then returns default value`() {
+        runBlocking {
             `when`(response.isSuccessful).thenReturn(true)
             `when`(response.body()).thenReturn(null)
-            val responseFunc: suspend () -> Response<String> = { response }
 
-            val result = apiService.request(default = null, call = responseFunc)
+            val result = apiService.request(default = TEST_DEFAULT_ARGUMENT, call = ::testCall)
+
+            result.assertRight { assertThat(it).isEqualTo(TEST_DEFAULT_ARGUMENT) }
+        }
+    }
+
+    @Test
+    fun `given request is called without default value, when response successful but has no body, then returns EmptyResponseBody error`() {
+        runBlocking {
+            `when`(response.isSuccessful).thenReturn(true)
+            `when`(response.body()).thenReturn(null)
+
+            val result = apiService.request(default = null, call = ::testCall)
 
             result.assertLeft { assertThat(it).isEqualTo(EmptyResponseBody) }
         }
     }
 
     @Test
-    fun `given call fails with http 400 error, then returns BadRequest failure`() = runBlocking {
+    fun `given rawRequest is called, when call fails with http 400 error, then returns BadRequest failure`() =
         assertHttpError(400, BadRequest)
-    }
 
     @Test
-    fun `given call fails with http 401 error, then returns Unauthorized failure`() = runBlocking {
+    fun `given rawRequest is called, when call fails with http 401 error, then returns Unauthorized failure`() =
         assertHttpError(401, Unauthorized)
-    }
 
     @Test
-    fun `given call fails with http 403 error, then returns Forbidden failure`() = runBlocking {
+    fun `given rawRequest is called, when call fails with http 403 error, then returns Forbidden failure`() =
         assertHttpError(403, Forbidden)
-    }
 
     @Test
-    fun `given call fails with http 404 error, then returns NotFound failure`() = runBlocking {
+    fun `given rawRequest is called, when call fails with http 404 error, then returns NotFound failure`() =
         assertHttpError(404, NotFound)
-    }
 
     @Test
-    fun `given call fails with http 429 error, then returns TooManyRequests failure`() = runBlocking {
+    fun `given rawRequest is called, when call fails with http 429 error, then returns TooManyRequests failure`() =
         assertHttpError(429, TooManyRequests)
-    }
 
     @Test
-    fun `given call fails with http 500 error, then returns InternalServerError failure`() = runBlocking {
+    fun `given rawRequest is called, when call fails with http 500 error, then returns InternalServerError failure`() =
         assertHttpError(500, InternalServerError)
-    }
 
     @Test
-    fun `given call fails with any other error, then returns ServerError failure`() = runBlocking {
+    fun `given rawRequest is called, when call fails with any other error, then returns ServerError failure`() =
         assertHttpError(-1, ServerError)
-    }
 
-    private suspend fun assertHttpError(httpErrorCode: Int, failure: Failure) {
-        `when`(networkHandler.isConnected).thenReturn(true)
-
+    private fun assertHttpError(httpErrorCode: Int, failure: Failure): Unit = runBlocking {
         `when`(response.isSuccessful).thenReturn(false)
         `when`(response.code()).thenReturn(httpErrorCode)
-        val responseFunc: suspend () -> Response<String> = { response }
 
-        val result = apiService.request(String.EMPTY, responseFunc)
+        val result = apiService.rawRequest(::testCall)
 
         result.assertLeft { assertThat(it).isEqualTo(failure) }
+    }
+
+    private suspend fun testCall(): Response<String> = response
+
+    companion object {
+        private const val TEST_DEFAULT_ARGUMENT = "default"
+        private const val TEST_BODY = """ { "email": "test@wire.com" } """
     }
 }

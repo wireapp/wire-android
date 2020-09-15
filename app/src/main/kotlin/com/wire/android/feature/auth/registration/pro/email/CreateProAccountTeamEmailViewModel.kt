d7db0d3f45437a4473c69c7a151a84ad600d70eb
Bradley Wilson <bradley.wilson@wire.com>
2020-09-15 14:54:30 +0200

    Migrate team email live data to single live event

--- app/src/main/kotlin/com/wire/android/feature/auth/registration/pro/email/CreateProAccountTeamEmailViewModel.kt
+++ app/src/main/kotlin/com/wire/android/feature/auth/registration/pro/email/CreateProAccountTeamEmailViewModel.kt
@@ -4,6 +4,7 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.wire.android.core.async.DispatcherProvider
+import com.wire.android.core.ui.SingleLiveEvent
 import com.wire.android.core.usecase.DefaultUseCaseExecutor
 import com.wire.android.core.usecase.UseCaseExecutor
 
@@ -11,8 +12,8 @@ class CreateProAccountTeamEmailViewModel(
     override val dispatcherProvider: DispatcherProvider
 ) : ViewModel(), UseCaseExecutor by DefaultUseCaseExecutor(dispatcherProvider) {
 
-    private val _teamEmailLiveData = MutableLiveData<String>()
-    val teamEmailLiveData: LiveData<String> = _teamEmailLiveData
+    private val _teamEmailLiveData = SingleLiveEvent<String>()
+    val teamEmailLiveData: SingleLiveEvent<String> = _teamEmailLiveData
 
     private val _confirmationButtonEnabled = MutableLiveData<Boolean>()
     val confirmationButtonEnabled: LiveData<Boolean> = _confirmationButtonEnabled

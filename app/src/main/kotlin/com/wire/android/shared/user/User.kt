81227744d61969a87fc903e954c5e7d71a78dbfd
Gizem Burhanoglu <gizembrh@gmail.com>
2020-09-28 16:37:09 +0200

    Save session info after registration

--- app/src/main/kotlin/com/wire/android/shared/user/User.kt
+++ app/src/main/kotlin/com/wire/android/shared/user/User.kt
@@ -1,3 +1,9 @@
 package com.wire.android.shared.user
 
-data class User(val id : String, val name: String, val email: String? = null)
+import com.wire.android.core.extension.EMPTY
+
+data class User(val id : String, val name: String, val email: String? = null) {
+    companion object {
+        val EMPTY = User(String.EMPTY, String.EMPTY, null)
+    }
+}

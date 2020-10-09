c929fee87391a1cfb274534b22bbc385354c1bde
Gizem Burhanoglu <gizembrh@gmail.com>
2020-10-09 15:52:28 +0200

    Add tests for navigation structure

--- app/src/test/kotlin/com/wire/android/framework/FrameworkTest.kt
+++ app/src/test/kotlin/com/wire/android/framework/FrameworkTest.kt
@@ -1,16 +1,19 @@
 package com.wire.android.framework
 
+import androidx.core.os.bundleOf
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.Observer
-import com.wire.android.UnitTest
+import com.wire.android.AndroidTest
+import com.wire.android.framework.android.contentEquals
 import com.wire.android.framework.livedata.TestLifecycleOwner
 import com.wire.android.framework.livedata.assertNotUpdated
 import com.wire.android.framework.livedata.awaitValue
 import kotlinx.coroutines.runBlocking
+import org.assertj.core.api.Assertions.assertThat
 import org.junit.Test
 import java.util.concurrent.TimeoutException
 
-class FrameworkTest : UnitTest() {
+class FrameworkTest : AndroidTest() {
 
     @Test
     fun `given awaitValue called on a LiveData, when a value is received, then doesn't disrupt the test`() {
@@ -52,5 +55,32 @@ class FrameworkTest : UnitTest() {
             liveData.observe(lifecycleOwner, Observer { })
             lifecycleOwner.destroy()
         }
+    }
+
+    @Test
+    fun `given contentEquals called on a bundle, when two bundles contain the same mappings, then returns true`() {
+        val bundle1 = bundleOf("key1" to "value1", "key2" to 4, "key3" to arrayOf("a", "b"), "key4" to bundleOf("x" to 9))
+        val bundle2 = bundleOf("key1" to "value1", "key2" to 4, "key3" to arrayOf("a", "b"), "key4" to bundleOf("x" to 9))
+
+        assertThat(bundle1.contentEquals(bundle2)).isTrue()
+        assertThat(bundle2.contentEquals(bundle1)).isTrue()
+    }
+
+    @Test
+    fun `given contentEquals called on a bundle, when one of the bundles is a subset of the other, then returns false`() {
+        val bundle1 = bundleOf("key1" to "value1", "key2" to 4, "key3" to arrayOf("a", "b"))
+        val bundle2 = bundleOf("key1" to "value1", "key2" to 4)
+
+        assertThat(bundle1.contentEquals(bundle2)).isFalse()
+        assertThat(bundle2.contentEquals(bundle1)).isFalse()
+    }
+
+    @Test
+    fun `given contentEquals called on a bundle, when two bundles are not equal, then returns false`() {
+        val bundle1 = bundleOf("key3" to arrayOf("a", "b"))
+        val bundle2 = bundleOf("key1" to "value1", "key2" to 4)
+
+        assertThat(bundle1.contentEquals(bundle2)).isFalse()
+        assertThat(bundle2.contentEquals(bundle1)).isFalse()
     }
 }

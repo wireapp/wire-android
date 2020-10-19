d44c821033830279e60277727ea459f05e4f9b67
Marco Bierbach <gardosen@gmx.de>
2020-10-19 17:00:14 +0200

    feedback changes
    - moved companion object to the bottom
    - removed TODO and created a jira ticket for it

--- app/src/androidTest/kotlin/com/wire/android/framework/retry/RetryTestRule.kt
+++ app/src/androidTest/kotlin/com/wire/android/framework/retry/RetryTestRule.kt
@@ -7,10 +7,6 @@ import org.junit.runners.model.Statement
 
 class RetryTestRule(val retryCount: Int = RETRY_COUNT) : TestRule {
 
-    companion object {
-        private const val RETRY_COUNT = 3
-    }
-
     private val TAG = RetryTestRule::class.java.simpleName
 
     override fun apply(testStatement: Statement, description: Description): Statement = object : Statement() {
@@ -23,18 +19,19 @@ class RetryTestRule(val retryCount: Int = RETRY_COUNT) : TestRule {
                 Log.i(TAG, "${description.displayName}: run #${i}")
                 try {
                     testStatement.evaluate()
-                    //TODO if successful without a catch we need to intervent
-                    // here and print out that the test run was successful after a failure
                     return
                 } catch (t: Throwable) {
                     caughtThrowable = t
                     Log.e(TAG, "${description.displayName}: run #${i} failed")
-                    //TODO i guess at this point we need to add the reporting modification
                 }
             }
 
             Log.e(TAG, "${description.displayName}: giving up after $retryCount failures")
             throw caughtThrowable
         }
+    }
+
+    companion object {
+        private const val RETRY_COUNT = 3
     }
 }

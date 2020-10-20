1aaddc6597adc6a2432ecc2777d3a3ada4707fcb
Fernando Cejas <fcejas@gmail.com>
2020-10-20 16:19:22 +0200

    Group detekt configuration in quality.gradle.kts file.

--- buildSrc/src/main/kotlin/scripts/quality.gradle.kts
+++ buildSrc/src/main/kotlin/scripts/quality.gradle.kts
@@ -3,9 +3,10 @@ package scripts
 plugins {
     id("com.android.application") apply false
     id("scripts.coverage")
-    id("scripts.code-analysis")
+    id("io.gitlab.arturbosch.detekt")
 }
 
+// Lint Configuration
 android {
     lintOptions {
         isQuiet = true
@@ -16,4 +17,36 @@ android {
         disable("IconDensities")            //For testing purpose. This is safe to remove.
         disable("IconMissingDensityFolder") //For testing purpose. This is safe to remove.
     }
+}
+
+// Detekt Configuration
+val detektAll by tasks.registering(io.gitlab.arturbosch.detekt.Detekt::class) {
+    group = "Quality"
+    description = "Runs a detekt code analysis ruleset on the Wire Android codebase "
+    parallel = true
+    buildUponDefaultConfig = true
+
+    val outputFile = "${project.buildDir}/staticAnalysis/index.html"
+
+    setSource(files(rootProject.projectDir))
+    config.setFrom("${project.rootDir}/config/detekt/detekt.yml")
+
+    include("**/*.kt")
+    exclude("**/*.kts", "*/build/*", "/buildSrc")
+
+    reports {
+        xml.enabled = true
+        html.enabled = true
+        html.destination = file(outputFile)
+        txt.enabled = false
+    }
+
+    val reportFile = "Static Analysis Report: $outputFile \n"
+    doFirst { println(reportFile) }
+    doLast { println(reportFile) }
+}
+
+tasks.register("staticCodeAnalysis") {
+    description = "Analyses code within the Wire Android codebase"
+    dependsOn(detektAll)
 }

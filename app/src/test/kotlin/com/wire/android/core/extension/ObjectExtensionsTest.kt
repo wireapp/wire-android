ce9ecc4a03d16702ca3bb66b7726202ad177e1a7
Fernando Cejas <fcejas@gmail.com>
2020-10-23 12:02:50 +0200

    Migrate core.extension.

--- app/src/test/kotlin/com/wire/android/core/extension/ObjectExtensionsTest.kt
+++ app/src/test/kotlin/com/wire/android/core/extension/ObjectExtensionsTest.kt
@@ -1,6 +1,7 @@
 package com.wire.android.core.extension
 
-import org.assertj.core.api.Assertions.assertThat
+import org.amshove.kluent.shouldBe
+import org.amshove.kluent.shouldEqual
 import org.junit.Test
 
 class ObjectExtensionsTest {
@@ -8,12 +9,12 @@ class ObjectExtensionsTest {
     @Test
     fun `given a null object, when toStringOrEmpty is called, returns empty string`() {
         val x: Int? = null
-        assertThat(x.toStringOrEmpty()).isEqualTo("")
+        x.toStringOrEmpty() shouldBe String.EMPTY
     }
 
     @Test
     fun `given a non-null object, when toStringOrEmpty is called, returns its toString() value`() {
         val x: Int? = 3
-        assertThat(x.toStringOrEmpty()).isEqualTo(3.toString())
+        x.toStringOrEmpty() shouldEqual 3.toString()
     }
 }

7ce0238ce0b527d33e4a24b28c4b3f6a6aabd6b3
Gizem Burhanoglu <gizembrh@gmail.com>
2020-11-04 10:40:40 +0100

    Add test for RetryTestRule

--- app/src/androidTest/kotlin/com/wire/android/framework/retry/RetryTestRule.kt
+++ app/src/androidTest/kotlin/com/wire/android/framework/retry/RetryTestRule.kt
@@ -7,31 +7,31 @@ import org.junit.runners.model.Statement
 
 class RetryTestRule(val retryCount: Int = RETRY_COUNT) : TestRule {
 
-    private val TAG = RetryTestRule::class.java.simpleName
+    override fun apply(testStatement: Statement, description: Description): Statement =
+        if (retryCount < 1) testStatement
+        else object : Statement() {
+            @Throws(Throwable::class)
+            override fun evaluate() {
+                lateinit var caughtThrowable: Throwable
 
-    override fun apply(testStatement: Statement, description: Description): Statement = object : Statement() {
-        @Throws(Throwable::class)
-        override fun evaluate() {
-            lateinit var caughtThrowable: Throwable
-
-            // implement retry logic here
-            for (i in 1..retryCount) {
-                Log.i(TAG, "${description.displayName}: run #${i}")
-                try {
-                    testStatement.evaluate()
-                    return
-                } catch (t: Throwable) {
-                    caughtThrowable = t
-                    Log.e(TAG, "${description.displayName}: run #${i} failed")
+                for (i in 1..retryCount) {
+                    Log.i(TAG, "${description.displayName}: run #${i}")
+                    try {
+                        testStatement.evaluate()
+                        return
+                    } catch (t: Throwable) {
+                        caughtThrowable = t
+                        Log.e(TAG, "${description.displayName}: run #${i} failed")
+                    }
                 }
-            }
 
-            Log.e(TAG, "${description.displayName}: giving up after $retryCount failures")
-            throw caughtThrowable
+                Log.e(TAG, "${description.displayName}: giving up after $retryCount failures")
+                throw caughtThrowable
+            }
         }
-    }
 
     companion object {
+        private val TAG = RetryTestRule::class.java.canonicalName!!
         private const val RETRY_COUNT = 3
     }
 }

2ccbf7ecc7df547894493a4d47cc956cd78be15f
Fernando Cejas <fcejas@gmail.com>
2020-11-17 14:35:54 +0100

    Refactor UserAgentInterceptor.

--- app/src/main/kotlin/com/wire/android/core/network/NetworkHandler.kt
+++ app/src/main/kotlin/com/wire/android/core/network/NetworkHandler.kt
@@ -7,10 +7,12 @@ import android.net.NetworkCapabilities
  * Class which returns information about the network connection state.
  */
 class NetworkHandler(private val connectivityManager: ConnectivityManager) {
+
     @SuppressWarnings("ReturnCount")
     fun isConnected(): Boolean {
         val network = connectivityManager.activeNetwork ?: return false
         val networkCapabilities = connectivityManager.getNetworkCapabilities(network) ?: return false
+
         return when {
             networkCapabilities.hasTransport(NetworkCapabilities.TRANSPORT_WIFI) -> true
             networkCapabilities.hasTransport(NetworkCapabilities.TRANSPORT_CELLULAR) -> true
--- app/src/main/kotlin/com/wire/android/core/network/UserAgentInterceptor.kt
+++ app/src/main/kotlin/com/wire/android/core/network/UserAgentInterceptor.kt
@@ -3,36 +3,31 @@ package com.wire.android.core.network
 import com.wire.android.core.config.AppVersionNameConfig
 import com.wire.android.core.extension.EMPTY
 import okhttp3.Interceptor
-import okhttp3.Response
 
-class UserAgentInterceptor(
-    userAgentConfig: UserAgentConfig
-) : Interceptor {
+class UserAgentInterceptor(userAgentConfig: UserAgentConfig) : Interceptor {
 
-    override fun intercept(chain: Interceptor.Chain): Response =
-        when (chain.request().header(USER_AGENT_HEADER_KEY)) {
-            null, String.EMPTY -> determineUserAgentRequest(chain)
-            else -> chain.proceed(chain.request())
+    private val newUserAgentHeader =
+        "Android ${userAgentConfig.androidVersion} / " +
+        "Wire ${userAgentConfig.appVersionNameNameConfig.versionName} / " +
+        "HttpLibrary ${userAgentConfig.httpUserAgent}"
+
+    override fun intercept(chain: Interceptor.Chain) =
+        when (requestHasUserHeader(chain)) {
+            true -> chain.proceed(chain.request())
+            else -> addUserAgentHeader(chain)
         }
 
-    private fun determineUserAgentRequest(chain: Interceptor.Chain) =
-        chain.proceed(
-            chain.request()
-                .newBuilder()
-                .addHeader(USER_AGENT_HEADER_KEY, newUserAgentHeader())
-                .build()
-        )
+    private fun addUserAgentHeader(chain: Interceptor.Chain) =
+        chain.proceed(chain.request()
+            .newBuilder()
+            .addHeader(USER_AGENT_HEADER_KEY, newUserAgentHeader)
+            .build())
 
-    private fun newUserAgentHeader() = "$androidVersion / $wireVersion / $httpVersion"
-
-    private val androidVersion: String =
-        "Android ${userAgentConfig.androidVersion}"
-
-    private val wireVersion: String =
-        "Wire ${userAgentConfig.appVersionNameNameConfig.versionName}"
-
-    private val httpVersion: String =
-        "HttpLibrary ${userAgentConfig.httpUserAgent}"
+    private fun requestHasUserHeader(chain: Interceptor.Chain): Boolean =
+        when (chain.request().header(USER_AGENT_HEADER_KEY)) {
+            null, String.EMPTY -> false
+            else -> true
+        }
 
     companion object {
         private const val USER_AGENT_HEADER_KEY = "User-Agent"

aab595816d07aefaee95ca46a244ec7286ad0fbc
Gizem Burhanoglu <gizembrh@gmail.com>
2020-11-27 15:55:01 +0100

    Add "contact" table to UserDatabase (#146)

---
+++ app/src/main/kotlin/com/wire/android/feature/contact/datasources/local/ContactDao.kt
@@ -0,0 +1,19 @@
+package com.wire.android.feature.contact.datasources.local
+
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+
+@Dao
+interface ContactDao {
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insert(contact: ContactEntity)
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertAll(contacts: List<ContactEntity>)
+
+    @Query("SELECT * FROM contact")
+    suspend fun contacts(): List<ContactEntity>
+}

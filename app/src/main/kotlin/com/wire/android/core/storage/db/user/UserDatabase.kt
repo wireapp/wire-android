5f497f506a00c77b2f064853ef7ba4ed35ef2486
Gizem Burhanoglu <gizembrh@gmail.com>
2020-12-02 17:35:54 +0100

    Add "conversation_members" table to store member id - conversation pairs

--- app/src/main/kotlin/com/wire/android/core/storage/db/user/UserDatabase.kt
+++ app/src/main/kotlin/com/wire/android/core/storage/db/user/UserDatabase.kt
@@ -6,13 +6,17 @@ import com.wire.android.feature.contact.datasources.local.ContactDao
 import com.wire.android.feature.contact.datasources.local.ContactEntity
 import com.wire.android.feature.conversation.list.datasources.local.ConversationDao
 import com.wire.android.feature.conversation.list.datasources.local.ConversationEntity
+import com.wire.android.feature.conversation.members.datasources.local.ConversationMemberEntity
+import com.wire.android.feature.conversation.members.datasources.local.ConversationMembersDao
 
 private const val VERSION = 1
 
-@Database(entities = [ConversationEntity::class, ContactEntity::class], version = VERSION)
+@Database(entities = [ConversationEntity::class, ContactEntity::class, ConversationMemberEntity::class], version = VERSION)
 abstract class UserDatabase : RoomDatabase() {
 
     abstract fun conversationDao(): ConversationDao
 
     abstract fun contactDao(): ContactDao
+
+    abstract fun conversationMembersDao(): ConversationMembersDao
 }

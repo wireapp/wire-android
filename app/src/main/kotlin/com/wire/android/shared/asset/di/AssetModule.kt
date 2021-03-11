744edf6290954c05c6c35936e5dad4f4ff4eccae
Gizem Burhanoglu <gizembrh@gmail.com>
2021-03-11 18:00:49 +0100

    Feature: Load Contact Asset - Part 4 - Add custom PublicAsset loader to Glide (#205)

--- app/src/main/kotlin/com/wire/android/shared/asset/di/AssetModule.kt
+++ app/src/main/kotlin/com/wire/android/shared/asset/di/AssetModule.kt
@@ -6,6 +6,7 @@ import com.wire.android.shared.asset.AssetRepository
 import com.wire.android.shared.asset.datasources.AssetDataSource
 import com.wire.android.shared.asset.datasources.remote.AssetApi
 import com.wire.android.shared.asset.datasources.remote.AssetRemoteDataSource
+import com.wire.android.shared.asset.ui.imageloader.publicasset.PublicAssetLoaderFactory
 import org.koin.dsl.module
 
 val assetModule = module {
@@ -15,4 +16,6 @@ val assetModule = module {
 
     single { AssetRemoteDataSource(get(), get()) }
     single<AssetRepository> { AssetDataSource(get()) }
+
+    single { PublicAssetLoaderFactory(get()) }
 }

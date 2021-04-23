5c72254be7decd193921c75f2ff90ee4c0590f9c
Vitor Hugo Schwaab <vitor@schwaab.xyz>
2021-04-23 17:31:59 +0200

    Use import alias for `com.wire.cryptobox.PreKey` when used alongside `PreKey`

--- crypto/src/main/kotlin/com/wire/android/crypto/mapper/PreKeyMapper.kt
+++ crypto/src/main/kotlin/com/wire/android/crypto/mapper/PreKeyMapper.kt
@@ -2,15 +2,16 @@ package com.wire.android.crypto.mapper
 
 import android.util.Base64
 import com.wire.android.crypto.model.PreKey
+import com.wire.cryptobox.PreKey as CryptoPreKey
 
 class PreKeyMapper {
 
-    fun toCryptoBoxModel(data: PreKey): com.wire.cryptobox.PreKey {
+    fun toCryptoBoxModel(data: PreKey): CryptoPreKey {
         val decoded = Base64.decode(data.encodedData, Base64.NO_WRAP)
-        return com.wire.cryptobox.PreKey(data.id, decoded)
+        return CryptoPreKey(data.id, decoded)
     }
 
-    fun fromCryptoBoxModel(model: com.wire.cryptobox.PreKey): PreKey {
+    fun fromCryptoBoxModel(model: CryptoPreKey): PreKey {
         val encoded = Base64.encode(model.data, Base64.NO_WRAP)
         return PreKey(model.id, encoded.decodeToString())
     }
--- crypto/src/test/kotlin/com/wire/android/crypto/mapper/PreKeyMapperTest.kt
+++ crypto/src/test/kotlin/com/wire/android/crypto/mapper/PreKeyMapperTest.kt
@@ -4,6 +4,7 @@ import com.wire.android.crypto.AndroidTest
 import com.wire.android.crypto.model.PreKey
 import org.amshove.kluent.shouldBeEqualTo
 import org.junit.Test
+import com.wire.cryptobox.PreKey as CryptoPreKey
 
 class PreKeyMapperTest : AndroidTest() {
 
@@ -11,7 +12,7 @@ class PreKeyMapperTest : AndroidTest() {
 
     @Test
     fun `given a CryptoBox model, when converting to data model, it should be done correctly`() {
-        val model = com.wire.cryptobox.PreKey(42, byteArrayOf(0x00, 0x16, 0x32, 0x42))
+        val model = CryptoPreKey(42, byteArrayOf(0x00, 0x16, 0x32, 0x42))
         val result = subject.fromCryptoBoxModel(model)
 
         result shouldBeEqualTo PreKey(42, "ABYyQg==")
@@ -22,7 +23,7 @@ class PreKeyMapperTest : AndroidTest() {
         val model = PreKey(42, "I wish you a very nice day")
         val result = subject.toCryptoBoxModel(model)
 
-        val expectedResult = com.wire.cryptobox.PreKey(
+        val expectedResult = CryptoPreKey(
             42,
             byteArrayOf(35, 8, -84, -121, 42, 46, 106, -9, -85, -54, 120, -100, 121, -42, -78)
         )
@@ -42,7 +43,7 @@ class PreKeyMapperTest : AndroidTest() {
 
     @Test
     fun `given CryptoBox model is being converted, when converting it back, it should return something equal to the original`() {
-        val original = com.wire.cryptobox.PreKey(42, byteArrayOf(0x00, 0x16, 0x32, 0x42))
+        val original = CryptoPreKey(42, byteArrayOf(0x00, 0x16, 0x32, 0x42))
 
         val converted = subject.fromCryptoBoxModel(original)
 

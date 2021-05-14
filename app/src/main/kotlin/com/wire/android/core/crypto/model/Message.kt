838f3068cb2b701ff69115f517638d053b7476a5
Vitor Hugo Schwaab <vitor@schwaab.xyz>
2021-05-14 17:36:09 +0200

    Make `PlainMessage` and `EncryptedMessage` into data classes

--- app/src/main/kotlin/com/wire/android/core/crypto/model/Message.kt
+++ app/src/main/kotlin/com/wire/android/core/crypto/model/Message.kt
@@ -1,5 +1,35 @@
 package com.wire.android.core.crypto.model
 
-class EncryptedMessage(val data: ByteArray)
+data class EncryptedMessage(val data: ByteArray) {
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
 
-class PlainMessage(val data: ByteArray)
+        other as EncryptedMessage
+
+        if (!data.contentEquals(other.data)) return false
+
+        return true
+    }
+
+    override fun hashCode(): Int {
+        return data.contentHashCode()
+    }
+}
+
+data class PlainMessage(val data: ByteArray) {
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
+
+        other as PlainMessage
+
+        if (!data.contentEquals(other.data)) return false
+
+        return true
+    }
+
+    override fun hashCode(): Int {
+        return data.contentHashCode()
+    }
+}

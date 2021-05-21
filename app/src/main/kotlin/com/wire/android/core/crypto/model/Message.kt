5ee979b34ea892eb231a42be241a1d0b378e6166
Vitor Hugo Schwaab <vitor@schwaab.xyz>
2021-05-21 14:18:36 +0200

    Simplify `EncryptedMessage` and `PlainMessage` implementation of `equals` and `hashCode`

--- app/src/main/kotlin/com/wire/android/core/crypto/model/Message.kt
+++ app/src/main/kotlin/com/wire/android/core/crypto/model/Message.kt
@@ -1,35 +1,15 @@
 package com.wire.android.core.crypto.model
 
 data class EncryptedMessage(val data: ByteArray) {
-    override fun equals(other: Any?): Boolean {
-        if (this === other) return true
-        if (javaClass != other?.javaClass) return false
+    override fun equals(other: Any?): Boolean =
+        this === other || (other is EncryptedMessage && other.data.contentEquals(data))
 
-        other as EncryptedMessage
-
-        if (!data.contentEquals(other.data)) return false
-
-        return true
-    }
-
-    override fun hashCode(): Int {
-        return data.contentHashCode()
-    }
+    override fun hashCode() = data.contentHashCode()
 }
 
 data class PlainMessage(val data: ByteArray) {
-    override fun equals(other: Any?): Boolean {
-        if (this === other) return true
-        if (javaClass != other?.javaClass) return false
+    override fun equals(other: Any?): Boolean =
+        this === other || (other is PlainMessage && other.data.contentEquals(data))
 
-        other as PlainMessage
-
-        if (!data.contentEquals(other.data)) return false
-
-        return true
-    }
-
-    override fun hashCode(): Int {
-        return data.contentHashCode()
-    }
+    override fun hashCode() = data.contentHashCode()
 }

b09f666cd228f7aef58a5bdec283bc6e230a6cc7
Vitor Hugo Schwaab <vitor@schwaab.xyz>
2021-06-01 10:46:31 +0200

    Remove UserAgent header from HTTP requests (#223)

--- app/src/main/kotlin/com/wire/android/core/network/UserAgentInterceptor.kt
+++ app/src/main/kotlin/com/wire/android/core/network/UserAgentInterceptor.kt
@@ -1,28 +1,25 @@
 package com.wire.android.core.network
 
-import com.wire.android.core.extension.EMPTY
 import com.wire.android.core.network.NetworkConfig.Companion.USER_AGENT_HEADER_KEY
 import okhttp3.Interceptor
 
-class UserAgentInterceptor(private val config: NetworkConfig) : Interceptor {
+class UserAgentInterceptor : Interceptor {
 
-    override fun intercept(chain: Interceptor.Chain) =
-        if (requestHasUserHeader(chain)) {
-            chain.proceed(chain.request())
-        } else addUserAgentHeader(chain)
+    override fun intercept(chain: Interceptor.Chain) = if (requestHasUserHeader(chain))
+        removeUserAgent(chain)
+    else
+        chain.proceed(chain.request())
 
-    private fun addUserAgentHeader(chain: Interceptor.Chain) =
-        chain.proceed(chain.request()
+    private fun removeUserAgent(chain: Interceptor.Chain) = chain.proceed(
+        chain.request()
             .newBuilder()
-            .addHeader(USER_AGENT_HEADER_KEY, buildUserAgentHeader())
-            .build())
+            .removeHeader(USER_AGENT_HEADER_KEY)
+            .build()
+    )
 
     private fun requestHasUserHeader(chain: Interceptor.Chain): Boolean =
         when (chain.request().header(USER_AGENT_HEADER_KEY)) {
-            null, String.EMPTY -> false
+            null -> false
             else -> true
         }
-
-    private fun buildUserAgentHeader(): String =
-        config.osVersion + " / " + config.appVersion + " / " + config.userAgent
 }

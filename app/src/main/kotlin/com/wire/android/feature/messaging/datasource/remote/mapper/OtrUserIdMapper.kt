e7c349de401b043a980e171c7b89adbab17b8f3e
Vitor Hugo Schwaab <vitor@schwaab.xyz>
2021-06-09 14:51:57 +0200

    Fix OtrUserIdMapper and add tests to it

--- app/src/main/kotlin/com/wire/android/feature/messaging/datasource/remote/mapper/OtrUserIdMapper.kt
+++ app/src/main/kotlin/com/wire/android/feature/messaging/datasource/remote/mapper/OtrUserIdMapper.kt
@@ -9,8 +9,8 @@ class OtrUserIdMapper {
 
     fun toOtrUserId(userId: String): Otr.UserId {
         val bytes = ByteArray(USER_UID_BYTE_COUNT)
-        val uuid = UUID.fromString(userId)
         val byteBuffer = ByteBuffer.wrap(bytes).asLongBuffer()
+        val uuid = UUID.fromString(userId)
         byteBuffer.put(uuid.mostSignificantBits)
         byteBuffer.put(uuid.leastSignificantBits)
         return Otr.UserId.newBuilder()
@@ -18,7 +18,11 @@ class OtrUserIdMapper {
             .build()
     }
 
-    fun fromOtrUserId(otrUserId: Otr.UserId) = UUID.nameUUIDFromBytes(otrUserId.uuid.toByteArray()).toString()
+    fun fromOtrUserId(otrUserId: Otr.UserId): String {
+        val bytes = otrUserId.uuid.toByteArray()
+        val byteBuffer = ByteBuffer.wrap(bytes)
+        return UUID(byteBuffer.long, byteBuffer.long).toString()
+    }
 
     companion object {
         private const val USER_UID_BYTE_COUNT = 16

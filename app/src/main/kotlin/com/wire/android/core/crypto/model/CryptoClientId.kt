86562a0d661497779cf97b5947619812d935cf8b
Vitor Hugo Schwaab <vitor@schwaab.xyz>
2021-06-10 10:36:53 +0200

    Remove useless todo

--- app/src/main/kotlin/com/wire/android/core/crypto/model/CryptoClientId.kt
+++ app/src/main/kotlin/com/wire/android/core/crypto/model/CryptoClientId.kt
@@ -1,6 +1,5 @@
 package com.wire.android.core.crypto.model
 
-// TODO: Maybe replace by ClientId that is being developed in order to register a client?
 data class CryptoClientId(val value: String) {
     override fun toString() = value
 }

bda2f9c5237aef319b55135cf4d846a02dacedea
Vitor Hugo Schwaab <vitor@schwaab.xyz>
2021-06-23 15:53:22 +0200

    Extract testImplementation dependencies as constants

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -2,6 +2,8 @@ private object Dependencies {
     const val androidBuildTools = "com.android.tools.build:gradle:4.1.0"
     const val kotlinGradlePlugin = "org.jetbrains.kotlin:kotlin-gradle-plugin:1.4.10"
     const val detektGradlePlugin = "io.gitlab.arturbosch.detekt:detekt-gradle-plugin:1.14.1"
+    const val junit = "junit:junit:4.13"
+    const val kluent = "org.amshove.kluent:kluent:1.60"
 }
 
 plugins {
@@ -18,6 +20,6 @@ dependencies {
     implementation(Dependencies.kotlinGradlePlugin)
     implementation(Dependencies.detektGradlePlugin)
 
-    testImplementation("junit:junit:4.13")
-    testImplementation("org.amshove.kluent:kluent:1.60")
+    testImplementation(Dependencies.junit)
+    testImplementation(Dependencies.kluent)
 }

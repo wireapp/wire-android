db47001baa23a2812996e1e113d340a1e109223e
Vitor Hugo Schwaab <vitor@schwaab.xyz>
2021-07-20 08:51:41 +0200

    Remove NexusURL

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,26 +1,4 @@
 buildscript {
-    val nexusUrl = run {
-        val urlEnvVar = "NEXUS_URL"
-        val urlLocalVar = "nexus.url"
-        val localPropertiesFileName = "local.properties"
-
-        val properties = java.util.Properties()
-        val propertiesFile = project.rootProject.file(localPropertiesFileName)
-
-        return@run System.getenv(urlEnvVar) ?: run {
-            properties.load(propertiesFile.inputStream())
-            properties.getProperty(urlLocalVar)
-        }
-    }
-
-    nexusUrl?.takeIf { it.isNotBlank() }?.let { url ->
-        fun RepositoryHandler.addNexus() = repositories.maven {
-            setUrl(url)
-            isAllowInsecureProtocol = true //TODO Remove when nexusUrl is not needed anymore
-        }
-        buildscript.repositories.addNexus()
-        allprojects { repositories.addNexus() }
-    }
     repositories {
         google()
         jcenter() //TODO Replace with mavenCentral

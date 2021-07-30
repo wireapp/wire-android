8c09d8f9395bbc36e15a0a5dee46859e6b859459
Vitor Hugo Schwaab <vitor@schwaab.xyz>
2021-07-30 12:08:05 +0200

    Improve KDoc for `spread` function

--- app/src/main/kotlin/com/wire/android/core/util/MapExt.kt
+++ app/src/main/kotlin/com/wire/android/core/util/MapExt.kt
@@ -1,10 +1,17 @@
 package com.wire.android.core.util
 
 /**
- * Based on the selection of an intermediate key, adds a new layer of mapping.
- * For example, take the following map as input {A: 1, B: 2, C: 3}
- * And an intermediate key selector that returns the value + 1.
- * The result would be:
+ * Based on the selection of an intermediate key, adds a new layer of mapping
+ * to each of the current values value.
+ *
+ * **Example**
+ *
+ * Take this map as input: {A: 1, B: 2, C: 3}
+ *
+ * And an intermediate key selector that returns `value + 1`.
+ *
+ * The result would be the following:
+ *
  * {A: {2: 1}, B: {3: 2}, C: {4: 3}}
  */
 inline fun <K, V, R> Map<K, V>.spread(intermediateKeySelector: (K, V) -> R): Map<K, Map<R, V>> =

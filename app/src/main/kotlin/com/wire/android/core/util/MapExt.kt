194d72cfb6ae92a0d99f3f78cf91802158a671c0
Vitor Hugo Schwaab <vitor@schwaab.xyz>
2021-07-30 12:02:11 +0200

    Simplify `spread` function

--- app/src/main/kotlin/com/wire/android/core/util/MapExt.kt
+++ app/src/main/kotlin/com/wire/android/core/util/MapExt.kt
@@ -2,19 +2,13 @@ package com.wire.android.core.util
 
 /**
  * Based on the selection of an intermediate key, adds a new layer of mapping.
- * For example, take the following map as input [A: [1,2], B: [2], C: [3]]
- * And an intermediate key selection that returns true if the value contains an even number.
+ * For example, take the following map as input {A: 1, B: 2, C: 3}
+ * And an intermediate key selector that returns the value + 1.
  * The result would be:
- * [A: [true: [1,2]], B: [true: [2], C: [false: [3]]]
- *
+ * {A: {2: 1}, B: {3: 2}, C: {4: 3}}
  */
-inline fun <K, V, R> Map<K, V>.spread(groupKeySelector: (K, V) -> R): Map<K, Map<R, V>> {
-    val newMap = mutableMapOf<K, MutableMap<R, V>>()
-
-    this.forEach { entry ->
-        val intermediateKey = groupKeySelector(entry.key, entry.value)
-        newMap[entry.key] = newMap[entry.key] ?: mutableMapOf()
-        newMap[entry.key]!![intermediateKey] = entry.value
+inline fun <K, V, R> Map<K, V>.spread(intermediateKeySelector: (K, V) -> R): Map<K, Map<R, V>> =
+    entries.fold(mutableMapOf()) { accumulator, entry ->
+        accumulator[entry.key] = mapOf(intermediateKeySelector(entry.key, entry.value) to entry.value)
+        accumulator
     }
-    return newMap
-}

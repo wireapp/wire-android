5c41ca277f6b6b132cf21600bf99f65d0e7b44b7
oussamah <oussama.has100@gmail.com>
2021-08-05 00:14:30 +0100

    implement quick sync

--- app/src/main/kotlin/com/wire/android/core/events/datasource/EventDataSource.kt
+++ app/src/main/kotlin/com/wire/android/core/events/datasource/EventDataSource.kt
@@ -1,30 +1,91 @@
 package com.wire.android.core.events.datasource
 
+import com.tinder.scarlet.WebSocket
 import com.wire.android.core.events.Event
 import com.wire.android.core.events.EventRepository
+import com.wire.android.core.events.datasource.local.NotificationLocalDataSource
+import com.wire.android.core.events.datasource.remote.NotificationRemoteDataSource
+import com.wire.android.core.events.datasource.remote.Payload
 import com.wire.android.core.events.datasource.remote.WebSocketService
-import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.collect
-import kotlinx.coroutines.flow.flow
+import com.wire.android.core.extension.EMPTY
+import com.wire.android.core.functional.*
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.channels.awaitClose
+import kotlinx.coroutines.channels.trySendBlocking
+import kotlinx.coroutines.flow.*
+import kotlinx.coroutines.launch
 
-class EventDataSource(private val webSocketService: WebSocketService) : EventRepository {
-    override fun events(): Flow<Event> = flow {
-        webSocketService.receiveEvent().collect {
-            it.payload?.let { payloads ->
-                for (payload in payloads)
-                    if (payload.type == NEW_MESSAGE_TYPE && payload.data != null)
-                        emit(
-                            Event.Conversation.MessageEvent(
-                                it.id,
-                                payload.conversation,
-                                payload.data.sender,
-                                payload.from,
-                                payload.data.text,
-                                payload.time
-                            )
-                        )
+class EventDataSource(
+    private val webSocketService: WebSocketService,
+    private val externalScope: CoroutineScope,
+    private val notificationLocalDataSource: NotificationLocalDataSource,
+    private val notificationRemoteDataSource: NotificationRemoteDataSource
+) : EventRepository {
+    override fun events(): Flow<Event> = callbackFlow {
+        externalScope.launch {
+            webSocketService.observeWebSocketEvent().collect {
+                if (it is WebSocket.Event.OnConnectionOpened<*>) {
+                    val notificationId = lastNotificationId("8ccdab56ec2156ad")
+                    var hasMore = true
+                    while (hasMore) {
+                        notificationRemoteDataSource.notificationsByBatch(100, "8ccdab56ec2156ad", notificationId).map { notificationPageResponse ->
+                            hasMore = notificationPageResponse.hasMore
+                            notificationPageResponse.notifications.forEach { notificationResponse ->
+                                notificationResponse.payload?.let { payloads ->
+                                    payloads.forEach { payload ->
+                                        trySendBlocking(eventFromPayload(payload, notificationResponse.id)
+                                        )
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
             }
         }
+
+        externalScope.launch {
+            webSocketService.receiveEvent().collect {
+                it.payload?.let { payloads ->
+                    payloads.forEach { payload ->
+                        trySendBlocking(eventFromPayload(payload, it.id))
+                    }
+                }
+            }
+        }
+
+        awaitClose { }
+    }
+
+    private fun eventFromPayload(payload: Payload, eventId: String) : Event {
+        when (payload.type) {
+            NEW_MESSAGE_TYPE -> {
+                if(payload.data != null) {
+                    return Event.Conversation.MessageEvent(
+                        eventId,
+                        payload.conversation,
+                        payload.data.sender,
+                        payload.from,
+                        payload.data.text,
+                        payload.time
+                    )
+                }
+            }
+        }
+        return Event.Unknown
+    }
+
+    //TODO this should be called in full state sync
+    private suspend fun lastNotificationId(clientId: String) : String {
+        notificationLocalDataSource.lastNotificationId()?.let {
+            return it
+        } ?: run {
+            notificationRemoteDataSource.lastNotification(clientId).map { notificationResponse ->
+                    notificationLocalDataSource.saveLastNotificationId(notificationResponse.id)
+                    return@map notificationResponse.id
+                }
+        }
+        return String.EMPTY
     }
 
     companion object {
--- app/src/main/kotlin/com/wire/android/core/events/di/EventModule.kt
+++ app/src/main/kotlin/com/wire/android/core/events/di/EventModule.kt
@@ -10,6 +10,7 @@ import com.wire.android.core.events.EventRepository
 import com.wire.android.core.events.WebSocketConfig
 import com.wire.android.core.events.adapter.FlowStreamAdapter
 import com.wire.android.core.events.datasource.EventDataSource
+import com.wire.android.core.events.datasource.local.NotificationLocalDataSource
 import com.wire.android.core.events.datasource.remote.NotificationApi
 import com.wire.android.core.events.datasource.remote.NotificationRemoteDataSource
 import com.wire.android.core.events.datasource.remote.WebSocketService
@@ -17,32 +18,37 @@ import com.wire.android.core.events.handler.EventsHandler
 import com.wire.android.core.events.handler.MessageEventsHandler
 import com.wire.android.core.events.usecase.ListenToEventsUseCase
 import com.wire.android.core.network.NetworkClient
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
 import okhttp3.OkHttpClient
 import org.koin.android.ext.koin.androidApplication
 import org.koin.dsl.module
 
 val eventModule = module {
-    fun provideWebSocketService(client: OkHttpClient, lifecycle: Lifecycle, webSocketUrl: String): WebSocketService {
+    fun provideWebSocketService(client: OkHttpClient, lifecycle: Lifecycle, webSocketUrl: String, externalScope: CoroutineScope): WebSocketService {
         val scarlet = Scarlet.Builder()
             .webSocketFactory(client.newWebSocketFactory(webSocketUrl))
             .addMessageAdapterFactory(GsonMessageAdapter.Factory())
-            .addStreamAdapterFactory(FlowStreamAdapter.Factory)
+            .addStreamAdapterFactory(FlowStreamAdapter.Factory(externalScope))
             .lifecycle(lifecycle)
             .build()
         return scarlet.create()
     }
     //TODO hardcoded client to be replaced with current clientId
-    single { WebSocketConfig("82317e19fdc80b7d") }
+    single { WebSocketConfig("8ccdab56ec2156ad") }
+    factory { CoroutineScope(Dispatchers.IO) }
     single {
         provideWebSocketService(
             get(),
             AndroidLifecycle.ofApplicationForeground(androidApplication(), get<WebSocketConfig>().throttleTimeout),
-            get<WebSocketConfig>().socketUrl
+            get<WebSocketConfig>().socketUrl,
+            get()
         )
     }
-    single { get<NetworkClient>().create(NotificationApi::class.java) }
     single { NotificationRemoteDataSource(get(), get()) }
+    single { get<NetworkClient>().create(NotificationApi::class.java) }
     single<EventsHandler<Event.Conversation.MessageEvent>> { MessageEventsHandler(get(), get()) }
-    single<EventRepository> { EventDataSource(get()) }
+    factory { NotificationLocalDataSource(get()) }
+    single<EventRepository> { EventDataSource(get(), get(), get(), get()) }
     single { ListenToEventsUseCase(get(), get()) }
 }

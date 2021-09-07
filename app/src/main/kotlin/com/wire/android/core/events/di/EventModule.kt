a78e5da5158c1d2c0963756abb88987b7cb48b47
vitorhugods <vitor@schwaab.xyz>
2021-09-07 15:02:05 -0300

    Add missing Lifecycle definition

--- app/src/main/kotlin/com/wire/android/core/events/di/EventModule.kt
+++ app/src/main/kotlin/com/wire/android/core/events/di/EventModule.kt
@@ -1,5 +1,6 @@
 package com.wire.android.core.events.di
 
+import com.tinder.scarlet.lifecycle.android.AndroidLifecycle
 import com.wire.android.core.config.GlobalConfig
 import com.wire.android.core.events.Event
 import com.wire.android.core.events.EventRepository
@@ -14,11 +15,13 @@ import com.wire.android.core.events.usecase.ListenToEventsUseCase
 import com.wire.android.core.network.NetworkClient
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
+import org.koin.android.ext.koin.androidApplication
 import org.koin.dsl.module
 
 val eventModule = module {
     single { WebSocketConfig(GlobalConfig.WEB_SOCKET_URL) }
     factory { CoroutineScope(Dispatchers.IO) }
+    factory { AndroidLifecycle.ofApplicationForeground(androidApplication(), get<WebSocketConfig>().throttleTimeout) }
     factory<WebSocketServiceProvider> { DefaultWebSocketServiceProvider(get(), get(), get()) }
     single { NotificationRemoteDataSource(get(), get(), get(), get(), get()) }
     single { get<NetworkClient>().create(NotificationApi::class.java) }

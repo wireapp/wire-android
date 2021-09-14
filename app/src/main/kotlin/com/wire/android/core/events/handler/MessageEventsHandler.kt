90c71529b756d9523cf88b8aac092554d5f7e168
Oussama Hassine <oussama.has100@gmail.com>
2021-09-14 10:25:48 +0100

    [Refactor] removed misplaced use case (#257)
    * remove shouldShowNotification usecase

--- app/src/main/kotlin/com/wire/android/core/events/handler/MessageEventsHandler.kt
+++ app/src/main/kotlin/com/wire/android/core/events/handler/MessageEventsHandler.kt
@@ -1,25 +1,21 @@
 package com.wire.android.core.events.handler
 
-import com.wire.android.core.async.DispatcherProvider
 import com.wire.android.core.events.Event
 import com.wire.android.core.functional.suspending
-import com.wire.android.core.usecase.DefaultUseCaseExecutor
-import com.wire.android.core.usecase.UseCaseExecutor
 import com.wire.android.feature.conversation.content.MessageRepository
 import com.wire.android.feature.conversation.content.mapper.MessageMapper
+import com.wire.android.feature.conversation.data.ConversationRepository
 import com.wire.android.shared.notification.builder.NotificationBuilder
 import com.wire.android.shared.notification.builder.NotificationSummaryBuilder
-import com.wire.android.shared.notification.usecase.ShouldDisplayNotificationUseCase
-import com.wire.android.shared.notification.usecase.ShouldDisplayNotificationUseCaseParams
 
 class MessageEventsHandler(
-    override val dispatcherProvider: DispatcherProvider,
-    private val shouldDisplayNotificationUseCase: ShouldDisplayNotificationUseCase,
     private val messageRepository: MessageRepository,
+    private val conversationRepository: ConversationRepository,
     private val messageMapper: MessageMapper,
     private val notificationSummaryBuilder: NotificationSummaryBuilder,
     private val notificationBuilder: NotificationBuilder
-) : EventsHandler<Event.Conversation.MessageEvent>, UseCaseExecutor by DefaultUseCaseExecutor(dispatcherProvider)  {
+) : EventsHandler<Event.Conversation.MessageEvent> {
+
     override suspend fun subscribe(event: Event.Conversation.MessageEvent) {
         val message = messageMapper.fromMessageEventToEncryptedMessageEnvelope(event)
         messageRepository.receiveEncryptedMessage(message)
@@ -27,14 +23,15 @@ class MessageEventsHandler(
     }
 
     private suspend fun displayNotification(conversationId: String) = suspending {
-        val shouldDisplayNotificationUseCaseParams = ShouldDisplayNotificationUseCaseParams(conversationId)
-        shouldDisplayNotificationUseCase.run(shouldDisplayNotificationUseCaseParams).map { shouldDisplayNotification ->
-            if(shouldDisplayNotification) {
+        conversationRepository.currentOpenedConversationId().map { currentOpenedConversationId ->
+            if(currentOpenedConversationId != conversationId) {
+                notificationSummaryBuilder.createSummaryNotification()
                 val messages = messageRepository.latestUnreadMessages(conversationId)
                 messages.map {
-                    notificationSummaryBuilder.createSummaryNotification()
-                    //TODO pass conversation name in next PR
-                    notificationBuilder.displayNotification(conversationId, "Conversation Name", it)
+                    val conversationName = conversationRepository.conversationName(conversationId)
+                    conversationName.map { name ->
+                        notificationBuilder.displayNotification(conversationId, name, it)
+                    }
                 }
             }
         }
--- app/src/main/kotlin/com/wire/android/shared/notification/di/HeadsUpNotificationModule.kt
+++ app/src/main/kotlin/com/wire/android/shared/notification/di/HeadsUpNotificationModule.kt
@@ -3,12 +3,10 @@ package com.wire.android.shared.notification.di
 import com.wire.android.shared.notification.builder.NotificationBuilder
 import com.wire.android.shared.notification.builder.NotificationChannelBuilder
 import com.wire.android.shared.notification.builder.NotificationSummaryBuilder
-import com.wire.android.shared.notification.usecase.ShouldDisplayNotificationUseCase
 import org.koin.android.ext.koin.androidContext
 import org.koin.dsl.module
 
 val headsUpNotificationModule = module {
-    factory { ShouldDisplayNotificationUseCase(get()) }
     single { NotificationBuilder(androidContext(), get(), get()) }
     single { NotificationChannelBuilder(androidContext()) }
     single { NotificationSummaryBuilder(androidContext()) }

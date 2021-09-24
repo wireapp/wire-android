fe0d2e3fdd0cc4cd699112b3d2793a509bf0477f
vitorhugods <vitor@schwaab.xyz>
2021-09-24 14:50:27 -0300

    Fix CryptoSessionId toString

--- app/src/main/kotlin/com/wire/android/core/crypto/model/CryptoSessionId.kt
+++ app/src/main/kotlin/com/wire/android/core/crypto/model/CryptoSessionId.kt
@@ -3,5 +3,6 @@ package com.wire.android.core.crypto.model
 import com.wire.android.shared.user.QualifiedId
 
 data class CryptoSessionId(val userId: QualifiedId, val cryptoClientId: CryptoClientId) {
-    val value: String = "${userId}_${cryptoClientId}"
+    //TODO Take domain into consideration here too
+    val value: String = "${userId.id}_${cryptoClientId}"
 }

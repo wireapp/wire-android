06d8e5921ae1c53affe6153448885e983d1eccc4
Vitor Hugo Schwaab <vitor@schwaab.xyz>
2021-10-04 08:59:47 +0200

    refactor: Provide more control over requests results and response handling for ApiServices

--- app/src/main/kotlin/com/wire/android/core/network/ApiService.kt
+++ app/src/main/kotlin/com/wire/android/core/network/ApiService.kt
@@ -1,19 +1,8 @@
 package com.wire.android.core.network
 
-import com.wire.android.core.exception.BadRequest
-import com.wire.android.core.exception.Cancelled
-import com.wire.android.core.exception.Conflict
-import com.wire.android.core.exception.EmptyResponseBody
-import com.wire.android.core.exception.Failure
-import com.wire.android.core.exception.Forbidden
-import com.wire.android.core.exception.InternalServerError
-import com.wire.android.core.exception.NetworkConnection
-import com.wire.android.core.exception.NotFound
-import com.wire.android.core.exception.ServerError
-import com.wire.android.core.exception.TooManyRequests
-import com.wire.android.core.exception.Unauthorized
+import com.wire.android.core.exception.*
 import com.wire.android.core.functional.Either
-import com.wire.android.core.functional.flatMap
+import com.wire.android.core.functional.suspending
 import kotlinx.coroutines.CancellationException
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
@@ -22,29 +11,42 @@ import retrofit2.Response
 abstract class ApiService {
     abstract val networkHandler: NetworkHandler
 
-    suspend fun <T> rawRequest(call: suspend () -> Response<T>): Either<Failure, Response<T>> =
-        withContext(Dispatchers.IO) {
-            return@withContext when (networkHandler.isConnected()) {
-                true -> performRequest(call)
-                false -> Either.Left(NetworkConnection)
-            }
-        }
-
-    suspend fun <T> request(default: T? = null, call: suspend () -> Response<T>): Either<Failure, T> =
-        rawRequest(call).flatMap { response ->
-            response.body()?.let { Either.Right(it) }
-                ?: default?.let { Either.Right(it) }
-                ?: Either.Left(EmptyResponseBody)
-        }
-
-    @Suppress("TooGenericExceptionCaught")
-    private suspend fun <T> performRequest(call: suspend () -> Response<T>): Either<Failure, Response<T>> {
-        return try {
-            val response = call()
+    suspend fun <T> rawRequest(
+        onResponseError: (suspend (response: Response<T>) -> Either<Failure, Response<T>>)? = null,
+        call: suspend () -> Response<T>
+    ): Either<Failure, Response<T>> = suspending {
+        performRequest(call).flatMap { response ->
             if (response.isSuccessful) {
                 Either.Right(response)
             } else {
+                onResponseError?.invoke(response)
+                    ?: handleRequestError(response)
+            }
+        }
+    }
+
+    suspend fun <T> request(
+        handleEmptyBody: (suspend (response: Response<T>) -> Either<Failure, T>)? = null,
+        call: suspend () -> Response<T>
+    ): Either<Failure, T> = suspending {
+        performRequest(call).flatMap { response ->
+            if (response.isSuccessful) {
+                response.body()?.let { Either.Right(it) }
+                    ?: handleEmptyBody?.invoke(response)
+                    ?: Either.Left(EmptyResponseBody)
+            } else {
                 handleRequestError(response)
+            }
+        }
+    }
+
+    @Suppress("TooGenericExceptionCaught")
+    private suspend fun <T> performRequest(call: suspend () -> Response<T>): Either<Failure, Response<T>> = withContext(Dispatchers.IO) {
+        try {
+            if (!networkHandler.isConnected()) {
+                Either.Left(NetworkConnection)
+            } else {
+                Either.Right(call())
             }
         } catch (exception: Throwable) {
             when (exception) {
@@ -54,7 +56,7 @@ abstract class ApiService {
         }
     }
 
-    private fun <T> handleRequestError(response: Response<T>): Either<Failure, Response<T>> =
+    private fun <T> handleRequestError(response: Response<T>): Either.Left<Failure> =
         Either.Left(buildFailure(response.code()))
 
     private fun buildFailure(errorCode: Int): Failure =

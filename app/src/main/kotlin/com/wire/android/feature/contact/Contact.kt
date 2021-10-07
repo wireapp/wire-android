e4604f2fd9fd4babb0e698df776a2e7ec7e4565f
Vitor Hugo Schwaab <vitor@schwaab.xyz>
2021-10-07 09:24:20 +0200

    refactor: make `Contact` a data class

--- app/src/main/kotlin/com/wire/android/feature/contact/Contact.kt
+++ app/src/main/kotlin/com/wire/android/feature/contact/Contact.kt
@@ -2,7 +2,7 @@ package com.wire.android.feature.contact
 
 import com.wire.android.shared.asset.Asset
 
-class Contact(val id: String, val name: String, val profilePicture: Asset?)
+data class Contact(val id: String, val name: String, val profilePicture: Asset?)
 
 data class ContactClient(val id: String)
 

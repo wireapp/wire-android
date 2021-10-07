e5999ca5d1189830b79d9fb194ab4b284b67e055
Vitor Hugo Schwaab <vitor@schwaab.xyz>
2021-10-07 02:06:23 +0200

    feat: add a way to fetch preKeys of users without qualified id
    By using the deprecated endpoints

--- app/src/main/kotlin/com/wire/android/shared/prekey/data/remote/PreKeyResponse.kt
+++ app/src/main/kotlin/com/wire/android/shared/prekey/data/remote/PreKeyResponse.kt
@@ -2,7 +2,13 @@ package com.wire.android.shared.prekey.data.remote
 
 import com.google.gson.annotations.SerializedName
 
-typealias PreKeyListResponse = Map<String, Map<String, Map<String, PreKeyResponse>>>
+typealias QualifiedPreKeyListResponse = Map<String, Map<String, Map<String, PreKeyResponse>>>
+
+@Deprecated(
+        "This data structure does not consider domain, needed for Federation",
+        ReplaceWith("QualifiedPreKeyListResponse")
+)
+typealias PreKeyListResponse = Map<String, Map<String, PreKeyResponse>>
 
 data class PreKeyResponse(
     @SerializedName("key")

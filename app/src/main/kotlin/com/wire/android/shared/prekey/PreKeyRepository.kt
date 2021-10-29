8050d124d83fcf3cbaa183887921994e6738f505
vitorhugods <vitor@schwaab.xyz>
2021-10-29 14:20:43 +0200

    style: split PreKeyRepository function declarations to avoid maxLineLength

--- app/src/main/kotlin/com/wire/android/shared/prekey/PreKeyRepository.kt
+++ app/src/main/kotlin/com/wire/android/shared/prekey/PreKeyRepository.kt
@@ -11,12 +11,16 @@ interface PreKeyRepository {
      * Given a map of qualified user IDs to client IDs, fetches one PreKey for each client.
      * @param qualifiedIdsMap, a map of qualified user IDs to the wanted client IDs
      */
-    suspend fun preKeysOfClientsByQualifiedUsers(qualifiedIdsMap: Map<QualifiedId, List<String>>): Either<Failure, List<QualifiedUserPreKeyInfo>>
+    suspend fun preKeysOfClientsByQualifiedUsers(
+        qualifiedIdsMap: Map<QualifiedId, List<String>>
+    ): Either<Failure, List<QualifiedUserPreKeyInfo>>
 
     /**
      * Given a map of user IDs to client IDs, fetches one PreKey for each client.
      * @param contactIdsMap, a map of user IDs to the wanted client IDs
      */
     @Deprecated("Qualified ID should be used", ReplaceWith("preKeysOfClientsByQualifiedUsers"))
-    suspend fun preKeysOfClientsByUsers(contactIdsMap: Map<String, List<String>>): Either<Failure, List<UserPreKeyInfo>>
+    suspend fun preKeysOfClientsByUsers(
+        contactIdsMap: Map<String, List<String>>
+    ): Either<Failure, List<UserPreKeyInfo>>
 }

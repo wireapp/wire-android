6bc53dee3d7d26cb4d94c0aa2a6d182469b93933
Michał Saleniuk <saleniuk@gmail.com>
2022-01-17 13:03:06 +0100

    MainActivity clean-up

--- app/src/main/kotlin/com/wire/android/ui/WireApp.kt
+++ app/src/main/kotlin/com/wire/android/ui/WireApp.kt
@@ -1,11 +1,12 @@
 package com.wire.android.ui
 
 import androidx.compose.runtime.Composable
+import com.wire.android.ui.main.MainScreen
 import com.wire.android.ui.welcome.WelcomeScreen
 
-// Here could be an entry point for navigation part, for now I just redirect to ConversationScreen
+// Here could be an entry point for the app navigation
 @Composable
 fun WireApp() {
-    WelcomeScreen()
+    MainScreen()
 }
 

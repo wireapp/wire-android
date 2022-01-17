763f6290bb71580171b902f0975889b1b26614e9
Boris Safonov <boris.safonov@appunite.com>
2022-01-17 10:08:03 +0200

    feat: add main navigation drawer: removed tmp code

--- app/src/main/kotlin/com/wire/android/ui/main/MainActivity.kt
+++ app/src/main/kotlin/com/wire/android/ui/main/MainActivity.kt
@@ -3,21 +3,18 @@ package com.wire.android.ui.main
 import android.os.Bundle
 import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.foundation.layout.ColumnScope
 import androidx.compose.material.DrawerValue
 import androidx.compose.material.Scaffold
-import androidx.compose.material.ScaffoldState
 import androidx.compose.material.rememberDrawerState
 import androidx.compose.material.rememberScaffoldState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.rememberCoroutineScope
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
-import com.wire.android.ui.theme.WireLightColors
-import kotlinx.coroutines.CoroutineScope
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -33,22 +30,15 @@ fun MainScreen() {
     val scaffoldState = rememberScaffoldState(rememberDrawerState(DrawerValue.Closed))
     val scope = rememberCoroutineScope()
     val navController = rememberNavController()
-    testing(scaffoldState, scope, navController)
-}
+    val topBar: @Composable () -> Unit = { MainTopBar(scope = scope, scaffoldState = scaffoldState, navController = navController) }
+    val drawerContent: @Composable (ColumnScope.() -> Unit) = {
+        Drawer(scope = scope, scaffoldState = scaffoldState, navController = navController)
+    }
 
-@Composable
-private fun testing(
-    scaffoldState: ScaffoldState,
-    scope: CoroutineScope,
-    navController: NavHostController
-) {
-    val topBar : @Composable () -> Unit = { MainTopBar(scope = scope, scaffoldState = scaffoldState, navController = navController) }
     Scaffold(
         scaffoldState = scaffoldState,
         topBar = topBar,
-        drawerContent = {
-            Drawer(scope = scope, scaffoldState = scaffoldState, navController = navController)
-        },
+        drawerContent = drawerContent,
     ) {
         Navigation(navController = navController)
     }

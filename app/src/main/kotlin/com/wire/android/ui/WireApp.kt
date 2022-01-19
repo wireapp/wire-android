89d251f8fd2aad79e4fac65bac6c6048dd5a889d
Gonzalo Gran Crespo <gongracr@gmail.com>
2022-01-19 09:08:26 +0100

    Fixed issues with tests

--- app/src/main/kotlin/com/wire/android/ui/WireApp.kt
+++ app/src/main/kotlin/com/wire/android/ui/WireApp.kt
@@ -1,10 +1,14 @@
 package com.wire.android.ui
 
+import androidx.compose.animation.ExperimentalAnimationApi
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.ExperimentalComposeUiApi
 import com.wire.android.ui.main.MainScreen
 import com.wire.android.ui.welcome.WelcomeScreen
 
 // Here could be an entry point for the app navigation
+@ExperimentalAnimationApi
+@ExperimentalComposeUiApi
 @Composable
 fun WireApp() {
     MainScreen()

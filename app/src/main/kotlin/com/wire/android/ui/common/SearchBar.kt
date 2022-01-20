7b36872a1844f3a3a8ba1d98cce590fbe530395e
Gonzalo Gran Crespo <gongracr@gmail.com>
2022-01-20 09:18:44 +0100

    Center align placeholder

--- app/src/main/kotlin/com/wire/android/ui/common/SearchBar.kt
+++ app/src/main/kotlin/com/wire/android/ui/common/SearchBar.kt
@@ -15,6 +15,7 @@ import androidx.compose.material.Icon
 import androidx.compose.material.IconButton
 import androidx.compose.material.LocalContentAlpha
 import androidx.compose.material.LocalContentColor
+import androidx.compose.material.LocalTextStyle
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.OutlinedTextField
 import androidx.compose.material.Text
@@ -32,6 +33,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.Color.Companion.Transparent
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.wire.android.R
@@ -79,7 +81,13 @@ fun SearchBarUI(placeholderText: String, modifier: Modifier = Modifier, onTextTy
             }
         },
 
-        placeholder = { Text(text = placeholderText) },
+        placeholder = {
+            Text(
+                text = placeholderText,
+                modifier = Modifier.fillMaxWidth(),
+                style = LocalTextStyle.current.copy(textAlign = TextAlign.Center)
+            )
+        },
         colors = TextFieldDefaults.textFieldColors(
             focusedIndicatorColor = Transparent,
             unfocusedIndicatorColor = Transparent,

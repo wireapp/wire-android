e264b18ae6209025d4ff1d3358ad902ae67af542
mateusz.pachulski <mateusz.pachulski@appunite.com>
2022-01-21 16:05:30 +0100

    use MainScreen as the main entry

--- app/src/main/kotlin/com/wire/android/ui/WireApp.kt
+++ app/src/main/kotlin/com/wire/android/ui/WireApp.kt
@@ -3,9 +3,6 @@ package com.wire.android.ui
 import androidx.compose.animation.ExperimentalAnimationApi
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.ExperimentalComposeUiApi
-import com.wire.android.ui.conversation.all.Conversation
-import com.wire.android.ui.conversation.call.Call
-import com.wire.android.ui.conversation.mention.Mention
 import com.wire.android.ui.main.MainScreen
 
 // Here could be an entry point for the app navigation
@@ -13,6 +10,6 @@ import com.wire.android.ui.main.MainScreen
 @ExperimentalComposeUiApi
 @Composable
 fun WireApp() {
-    Conversation()
+    MainScreen()
 }
 

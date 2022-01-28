6f46d9a22dbbe36193cc1113066ceee29a0b7a75
boris <safonovboris@gmail.com>
2022-01-28 18:48:15 +0200

    feat: Close drawer on back click (#339)
    Co-authored-by: Boris Safonov <boris.safonov@appunite.com>

--- app/src/main/kotlin/com/wire/android/ui/home/HomeScreen.kt
+++ app/src/main/kotlin/com/wire/android/ui/home/HomeScreen.kt
@@ -1,5 +1,6 @@
 package com.wire.android.ui.home
 
+import androidx.activity.compose.BackHandler
 import androidx.compose.foundation.layout.ColumnScope
 import androidx.compose.material3.DrawerValue
 import androidx.compose.material3.ExperimentalMaterial3Api
@@ -7,10 +8,12 @@ import androidx.compose.material3.NavigationDrawer
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.rememberDrawerState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.unit.dp
 import androidx.navigation.compose.rememberNavController
+import kotlinx.coroutines.launch
 
 @ExperimentalMaterial3Api
 @Composable
@@ -18,13 +21,16 @@ fun HomeScreen(startScreen: String?, viewModel: HomeViewModel) {
     val navController = rememberNavController()
     val drawerState = rememberDrawerState(DrawerValue.Closed)
     val currentItem = HomeNavigationItem.getCurrentNavigationItem(navController)
+    val scope = rememberCoroutineScope()
 
     val topBar: @Composable () -> Unit = {
-        HomeTopBar(currentItem.title, currentItem.isSearchable, drawerState, viewModel)
+        HomeTopBar(currentItem.title, currentItem.isSearchable, drawerState, scope, viewModel)
     }
     val drawerContent: @Composable ColumnScope.() -> Unit = {
-        HomeDrawer(drawerState, currentItem.route, navController, HomeNavigationItem.all, viewModel)
+        HomeDrawer(drawerState, currentItem.route, navController, HomeNavigationItem.all, scope, viewModel)
     }
+
+    BackHandler(enabled = drawerState.isOpen) { scope.launch { drawerState.close() } }
 
     NavigationDrawer(
         drawerContainerColor = Color.White,

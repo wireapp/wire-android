c92b67afa767f4767ad82f9fc0d3c5168a31c682
Gonzalo Gran Crespo <gongracr@gmail.com>
2022-02-08 17:31:57 +0100

    fix: update search bar icons with custom ones

--- app/src/main/kotlin/com/wire/android/ui/common/SearchBar.kt
+++ app/src/main/kotlin/com/wire/android/ui/common/SearchBar.kt
@@ -13,12 +13,14 @@ import androidx.compose.material.icons.filled.Search
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.LocalTextStyle
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.text.style.TextAlign
@@ -47,8 +49,9 @@ fun SearchBarUI(placeholderText: String, modifier: Modifier = Modifier, onTextTy
         leadingIcon = {
             IconButton(modifier = Modifier.height(40.dp), onClick = {}) {
                 Icon(
-                    Icons.Filled.Search,
-                    stringResource(R.string.content_description_clear_content)
+                    painter = painterResource(id = R.drawable.ic_search_icon),
+                    contentDescription = stringResource(R.string.content_description_conversation_search_icon),
+                    tint = MaterialTheme.colorScheme.onBackground
                 )
             }
         },
@@ -65,7 +68,7 @@ fun SearchBarUI(placeholderText: String, modifier: Modifier = Modifier, onTextTy
                     }) {
                         Icon(
                             modifier = Modifier.height(35.dp),
-                            imageVector = Icons.Filled.Close,
+                            painter = painterResource(id = R.drawable.ic_clear_search),
                             contentDescription = stringResource(R.string.content_description_clear_content)
                         )
                     }

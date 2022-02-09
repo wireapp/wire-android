1ddb107da777634c57b83bfc7cf4fae0473500ea
mateusz.pachulski <mateusz.pachulski@appunite.com>
2022-02-09 11:48:32 +0100

    WIP: animate addiotnal options

--- app/src/main/kotlin/com/wire/android/ui/home/messagecomposer/MessageComposer.kt
+++ app/src/main/kotlin/com/wire/android/ui/home/messagecomposer/MessageComposer.kt
@@ -19,6 +19,7 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.heightIn
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.layout.wrapContentHeight
@@ -58,6 +59,9 @@ fun rememberMessageComposerState(
         defaultMessageText,
         defaultMessageComposeInputState
     )
+
+
+    Modifier.padding(hori)
 }
 
 @OptIn(ExperimentalMaterialApi::class)

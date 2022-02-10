094273ab0173aad9b22114232f6610443e58e7d8
Boris Safonov <boris.safonov@appunite.com>
2022-02-10 16:02:58 +0200

    feat: Make home top bar scrollable: fixed build

--- app/src/main/kotlin/com/wire/android/ui/home/HomeTopBar.kt
+++ app/src/main/kotlin/com/wire/android/ui/home/HomeTopBar.kt
@@ -8,7 +8,15 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Menu
-import androidx.compose.material3.*
+import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.DrawerState
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.livedata.observeAsState

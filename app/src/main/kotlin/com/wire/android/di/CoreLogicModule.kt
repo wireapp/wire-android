9bfd18f70c51648422b4795e4d767e334b51885c
Mohamad Jaara <mohamad.jaara@wire.com>
2022-02-15 12:19:12 +0000

    feat: injecting current session with hilt (#379)
    * feat: injecting current session with hilt
    
    * update kalium reference
    
    * add TODO

--- app/src/main/kotlin/com/wire/android/di/CoreLogicModule.kt
+++ app/src/main/kotlin/com/wire/android/di/CoreLogicModule.kt
@@ -12,11 +12,19 @@ import dagger.hilt.android.components.ViewModelComponent
 import dagger.hilt.android.scopes.ViewModelScoped
 import javax.inject.Qualifier
 import javax.inject.Singleton
-
+import com.wire.kalium.logic.feature.auth.AuthSession
+import com.wire.kalium.logic.feature.session.CurrentSessionResult
+import dagger.hilt.android.scopes.ActivityRetainedScoped
+import kotlinx.coroutines.runBlocking
+import java.lang.IllegalStateException
 
 @Qualifier
 @Retention(AnnotationRetention.BINARY)
 annotation class KaliumCoreLogic
+
+@Qualifier
+@Retention(AnnotationRetention.BINARY)
+annotation class CurrentSession
 
 @Module
 @InstallIn(SingletonComponent::class)
@@ -35,7 +43,29 @@ class CoreLogicModule {
 @Module
 @InstallIn(ViewModelComponent::class)
 class UseCaseModule {
+
+    @CurrentSession
+    @ViewModelScoped
+    @Provides
+    // TODO: can be improved by caching the current session in kalium or changing the scope to ActivityRetainedScoped
+    fun currentSessionProvider(@KaliumCoreLogic coreLogic: CoreLogic): AuthSession {
+        return runBlocking {
+            return@runBlocking when(val result = coreLogic.getAuthenticationScope().session.currentSession.invoke()) {
+                is CurrentSessionResult.Success -> result.authSession
+                else -> {throw IllegalStateException("no current session where found")}
+            }
+        }
+    }
+
     @ViewModelScoped
     @Provides
     fun loginUseCaseProvider(@KaliumCoreLogic coreLogic: CoreLogic) = coreLogic.getAuthenticationScope().login
+
+    @ViewModelScoped
+    @Provides
+    fun registerClient(
+        @KaliumCoreLogic coreLogic: CoreLogic,
+        @CurrentSession currentSession: AuthSession
+    ) = coreLogic.getSessionScope(currentSession).client.register
+
 }

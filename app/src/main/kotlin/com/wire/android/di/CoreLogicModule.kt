bac418703216a2ec50c7f1c049b9efae8a495c61
boris <safonovboris@gmail.com>
2022-02-23 17:35:34 +0200

    feat: add get messages usecase from kalium (AR-970) (#397)
    * feat: Add get Conversations UseCase from kalium
    
    * feat: Add get Conversations UseCase from kalium: review 1
    
    * feat: Add get Messages UseCase from kalium
    
    * feat: Add get Messages UseCase from kalium: need to merge develop
    
    * feat: Add get Messages UseCase from kalium: ready for review
    
    * feat: Add get Messages UseCase from kalium: self review
    
    * feat: Add get Messages UseCase from kalium: review 1
    
    * feat: Add get Messages UseCase from kalium: review 2
    
    Co-authored-by: Boris Safonov <boris.safonov@appunite.com>

--- app/src/main/kotlin/com/wire/android/di/CoreLogicModule.kt
+++ app/src/main/kotlin/com/wire/android/di/CoreLogicModule.kt
@@ -3,9 +3,9 @@ package com.wire.android.di
 import android.content.Context
 import com.wire.android.util.DeviceLabel
 import com.wire.kalium.logic.CoreLogic
-import com.wire.kalium.logic.feature.user.UploadUserAvatarUseCase
 import com.wire.kalium.logic.feature.auth.AuthSession
 import com.wire.kalium.logic.feature.session.CurrentSessionResult
+import com.wire.kalium.logic.feature.user.UploadUserAvatarUseCase
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
@@ -87,6 +87,16 @@ class UseCaseModule {
 
     @ViewModelScoped
     @Provides
+    fun getConversationDetailsUseCaseProvider(@KaliumCoreLogic coreLogic: CoreLogic, @CurrentSession session: AuthSession) =
+        coreLogic.getSessionScope(session).conversations.getConversationDetails
+
+    @ViewModelScoped
+    @Provides
+    fun getMessagesUseCaseProvider(@KaliumCoreLogic coreLogic: CoreLogic, @CurrentSession session: AuthSession) =
+        coreLogic.getSessionScope(session).messages.getRecentMessages
+
+    @ViewModelScoped
+    @Provides
     fun deleteClientUseCase(@CurrentSession currentSession: AuthSession, clientScopeProviderFactory: ClientScopeProvider.Factory) =
         clientScopeProviderFactory.create(currentSession).clientScope.deleteClient
 
@@ -94,4 +104,9 @@ class UseCaseModule {
     @Provides
     fun registerClientUseCase(@CurrentSession currentSession: AuthSession, clientScopeProviderFactory: ClientScopeProvider.Factory) =
         clientScopeProviderFactory.create(currentSession).clientScope.register
+
+    @ViewModelScoped
+    @Provides
+    fun listenToEventsUseCaseProvider(@KaliumCoreLogic coreLogic: CoreLogic, @CurrentSession session: AuthSession) =
+        coreLogic.getSessionScope(session).listenToEvents
 }

61b2a3b90758fb645f72764c80c96e144843a8d5
yamilmedina <yamilmedina@users.noreply.github.com>
2022-03-01 16:18:38 -0300

    chore: fix detekt issue

--- app/src/androidTest/java/com/wire/android/utils/Extensions.kt
+++ app/src/androidTest/java/com/wire/android/utils/Extensions.kt
@@ -10,7 +10,8 @@ import org.junit.rules.TestRule
 /**
  * Convenience function to obtain a ViewModel instance from the AndroidComposeTestRule
  */
-inline fun <reified VM : ViewModel, R : TestRule, A : ComponentActivity> AndroidComposeTestRule<R, A>.getViewModel(viewModel: KClass<VM>): VM { // ktlint-disable max-line-length
+inline fun <reified VM : ViewModel, R : TestRule, A : ComponentActivity>
+AndroidComposeTestRule<R, A>.getViewModel(viewModel: KClass<VM>): VM {
     println("Getting test instance of ViewModel: $viewModel")
     return this.activity.viewModels<VM>().value
 }

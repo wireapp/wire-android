eebfb96203b14e2194249dab09f0995a2fb9ef85
mateusz.pachulski <mateusz.pachulski@appunite.com>
2022-03-03 16:10:00 +0100

    correctly align searchbar

--- app/src/main/kotlin/com/wire/android/ui/home/newconversation/NewConversation.kt
+++ app/src/main/kotlin/com/wire/android/ui/home/newconversation/NewConversation.kt
@@ -5,12 +5,14 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.IntrinsicSize
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.foundation.layout.wrapContentWidth
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.material3.Divider
@@ -24,6 +26,7 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.runtime.snapshotFlow
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalDensity
@@ -35,11 +38,9 @@ import androidx.hilt.navigation.compose.hiltViewModel
 import com.wire.android.R
 import com.wire.android.model.UserStatus
 import com.wire.android.ui.common.NavigableSearchBar
-import com.wire.android.ui.common.NavigationIconType
 import com.wire.android.ui.common.UserProfileAvatar
 import com.wire.android.ui.common.dimensions
 import com.wire.android.ui.common.textfield.WirePrimaryButton
-import com.wire.android.ui.common.topappbar.WireCenterAlignedTopAppBar
 import com.wire.android.ui.home.conversationslist.common.RowItem
 import com.wire.android.ui.home.conversationslist.folderWithElements
 import com.wire.android.ui.theme.wireTypography
@@ -92,14 +93,17 @@ fun NewConversationContent(
         }
 
         val searchFieldFullHeightPx = LocalDensity.current.run {
-            (dimensions().topBarSearchFieldHeight + dimensions().topBarElevationHeight).toPx()
+            (dimensions().topBarSearchFieldHeight).toPx()
         }
 
-        val searchFieldPosition by animateFloatAsState(if (isCollapsed) -searchFieldFullHeightPx else 0f)
+        val searchFieldPosition by animateFloatAsState(if (isCollapsed) 0f else searchFieldFullHeightPx)
 
-        Box(modifier = Modifier.constrainAs(topBar) {
-            top.linkTo(parent.top)
-        }) {
+        Box(modifier = Modifier
+            .constrainAs(topBar) {
+                top.linkTo(parent.top)
+                bottom.linkTo(content.top)
+            }
+            .wrapContentSize()) {
             Test(searchFieldPosition)
         }
 
@@ -191,45 +195,53 @@ fun NewConversationContent(
     }
 }
 
-
 @Composable
 fun Test(searchFieldPosition: Float) {
     Box(
         Modifier
-            .wrapContentSize()
-            .background(Color.Red)
+            .fillMaxWidth()
+            .height(IntrinsicSize.Min)
     ) {
         Surface(
             modifier = Modifier
-                .height(searchFieldPosition.dp),
+                .height(searchFieldPosition.dp)
+                .background(Color.Yellow)
+                .wrapContentWidth(),
             shadowElevation = dimensions().topBarElevationHeight
         ) {
             val interactionSource = remember {
                 MutableInteractionSource()
             }
 
+            Box(
+                Modifier
+                    .fillMaxSize()
+                    .background(Color.Yellow)
+            ) {
+                NavigableSearchBar(
+                    placeholderText = "Search people",
+                    onNavigateBack = { },
+                    interactionSource = interactionSource,
+                    modifier = Modifier
+                        .align(Alignment.BottomCenter)
+                )
+            }
+
 //            if (interactionSource.collectIsPressedAsState().value) {
 //                isTopBarVisible = !isTopBarVisible
 //            }
 
-            NavigableSearchBar(
-                placeholderText = "Search people",
-                onNavigateBack = { },
-                interactionSource = interactionSource
-            )
         }
-
 //        AnimatedVisibility(visible = isTopBarVisible) {
-        WireCenterAlignedTopAppBar(
-            elevation = 0.dp,
-            title = stringResource(R.string.label_new_conversation),
-            navigationIconType = NavigationIconType.Close,
-            onNavigationPressed = { }
-        )
+//        WireCenterAlignedTopAppBar(
+//            elevation = 0.dp,
+//            title = stringResource(R.string.label_new_conversation),
+//            navigationIconType = NavigationIconType.Close,
+//            onNavigationPressed = { }
+//        )
 //        }
     }
 }
-
 
 @Composable
 private fun ContactItem(

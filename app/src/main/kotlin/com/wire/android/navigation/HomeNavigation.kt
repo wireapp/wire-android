951ae2f8526e733ccda6a8954948ad2e1793b46d
yamilmedina <yamilmedina@users.noreply.github.com>
2022-03-07 17:25:27 -0300

    feat: adjust animations for self profile

--- app/src/main/kotlin/com/wire/android/navigation/HomeNavigation.kt
+++ app/src/main/kotlin/com/wire/android/navigation/HomeNavigation.kt
@@ -43,9 +43,7 @@ fun HomeNavigationGraph(homeState: HomeState, navController: NavHostController, 
                     route = item.route,
                     content = item.content(homeState),
                     enterTransition = { item.enterTransition },
-                    exitTransition = { item.exitTransition },
-                    popEnterTransition = { item.enterTransition }, // should create a structure to hold the animations per item ?
-                    popExitTransition = { item.exitTransition } // should create a structure to hold the animations per item ?
+                    exitTransition = { item.exitTransition }
                 )
             }
     }

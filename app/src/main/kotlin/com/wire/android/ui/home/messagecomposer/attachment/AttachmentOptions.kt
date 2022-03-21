2a22301c67d27af3f24c1161e970ddb8bebe2810
Yamil Medina <yamilmedina@users.noreply.github.com>
2022-03-21 07:33:12 -0300

    feat: add msg sharing options ui (AR-1086) (#438)
    * feat: add base structure and attach component button
    
    * feat: refactor move attachment components to dedicated package
    
    * feat: extract strings and add share location menu
    
    * chore: update kalium ref
    
    * chore: extract values to dimens and simplify creation of items
    
    * chore: add animation to attachment button
    
    * chore: add animation to attachment button
    
    * feat: adjust dimens and content desc
    
    * feat: adjust dimens
    
    * feat: extract animation
    
    * feat: extract message compose creation
    
    * feat: fullscreen state when attach options clicked
    
    Co-authored-by: Vitor Hugo Schwaab <vitor@schwaab.xyz>

---
+++ app/src/main/kotlin/com/wire/android/ui/home/messagecomposer/attachment/AttachmentOptions.kt
@@ -0,0 +1,77 @@
+package com.wire.android.ui.home.messagecomposer.attachment
+
+import androidx.annotation.DrawableRes
+import androidx.annotation.StringRes
+import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.lazy.GridCells
+import androidx.compose.foundation.lazy.LazyVerticalGrid
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.ExperimentalComposeUiApi
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import com.wire.android.R
+import com.wire.android.ui.common.AttachmentButton
+import com.wire.android.ui.common.dimensions
+
+@OptIn(
+    ExperimentalComposeUiApi::class,
+    ExperimentalFoundationApi::class
+)
+@Composable
+fun AttachmentOptionsComponent() {
+    val attachmentOptions = buildAttachmentOptionItems()
+    LazyVerticalGrid(
+        cells = GridCells.Adaptive(dimensions().spacing80x),
+        contentPadding = PaddingValues(dimensions().spacing8x),
+        modifier = Modifier.fillMaxWidth(),
+        verticalArrangement = Arrangement.SpaceEvenly,
+        horizontalArrangement = Arrangement.SpaceEvenly,
+    ) {
+        attachmentOptions.forEach { option ->
+            item {
+                AttachmentButton(
+                    stringResource(option.text), option.icon
+                ) {
+                    option.onClick()
+                }
+            }
+        }
+    }
+}
+
+private fun buildAttachmentOptionItems() = listOf(
+    AttachmentOptionItem(R.string.attachment_share_file, R.drawable.ic_attach_file) {
+        // TODO: implement attach file options
+    },
+    AttachmentOptionItem(R.string.attachment_share_image, R.drawable.ic_gallery) {
+        // TODO: implement attach image options
+    },
+    AttachmentOptionItem(R.string.attachment_take_photo, R.drawable.ic_camera) {
+        // TODO: implement take photo options
+    },
+    AttachmentOptionItem(R.string.attachment_record_video, R.drawable.ic_video_icon) {
+        // TODO: implement record video options
+    },
+    AttachmentOptionItem(R.string.attachment_voice_message, R.drawable.ic_mic_on) {
+        // TODO: implement voice message options
+    },
+    AttachmentOptionItem(R.string.attachment_share_location, R.drawable.ic_location) {
+        // TODO: implement share location options
+    }
+)
+
+private data class AttachmentOptionItem(
+    @StringRes val text: Int,
+    @DrawableRes val icon: Int,
+    val onClick: () -> Unit
+)
+
+@Preview(showBackground = true)
+@Composable
+fun PreviewAttachmentComponents() {
+    AttachmentOptionsComponent()
+}

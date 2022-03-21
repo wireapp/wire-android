b695c4bfb2cd9bd00c23310482d75540541ce01b
Yamil Medina <yamilmedina@users.noreply.github.com>
2022-03-21 10:36:37 -0300

    chore: add spotless tooling for style and formatting (#411)
    * chore: add base plugin spotless for kotlin, kts and xml files
    
    * chore: move plugin to internal plugins
    
    * chore: add spotless header config
    
    * chore: update from develop
    
    * chore: fix max lines accordingly to team doc
    
    * chore: add optional githook, as a precommit helper to autoformat
    
    * chore: add step to check on ci for bad style
    
    * chore: remove hard check on ci

---
+++ buildSrc/src/main/kotlin/scripts/spotless.gradle.kts
@@ -0,0 +1,25 @@
+package scripts
+
+apply(plugin = "com.diffplug.spotless")
+
+configure<com.diffplug.gradle.spotless.SpotlessExtension> {
+    kotlin {
+        target("**/*.kt")
+        ktlint().userData(
+            mapOf(
+                "disabled_rules" to "import-ordering",
+                "max_line_length" to "140"
+            )
+        )
+        trimTrailingWhitespace()
+        licenseHeaderFile("$rootDir/buildSrc/src/main/kotlin/spotless/class.license")
+    }
+    kotlinGradle {
+        target("*.gradle.kts")
+        ktlint()
+    }
+    format("xml") {
+        target("**/*.xml")
+        trimTrailingWhitespace()
+    }
+}

599273bbda4604fe90964077507113711e36a7a8
yamilmedina <yamilmedina@users.noreply.github.com>
2022-03-24 16:03:45 -0300

    chore: cov for dateutils

--- app/src/test/kotlin/com/wire/android/util/DateTimeUtilKtTest.kt
+++ app/src/test/kotlin/com/wire/android/util/DateTimeUtilKtTest.kt
@@ -10,4 +10,11 @@ class DateTimeUtilKtTest {
         val result = "NOT_VALID".formatMediumDateTime()
         assertEquals(null, result)
     }
+
+    @Test
+    fun `given an valid date, when performing a transformation, then return with medium format`() {
+        val result = "2022-03-24T18:02:30.360Z".formatMediumDateTime()
+
+        assertEquals("Mar 24, 2022, 6:02:30 PM", result)
+    }
 }

01e0c16de3df3a50500074d79a4c36a8aed82fe2
Mohamad Jaara <mohamad.jaara@wire.com>
2022-03-25 15:14:55 +0000

    feat: token refresh (#446)
    Co-authored-by: Alexandre Ferris <ferris.alexandre@gmail.com>
    Co-authored-by: Vitor Hugo Schwaab <vitor@schwaab.xyz>

--- app/src/main/kotlin/com/wire/android/di/ClientScopeProvider.kt
+++ app/src/main/kotlin/com/wire/android/di/ClientScopeProvider.kt
@@ -1,7 +1,6 @@
 package com.wire.android.di
 
 import com.wire.kalium.logic.CoreLogic
-import com.wire.kalium.logic.feature.auth.AuthSession
 import com.wire.kalium.logic.feature.client.ClientScope
 import dagger.assisted.Assisted
 import dagger.assisted.AssistedFactory
@@ -9,13 +8,13 @@ import dagger.assisted.AssistedInject
 
 class ClientScopeProvider @AssistedInject constructor(
     @KaliumCoreLogic private val coreLogic: CoreLogic,
-    @Assisted private val authSession: AuthSession
+    @Assisted private val userId: String
 ) {
     val clientScope: ClientScope
-        get() = coreLogic.getSessionScope(authSession).client
+        get() = coreLogic.getSessionScope(userId).client
 
     @AssistedFactory
     interface Factory {
-        fun create(authSession: AuthSession): ClientScopeProvider
+        fun create(userId: String): ClientScopeProvider
     }
 }

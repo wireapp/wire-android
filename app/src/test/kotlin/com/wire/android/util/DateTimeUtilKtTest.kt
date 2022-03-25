59fa40ff3a869403037abefdaf9419a5ded4f61d
yamilmedina <yamilmedina@users.noreply.github.com>
2022-03-25 10:27:44 -0300

    chore: fix typos

--- app/src/test/kotlin/com/wire/android/util/DateTimeUtilKtTest.kt
+++ app/src/test/kotlin/com/wire/android/util/DateTimeUtilKtTest.kt
@@ -6,13 +6,13 @@ import org.junit.jupiter.api.Test
 class DateTimeUtilKtTest {
 
     @Test
-    fun `given an invalid date, when performing a transformation, then return null`() {
+    fun `given a invalid date, when performing a transformation, then return null`() {
         val result = "NOT_VALID".formatMediumDateTime()
         assertEquals(null, result)
     }
 
     @Test
-    fun `given an valid date, when performing a transformation, then return with medium format`() {
+    fun `given a valid date, when performing a transformation, then return with medium format`() {
         val result = "2022-03-24T18:02:30.360Z".formatMediumDateTime()
         assertEquals("Mar 24, 2022, 6:02:30 PM", result)
     }

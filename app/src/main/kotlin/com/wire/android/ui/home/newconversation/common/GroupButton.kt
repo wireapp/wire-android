1c53c20aae7f17476a5f6bd46cf7ab58515a2f54
mateusz.pachulski <mateusz.pachulski@appunite.com>
2022-03-25 15:57:59 +0100

    add more icon

--- app/src/main/kotlin/com/wire/android/ui/home/newconversation/common/GroupButton.kt
+++ app/src/main/kotlin/com/wire/android/ui/home/newconversation/common/GroupButton.kt
@@ -6,17 +6,17 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Search
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import com.wire.android.R
-import com.wire.android.ui.common.Icon
 import com.wire.android.ui.common.button.IconAlignment
 import com.wire.android.ui.common.button.WireTertiaryButton
 import com.wire.android.ui.common.button.wireSecondaryButtonColors
@@ -45,7 +45,14 @@ fun GroupButton(groupSize: Int) {
         WireTertiaryButton(
             colors = wireSecondaryButtonColors(),
             onClick = { },
-            leadingIcon = Icons.Filled.Search.Icon(),
+            leadingIcon = {
+                Icon(
+                    painter = painterResource(id = R.drawable.ic_more),
+                    contentDescription = stringResource(R.string.content_description_right_arrow),
+                    modifier = Modifier
+                        .size(MaterialTheme.wireDimensions.conversationBottomSheetItemSize)
+                )
+            },
             leadingIconAlignment = IconAlignment.Center,
             fillMaxWidth = false
         )

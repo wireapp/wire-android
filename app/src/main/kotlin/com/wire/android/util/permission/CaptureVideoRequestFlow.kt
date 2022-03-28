a1c630ad82b6c4bafca425f72595d27b3ae55d71
yamilmedina <yamilmedina@users.noreply.github.com>
2022-03-28 15:55:34 -0300

    chore: add docs to main flows

--- app/src/main/kotlin/com/wire/android/util/permission/CaptureVideoRequestFlow.kt
+++ app/src/main/kotlin/com/wire/android/util/permission/CaptureVideoRequestFlow.kt
@@ -8,6 +8,14 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
 import androidx.compose.ui.platform.LocalContext
 
+/**
+ * Flow that will launch the camera to record a video.
+ * This will handle the permissions request in case there is no permission granted for the camera.
+ *
+ * @param onVideoRecorded action that will be executed for Camera's [ActivityResultContract]
+ * @param onPermissionDenied action to be executed when the permissions is denied
+ * @param targetVideoFileUri target file where the media will be stored
+ */
 @Composable
 fun rememberCaptureVideoFlow(
     onVideoRecorded: (Boolean) -> Unit,

6888b7ec70161ce144e9095fc74525547157e8b5
mateusz.pachulski <mateusz.pachulski@appunite.com>
2022-03-29 13:42:48 +0200

    add destination

---
+++ app/src/main/kotlin/com/wire/android/ui/userprofile/other/OtherUserProfileViewModel.kt
@@ -0,0 +1,11 @@
+package com.wire.android.ui.userprofile.other
+
+import androidx.lifecycle.ViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+
+
+@HiltViewModel
+class OtherUserProfileViewModel : ViewModel() {
+
+
+}

176df92b57f3ae2dba49268f875835b2deaedba9
Mohamad Jaara <mohamad.jaara@wire.com>
2022-03-30 13:21:35 +0200

    fix: inject qualifiedID (#455)
    * fix: inject qualifiedID
    
    * update kalium reference

--- app/src/main/kotlin/com/wire/android/di/ClientScopeProvider.kt
+++ app/src/main/kotlin/com/wire/android/di/ClientScopeProvider.kt
@@ -2,19 +2,20 @@ package com.wire.android.di
 
 import com.wire.kalium.logic.CoreLogic
 import com.wire.kalium.logic.feature.client.ClientScope
+import com.wire.kalium.logic.data.user.UserId
 import dagger.assisted.Assisted
 import dagger.assisted.AssistedFactory
 import dagger.assisted.AssistedInject
 
 class ClientScopeProvider @AssistedInject constructor(
     @KaliumCoreLogic private val coreLogic: CoreLogic,
-    @Assisted private val userId: String
+    @Assisted private val userId: UserId
 ) {
     val clientScope: ClientScope
         get() = coreLogic.getSessionScope(userId).client
 
     @AssistedFactory
     interface Factory {
-        fun create(userId: String): ClientScopeProvider
+        fun create(userId: UserId): ClientScopeProvider
     }
 }

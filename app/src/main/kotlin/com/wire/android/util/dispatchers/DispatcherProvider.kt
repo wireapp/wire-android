5eaa958b1d2c83ed62ab2e0f3d5e284e951a2b8e
yamilmedina <yamilmedina@users.noreply.github.com>
2022-03-31 09:00:03 -0300

    chore: inject providers in test

--- app/src/main/kotlin/com/wire/android/util/dispatchers/DispatcherProvider.kt
+++ app/src/main/kotlin/com/wire/android/util/dispatchers/DispatcherProvider.kt
@@ -3,6 +3,9 @@ package com.wire.android.util.dispatchers
 import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.Dispatchers
 
+/**
+ * Wrapper injectable provided interface that enables correctness for testing
+ */
 interface DispatcherProvider {
     fun main(): CoroutineDispatcher
     fun io(): CoroutineDispatcher
@@ -10,6 +13,9 @@ interface DispatcherProvider {
     fun unconfined(): CoroutineDispatcher
 }
 
+/**
+ * Default production dispatchers provider wrapper
+ */
 class DefaultDispatcherProvider : DispatcherProvider {
     override fun main() = Dispatchers.Main
     override fun io() = Dispatchers.IO
--- app/src/test/kotlin/com/wire/android/config/TestDispatcherProvider.kt
+++ app/src/test/kotlin/com/wire/android/config/TestDispatcherProvider.kt
@@ -1,13 +1,17 @@
 package com.wire.android.config
 
 import com.wire.android.util.dispatchers.DispatcherProvider
+import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.test.UnconfinedTestDispatcher
 
+/**
+ * Default testing dispatchers provider wrapper
+ */
 @OptIn(ExperimentalCoroutinesApi::class)
-class TestDispatcherProvider : DispatcherProvider {
-    override fun main() = UnconfinedTestDispatcher()
-    override fun io() = UnconfinedTestDispatcher()
-    override fun default() = UnconfinedTestDispatcher()
-    override fun unconfined() = UnconfinedTestDispatcher()
+class TestDispatcherProvider(private val dispatcher: CoroutineDispatcher = UnconfinedTestDispatcher()) : DispatcherProvider {
+    override fun main() = dispatcher
+    override fun io() = dispatcher
+    override fun default() = dispatcher
+    override fun unconfined() = dispatcher
 }

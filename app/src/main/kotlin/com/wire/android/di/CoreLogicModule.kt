a4ac3464217d68f352a735e17272cb73219a2346
Vitor Hugo Schwaab <vitor@schwaab.xyz>
2022-04-05 18:02:15 +0200

    feat(conversation): show 1:1 conversation name (AR-1177) (#474)

--- app/src/main/kotlin/com/wire/android/di/CoreLogicModule.kt
+++ app/src/main/kotlin/com/wire/android/di/CoreLogicModule.kt
@@ -120,8 +120,8 @@ class UseCaseModule {
 
     @ViewModelScoped
     @Provides
-    fun getConversationsUseCaseProvider(@KaliumCoreLogic coreLogic: CoreLogic, @CurrentAccount currentAccount: UserId) =
-        coreLogic.getSessionScope(currentAccount).conversations.getConversations
+    fun provideObserveConversationListDetailsUseCase(@KaliumCoreLogic coreLogic: CoreLogic, @CurrentAccount currentAccount: UserId) =
+        coreLogic.getSessionScope(currentAccount).conversations.observeConversationListDetails
 
     @ViewModelScoped
     @Provides

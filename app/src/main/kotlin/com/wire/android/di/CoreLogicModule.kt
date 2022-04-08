1b1cb8cc66a89de0f6102e5112473140b94bec9c
Yamil Medina <yamilmedina@users.noreply.github.com>
2022-04-08 14:18:03 -0400

    test: configure di for authenticated tests (#479)
    * test: add test junit test rule to enable login session for tests
    
    * test: add manual login attempt
    
    * test: configure di for authenticated testing
    
    * test: format
    
    * chore: change strategy, instead of login use real authsess snapshot
    
    * chore: fix max line lenght
    
    * chore: using orchestrator to avoid oom
    
    * chore: increase default timeout for checks
    
    * chore: upd kalium ref
    
    * chore: rollback to login strategy

--- app/src/main/kotlin/com/wire/android/di/CoreLogicModule.kt
+++ app/src/main/kotlin/com/wire/android/di/CoreLogicModule.kt
@@ -23,9 +23,9 @@ import dagger.hilt.android.components.ViewModelComponent
 import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.android.scopes.ViewModelScoped
 import dagger.hilt.components.SingletonComponent
-import kotlinx.coroutines.runBlocking
 import javax.inject.Qualifier
 import javax.inject.Singleton
+import kotlinx.coroutines.runBlocking
 
 @Qualifier
 @Retention(AnnotationRetention.BINARY)
@@ -56,9 +56,7 @@ class CoreLogicModule {
 
 @Module
 @InstallIn(ViewModelComponent::class)
-@Suppress("TooManyFunctions")
-class UseCaseModule {
-
+class SessionModule {
     @CurrentAccount
     @ViewModelScoped
     @Provides
@@ -73,6 +71,12 @@ class UseCaseModule {
             }
         }
     }
+}
+
+@Module
+@InstallIn(ViewModelComponent::class)
+@Suppress("TooManyFunctions")
+class UseCaseModule {
 
     @ViewModelScoped
     @Provides

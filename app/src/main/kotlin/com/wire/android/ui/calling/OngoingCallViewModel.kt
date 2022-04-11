25a05bda3f3548829ad3502e7c32f63f16e0b06f
Oussama <oussama.has100@gmail.com>
2022-04-11 10:18:29 +0100

    chore: resolve conflict

--- app/src/main/kotlin/com/wire/android/ui/calling/OngoingCallViewModel.kt
+++ app/src/main/kotlin/com/wire/android/ui/calling/OngoingCallViewModel.kt
@@ -3,14 +3,30 @@ package com.wire.android.ui.calling
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
+import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.wire.android.navigation.EXTRA_CONVERSATION_ID
+import com.wire.android.navigation.parseIntoQualifiedID
+import com.wire.kalium.logic.feature.call.usecase.StartCallUseCase
 import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
+import com.wire.kalium.logic.data.id.QualifiedID
+import kotlinx.coroutines.launch
 
 @HiltViewModel
-class OngoingCallViewModel @Inject constructor() : ViewModel() {
+class OngoingCallViewModel @Inject constructor(
+    savedStateHandle: SavedStateHandle,
+    private val startCallUseCase: StartCallUseCase
+) : ViewModel() {
+
     var callEstablishedState by mutableStateOf(OngoingCallState())
-    private set
+        private set
+
+    val conversationId: QualifiedID? = savedStateHandle
+        .getLiveData<String>(EXTRA_CONVERSATION_ID)
+        .value
+        ?.parseIntoQualifiedID()
 
     init {
         //init with fake values
@@ -21,5 +37,15 @@ class OngoingCallViewModel @Inject constructor() : ViewModel() {
             isCameraOn = false,
             isSpeakerOn = false
         )
+        viewModelScope.launch {
+            initiateCall()
+        }
+    }
+
+    private suspend fun initiateCall() {
+        conversationId?.let {
+            //TODO pass conversation type
+            startCallUseCase.invoke(it)
+        }
     }
 }

b3d3e0359c8ec3f005145def5dd24d0ccc8999ed
alexandreferris <ferris.alexandre@gmail.com>
2022-04-13 14:03:09 +0200

    feat: add accept button with icon

---
+++ app/src/main/kotlin/com/wire/android/ui/calling/controlButtons/AcceptButton.kt
@@ -0,0 +1,27 @@
+package com.wire.android.ui.calling.controlButtons
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import com.wire.android.R
+import com.wire.android.ui.theme.wireDimensions
+
+@Composable
+fun AcceptButton() {
+    IconButton(
+        modifier = Modifier.width(MaterialTheme.wireDimensions.defaultCallingControlsSize),
+        onClick = { }
+    ) {
+        Image(
+            painter = painterResource(
+                id = R.drawable.ic_accept
+            ),
+            contentDescription = stringResource(id = R.string.calling_accept_call)
+        )
+    }
+}

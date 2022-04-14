3d1101ce599d66bb01eceb537bcf14d211c395ed
mateusz.pachulski <mateusz.pachulski@appunite.com>
2022-04-14 10:52:09 +0200

    address review comments

--- app/src/main/kotlin/com/wire/android/ui/userprofile/other/OtherUserProfileScreenViewModel.kt
+++ app/src/main/kotlin/com/wire/android/ui/userprofile/other/OtherUserProfileScreenViewModel.kt
@@ -42,9 +42,9 @@ class OtherUserProfileScreenViewModel @Inject constructor(
         // for now it is just to be able to proceed forward
         val internalStatus = savedStateHandle.get<String>(EXTRA_CONNECTED_STATUS)
 
-        val booleanStatus = internalStatus == "true"
+        val isAKnownUser = internalStatus == "true"
 
-        if (booleanStatus) {
+        if (isAKnownUser) {
             state = state.copy(isDataLoading = true)
 
             viewModelScope.launch {
@@ -64,19 +64,19 @@ class OtherUserProfileScreenViewModel @Inject constructor(
                     }
                 }
             }
+        } else {
+            //TODO: for now this is a mock data when we open a screen for a not connected user
+            // we need to retrieve it from the back-end ? and not the local source ?
+            state = state.copy(
+                isAvatarLoading = false,
+                fullName = "Kim",
+                userName = "Dawson",
+                teamName = "AWESOME TEAM NAME",
+                email = "kim.dawson@gmail.com",
+                phone = "+49 123 456 000",
+                connectionStatus = if (isAKnownUser) ConnectionStatus.Connected else ConnectionStatus.NotConnected(false)
+            )
         }
-
-        //TODO: for now this is a mock data when we open a screen for a not connected user
-        // we need to retrieve it from the back-end ? and not the local source ?
-        state = state.copy(
-            isAvatarLoading = false,
-            fullName = "Kim",
-            userName = "Dawson",
-            teamName = "AWESOME TEAM NAME",
-            email = "kim.dawson@gmail.com",
-            phone = "+49 123 456 000",
-            connectionStatus = if (booleanStatus) ConnectionStatus.Connected else ConnectionStatus.NotConnected(false)
-        )
     }
 
     fun openConversation() {

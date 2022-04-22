6217ad8abc457d00dfec0c1b092f4273bc78a54a
Alexandre Ferris <ferris.alexandre@gmail.com>
2022-04-22 15:55:59 +0200

    feat: Update Calling UI (#528)
    * feat: Create Incoming Call UI (#509)
    
    * feat: change kalium branch to develop
    
    * feat: add accept icon
    
    * feat: add accept button with icon
    
    * feat: add decline button, fix speaker button dimensions and add drawables
    
    * feat: add strings, dimensions and incoming call screen
    
    * test: adjust connection state in test
    
    * feat: Update Calling UI - Part 2 (#524)
    
    * feat: change kalium branch to develop
    
    * feat: add accept icon
    
    * feat: add accept button with icon
    
    * feat: add decline button, fix speaker button dimensions and add drawables
    
    * feat: add strings, dimensions and incoming call screen
    
    * test: adjust connection state in test
    
    * feat: adjust padding named params
    
    * feat: add initial state for control buttons
    
    * feat: add incoming call state and viewModel to screen
    
    * feat: revert navigation back to ongoing call screen when starting a call
    
    * feat: add initial button state and onclick callback to control buttons
    
    * feat: add incoming call screen and its navigation
    
    * feat: add necessary use cases, call state and incoming call view model
    
    * chore: remove unnecessary import
    
    * chore: remove unused variable
    
    * chore: remove logger
    
    * chore: add unknown caller string
    
    * chore: force getting the conversationId in the view model otherwise it throws an exception
    
    * test: Update Calling UI - Part3 (#525)
    
    * chore: adjust changed microphone button params
    
    * test: add IncomingCallViewModelTest test
    
    * test: merge test cases
    
    * chore: remove unused param
    
    * chore: add conversation id when opening ongoing call screen
    
    * chore: add correct usage of incoming calls usecase
    
    * chore: remove start call from ongoing call screen
    
    * chore: update kalium reference
    
    * chore: add fixed height for bottomsheet
    
    * chore: adjust jumping bottomsheet
    
    * test: fix failing test
    
    * retrigger checks
    
    * fix: adjust string naming typo
    
    * chore: remove viewmodel injection from before and let it be from the screen itself
    
    * chore: move usecase collect logic to private fun
    
    * chore: adjust naming
    
    * chore: move incoming calls usecase collect to private fun
    
    * chore: update kalium

--- app/src/main/kotlin/com/wire/android/ui/calling/controlButtons/AcceptButton.kt
+++ app/src/main/kotlin/com/wire/android/ui/calling/controlButtons/AcceptButton.kt
@@ -5,10 +5,6 @@ import androidx.compose.foundation.layout.width
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
@@ -16,22 +12,16 @@ import com.wire.android.R
 import com.wire.android.ui.theme.wireDimensions
 
 @Composable
-fun CameraButton(initialState: Boolean = false) {
-    var isCameraOn by remember { mutableStateOf(initialState) }
-
+fun AcceptButton(buttonClicked: () -> Unit) {
     IconButton(
         modifier = Modifier.width(MaterialTheme.wireDimensions.defaultCallingControlsSize),
-        onClick = { isCameraOn = !isCameraOn }
+        onClick = buttonClicked
     ) {
         Image(
             painter = painterResource(
-                id = if (isCameraOn) {
-                    R.drawable.ic_camera_on
-                } else {
-                    R.drawable.ic_camera_off
-                }
+                id = R.drawable.ic_accept
             ),
-            contentDescription = stringResource(id = R.string.calling_turn_camera_on_off),
+            contentDescription = stringResource(id = R.string.calling_accept_call)
         )
     }
 }

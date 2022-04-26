94fc94be2d9272c7198777a63782025319696126
Mojtaba Chenani <chenani@outlook.com>
2022-04-26 15:44:09 +0200

    chore: refactor UIText class

--- app/src/main/kotlin/com/wire/android/util/ui/UIText.kt
+++ app/src/main/kotlin/com/wire/android/util/ui/UIText.kt
@@ -6,25 +6,21 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.res.stringResource
 
 sealed class UIText {
-    data class DynamicString(val value:String): UIText()
+    data class DynamicString(val value: String) : UIText()
 
     class StringResource(
         @StringRes val resId: Int,
         vararg val args: Any
-    ):UIText()
+    ) : UIText()
 
     @Composable
-    fun asString(): String{
-        return when(this){
-            is DynamicString -> value
-            is StringResource -> stringResource(id = resId, *args)
-        }
+    fun asString() = when (this) {
+        is DynamicString -> value
+        is StringResource -> stringResource(id = resId, *args)
     }
 
-    fun asString(context: Context): String{
-        return when(this){
-            is DynamicString -> value
-            is StringResource -> context.getString(resId, *args)
-        }
+    fun asString(context: Context) = when (this) {
+        is DynamicString -> value
+        is StringResource -> context.getString(resId, *args)
     }
 }

164b8fef1d0478977734ba7506633369668f519e
Mojtaba Chenani <chenani@outlook.com>
2022-04-28 11:44:13 +0200

    fix: code styles

--- app/src/main/kotlin/com/wire/android/ui/authentication/login/LoginError.kt
+++ app/src/main/kotlin/com/wire/android/ui/authentication/login/LoginError.kt
@@ -4,18 +4,21 @@ import androidx.compose.material3.ExperimentalMaterial3Api
 import com.wire.android.util.deeplink.SSOFailureCodes
 import com.wire.kalium.logic.CoreFailure
 
-sealed class LoginError{
-    object None: LoginError()
-    sealed class TextFieldError: LoginError() {
-        object InvalidValue: TextFieldError()
+sealed class LoginError {
+    object None : LoginError()
+    sealed class TextFieldError : LoginError() {
+        object InvalidValue : TextFieldError()
     }
-    sealed class DialogError: LoginError(){
+
+    sealed class DialogError : LoginError() {
         data class GenericError(val coreFailure: CoreFailure) : LoginError.DialogError()
-        object InvalidCredentialsError: DialogError()
-        object InvalidSSOCookie: DialogError()
-        object UserAlreadyExists: DialogError()
-        data class SSOResultError @OptIn(ExperimentalMaterial3Api::class) constructor(val result: SSOFailureCodes) : LoginError.DialogError()
+        object InvalidCredentialsError : DialogError()
+        object InvalidSSOCookie : DialogError()
+        object UserAlreadyExists : DialogError()
+        data class SSOResultError @OptIn(ExperimentalMaterial3Api::class) constructor(val result: SSOFailureCodes) :
+            LoginError.DialogError()
     }
-    object TooManyDevicesError: LoginError()
+
+    object TooManyDevicesError : LoginError()
 
 }

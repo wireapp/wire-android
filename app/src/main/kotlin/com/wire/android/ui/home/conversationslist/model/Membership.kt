304a825311d45438127a92f78b86949f19f7d515
Mateusz <m.pachulski94@gmail.com>
2022-04-28 16:07:50 +0200

    fix ui tests failing

--- app/src/main/kotlin/com/wire/android/ui/home/conversationslist/model/Membership.kt
+++ app/src/main/kotlin/com/wire/android/ui/home/conversationslist/model/Membership.kt
@@ -4,5 +4,8 @@ import androidx.annotation.StringRes
 import com.wire.android.R
 
 enum class Membership(@StringRes val stringResourceId: Int) {
-    Guest(R.string.label_membership_guest), Federated(R.string.label_federated_membership), External(R.string.label_membership_external), None(-1)
+    Guest(R.string.label_membership_guest),
+    Federated(R.string.label_federated_membership),
+    External(R.string.label_membership_external),
+    None(-1)
 }

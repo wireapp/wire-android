621ce812f773941745c871063020b0d02df787e7
Gonzalo Gran Crespo <gongracr@gmail.com>
2022-04-29 16:20:28 +0200

    Fixed linter issue

--- app/src/main/kotlin/com/wire/android/ui/home/gallery/MediaGallery.kt
+++ app/src/main/kotlin/com/wire/android/ui/home/gallery/MediaGallery.kt
@@ -72,6 +72,7 @@ fun MediaGalleryContent(imageAsset: ImageAsset.PrivateAsset) {
     }
 }
 
+@Composable
 fun EditGalleryMenuItems(
     onDeleteMessage: () -> Unit
 ): List<@Composable () -> Unit> {

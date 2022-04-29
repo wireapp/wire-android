7791f45d33b00f43880e9196552421a1dda49cf9
Mohamad Jaara <mohamad.jaara@wire.com>
2022-04-29 12:17:52 +0200

    add ViewModelExtensions.kt

---
+++ app/src/main/kotlin/com/wire/android/util/ViewModelExtensions.kt
@@ -0,0 +1,39 @@
+@file:OptIn(ExperimentalContracts::class)
+
+package com.wire.android.util
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlin.contracts.ExperimentalContracts
+import kotlin.contracts.InvocationKind
+import kotlin.contracts.contract
+
+inline fun ViewModel.launchOnIO(crossinline execute: suspend () -> Unit) {
+    contract {
+        callsInPlace(execute, InvocationKind.EXACTLY_ONCE)
+    }
+    this.viewModelScope.launch(Dispatchers.IO) {
+        execute()
+    }
+}
+
+inline fun ViewModel.launchOnDefault(crossinline execute: suspend () -> Unit) {
+    contract {
+        callsInPlace(execute, InvocationKind.EXACTLY_ONCE)
+    }
+    this.viewModelScope.launch(Dispatchers.Default) {
+        execute()
+    }
+}
+
+// TODO: add lint warning
+inline fun ViewModel.launchOnMain(crossinline execute: suspend () -> Unit) {
+    contract {
+        callsInPlace(execute, InvocationKind.EXACTLY_ONCE)
+    }
+    this.viewModelScope.launch(Dispatchers.Main) {
+        execute()
+    }
+}

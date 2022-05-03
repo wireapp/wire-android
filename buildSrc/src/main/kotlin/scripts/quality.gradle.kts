efb63a14873faae7b84c3f034a3030dd8afbeb79
yamilmedina <yamilmedina@users.noreply.github.com>
2022-05-03 15:18:44 +0200

    chore: comments

--- buildSrc/src/main/kotlin/scripts/quality.gradle.kts
+++ buildSrc/src/main/kotlin/scripts/quality.gradle.kts
@@ -93,8 +93,8 @@ val jacocoReport by tasks.registering(JacocoReport::class) {
                 "**/*Response.*",
                 "**/*Application.*",
                 "**/*Entity.*",
-                "**/theme/*.*", // Ignores jetpack theme related code
-                "**/common/*.*", // Ignores jetpack common components related code
+                "**/theme/*.*", // Ignores jetpack compose theme related code
+                "**/common/*.*", // Ignores jetpack compose common components related code
                 "**/navigation/*.*", // Ignores jetpack navigation related code
             )
         }

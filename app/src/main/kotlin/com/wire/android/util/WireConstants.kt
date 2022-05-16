ed3f0596bbdaf72c34086a47899fd9ad29a9f1d0
Mohamad Mukresh <mokresh.mohammed@gmail.com>
2022-05-16 21:08:54 +0200

    update the API key

--- app/src/main/kotlin/com/wire/android/util/WireConstants.kt
+++ app/src/main/kotlin/com/wire/android/util/WireConstants.kt
@@ -3,7 +3,7 @@ package com.wire.android.util
 object WireConstants {
     const val  FB_STAGING_PROJECT_ID="wiregcmpushstaging"
     const val  FB_STAGING_APPLICATION_ID="1:723990470614:android:10cf802bfcc7bb71d28e77"
-    const val  FB_STAGING_API_KEY="AIzaSyAxGWhUX9367Vrg3bkAZbtEk7pdsnF89mE"
+    const val  FB_STAGING_API_KEY="AIzaSyAGCoJGUtDBLJJiQPLxHQRrdkbyI0wlbo8"
 
     enum class ServerTitle(val title: String) {
         STAGING("staging"),

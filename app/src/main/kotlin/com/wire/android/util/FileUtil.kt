b30594ff4728f862dc00cd34cd37efe6abc8f3f1
Mateusz <m.pachulski94@gmail.com>
2022-05-17 16:08:44 +0200

    some commit

--- app/src/main/kotlin/com/wire/android/util/FileUtil.kt
+++ app/src/main/kotlin/com/wire/android/util/FileUtil.kt
@@ -49,6 +49,7 @@ private fun getTempWritableAttachmentUri(context: Context, fileName: String): Ur
     return FileProvider.getUriForFile(context, BuildConfig.APPLICATION_ID + ".provider", file)
 }
 
+
 fun Uri.getMimeType(context: Context): String? {
     val extension = MimeTypeMap.getFileExtensionFromUrl(path)
     return context.contentResolver.getType(this)

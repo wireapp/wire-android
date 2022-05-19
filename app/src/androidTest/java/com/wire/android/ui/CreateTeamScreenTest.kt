ae6d7c732b9037fd14902939626f475b3e67f45b
dogaguclu <doga-guclu@hotmail.com>
2022-05-19 13:56:18 +0200

    new test cases for create team

--- app/src/androidTest/java/com/wire/android/ui/CreateTeamScreenTest.kt
+++ app/src/androidTest/java/com/wire/android/ui/CreateTeamScreenTest.kt
@@ -7,6 +7,7 @@ import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.test.assertHasClickAction
 import androidx.compose.ui.test.assertIsDisplayed
+import androidx.compose.ui.test.assertIsEnabled
 import androidx.compose.ui.test.assertIsNotEnabled
 import androidx.compose.ui.test.assertTextEquals
 import androidx.compose.ui.test.hasTestTag
@@ -89,6 +90,8 @@ class CreateTeamScreenTest {
 
     val invalidEmailError = "Please enter a valid format for your email."
     val createATeamText = "Enter your email to create your team:"
+    val invalidPassword = "Use at least 8 characters, with one lowercase letter, one capital letter, and a special character."
+    val passwordsNotMatch = "Passwords do not match"
 
     @Ignore
     @Test
@@ -133,5 +136,66 @@ class CreateTeamScreenTest {
         tcTitle.assertIsDisplayed()
         tcButton.performClick()
         Intents.intending(allOf(hasAction(Intent.ACTION_VIEW), hasData("https://wire.com/en/legal/")))
+    }
+
+    @Test
+    fun create_team_invalid_password() {
+        title.assertIsDisplayed()
+        continueButton.performClick()
+        createTeamText.assertTextEquals(createATeamText)
+        emailField.onChildren()[1].performTextInput(EMAIL)
+        continueButton.performClick()
+        tcTitle.assertIsDisplayed()
+        tcButton.onSiblings()[1].performClick()
+        firstName.onChildren()[1].performTextInput("name")
+        firstName.onChildren()[2].performTextInput("surName")
+        firstName.onChildren()[3].performTextInput("teamName")
+        firstName.onChildren()[4].performTextInput("password")
+        firstName.onChildren()[5].performTextInput("password")
+        continueButton.performClick()
+        composeTestRule.waitForExecution {
+            composeTestRule.onNodeWithText(invalidPassword).assertDoesNotExist()
+        }
+    }
+
+    @Test
+    fun create_team_missmatch_password() {
+        title.assertIsDisplayed()
+        continueButton.performClick()
+        createTeamText.assertTextEquals(createATeamText)
+        emailField.onChildren()[1].performTextInput(EMAIL)
+        continueButton.performClick()
+        tcTitle.assertIsDisplayed()
+        tcButton.onSiblings()[1].performClick()
+        firstName.onChildren()[1].performTextInput("name")
+        firstName.onChildren()[2].performTextInput("surName")
+        firstName.onChildren()[3].performTextInput("teamName")
+        firstName.onChildren()[4].performTextInput("Abcd1234!")
+        firstName.onChildren()[5].performTextInput("Abcd1234.")
+        continueButton.performClick()
+        composeTestRule.waitForExecution {
+            composeTestRule.onNodeWithText(passwordsNotMatch).assertDoesNotExist()
+        }
+    }
+
+    @Test
+    fun create_team_required_fields() {
+        title.assertIsDisplayed()
+        continueButton.performClick()
+        createTeamText.assertTextEquals(createATeamText)
+        emailField.onChildren()[1].performTextInput(EMAIL)
+        continueButton.performClick()
+        tcTitle.assertIsDisplayed()
+        tcButton.onSiblings()[1].performClick()
+        firstName.onChildren()[1].performTextInput("name")
+        continueButton.assertIsNotEnabled()
+        firstName.onChildren()[2].performTextInput("surName")
+        continueButton.assertIsNotEnabled()
+        firstName.onChildren()[3].performTextInput("teamName")
+        continueButton.assertIsNotEnabled()
+        firstName.onChildren()[4].performTextInput("Abcd1234!")
+        continueButton.assertIsNotEnabled()
+        firstName.onChildren()[5].performTextInput("Abcd1234.")
+        continueButton.assertIsEnabled()
     }
 }

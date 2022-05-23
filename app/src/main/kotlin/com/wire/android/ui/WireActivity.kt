151d87012b7fece7e25fc1576eb8c479cd96233d
Michał Saleniuk <saleniuk@gmail.com>
2022-05-24 00:05:20 +0200

    use internalPadding in WireActivity, downgrade accompanist lib

--- app/src/main/kotlin/com/wire/android/ui/WireActivity.kt
+++ app/src/main/kotlin/com/wire/android/ui/WireActivity.kt
@@ -1,12 +1,13 @@
 package com.wire.android.ui
 
 import android.content.Intent
-import android.net.Uri
 import android.os.Bundle
 import androidx.activity.compose.setContent
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.animation.ExperimentalAnimationApi
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.ExperimentalMaterialApi
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
@@ -14,23 +15,18 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.ExperimentalComposeUiApi
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalSoftwareKeyboardController
-import androidx.compose.ui.res.stringResource
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.navigation.NavHostController
 import com.google.accompanist.navigation.animation.rememberAnimatedNavController
-import com.wire.android.R
 import com.wire.android.navigation.NavigationGraph
 import com.wire.android.navigation.NavigationManager
 import com.wire.android.navigation.navigateToItem
-import com.wire.android.ui.common.WireDialog
-import com.wire.android.ui.common.WireDialogButtonProperties
-import com.wire.android.ui.common.WireDialogButtonType
 import com.wire.android.ui.server.ClientUpdateRequiredDialog
 import com.wire.android.ui.server.ServerVersionNotSupportedDialog
 import com.wire.android.ui.theme.WireTheme
 import com.wire.android.util.CustomTabsHelper
-import com.wire.android.util.deeplink.DeepLinkResult
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.flow.launchIn
@@ -73,34 +69,36 @@ class WireActivity : AppCompatActivity() {
                 val navController = rememberAnimatedNavController()
                 val state: WireActivityState = viewModel.state
                 setUpNavigation(navController, scope)
-                Scaffold {
-                    when (state) {
-                        WireActivityState.ServerVersionNotSupported -> {
-                            keepSplashScreen(false)
-                            ServerVersionNotSupportedDialog(
-                                onClose = { this@WireActivity.finish() }
-                            )
-                        }
-                        is WireActivityState.ClientUpdateRequired -> {
-                            keepSplashScreen(false)
-                            ClientUpdateRequiredDialog(
-                                onUpdate = {
-                                    CustomTabsHelper.launchUrl(this@WireActivity, state.clientUpdateUrl)
-                                    this@WireActivity.finish()
-                                },
-                                onClose = { this@WireActivity.finish() }
-                            )
-                        }
-                        is WireActivityState.NavigationGraph -> {
-                            keepSplashScreen(false)
-                            NavigationGraph(
-                                navController = navController,
-                                startDestination = state.startNavigationRoute,
-                                appInitialArgs = state.navigationArguments
-                            )
-                        }
-                        WireActivityState.Loading -> {
-                            keepSplashScreen(true)
+                Scaffold { internalPadding ->
+                    Box(modifier = Modifier.padding(internalPadding)) {
+                        when (state) {
+                            WireActivityState.ServerVersionNotSupported -> {
+                                keepSplashScreen(false)
+                                ServerVersionNotSupportedDialog(
+                                    onClose = { this@WireActivity.finish() }
+                                )
+                            }
+                            is WireActivityState.ClientUpdateRequired -> {
+                                keepSplashScreen(false)
+                                ClientUpdateRequiredDialog(
+                                    onUpdate = {
+                                        CustomTabsHelper.launchUrl(this@WireActivity, state.clientUpdateUrl)
+                                        this@WireActivity.finish()
+                                    },
+                                    onClose = { this@WireActivity.finish() }
+                                )
+                            }
+                            is WireActivityState.NavigationGraph -> {
+                                keepSplashScreen(false)
+                                NavigationGraph(
+                                    navController = navController,
+                                    startDestination = state.startNavigationRoute,
+                                    appInitialArgs = state.navigationArguments
+                                )
+                            }
+                            WireActivityState.Loading -> {
+                                keepSplashScreen(true)
+                            }
                         }
                     }
                 }

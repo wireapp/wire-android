e68d768b851192d10d713af0b952fec7eb864285
Mohamad Mukresh <mokresh.mohammed@gmail.com>
2022-05-31 17:42:18 +0200

    generate build configs

--- buildSrc/src/main/kotlin/scripts/variants.gradle.kts
+++ buildSrc/src/main/kotlin/scripts/variants.gradle.kts
@@ -1,5 +1,15 @@
 package scripts
 
+import CertificatePin
+import ClientConfig
+import ConfigFields
+import ConfigType
+import Configs
+import Customization.defaultBuildtimeConfiguration
+import FeatureFlags
+import Features
+import FlavourConfigs
+
 plugins { id("com.android.application") apply false }
 
 object BuildTypes {
@@ -84,6 +94,8 @@ android {
      *
      * @see "ClientConfig.kt" file definition.
      */
+    val buildtimeConfiguration = defaultBuildtimeConfiguration(rootDir = rootDir)
+
     buildTypes.map { type ->
         ConfigFields.values().forEach { configField ->
             val configValuesMap = ClientConfig.properties[type.name].orEmpty()
@@ -106,6 +118,69 @@ android {
         Features.values().forEach { feature ->
             val activated = FeatureFlags.activated[flavor.name].orEmpty().contains(feature)
             flavor.buildConfigField("Boolean", feature.name, activated.toString())
+        }
+
+        Configs.values().forEach { configs ->
+            when (configs.configType) {
+                ConfigType.STRING -> {
+                    flavor.buildConfigField(
+                        configs.configType.type, configs.name, """"${buildtimeConfiguration?.configuration?.get(configs.value)}""""
+                    )
+                }
+                ConfigType.INT, ConfigType.BOOLEAN -> {
+                    flavor.buildConfigField(
+                        configs.configType.type, configs.name, buildtimeConfiguration?.configuration?.get(configs.value).toString()
+                    )
+                }
+                ConfigType.CERTIFICATE_PIN -> {
+                    val certificatePinMap = buildtimeConfiguration?.configuration?.get(Configs.CERTIFICATE_PIN.value) as Map<*, *>
+                    CertificatePin.values().forEach { certificatePin ->
+                        when (certificatePin.configType) {
+                            ConfigType.STRING -> {
+                                flavor.buildConfigField(
+                                    certificatePin.configType.type,
+                                    certificatePin.name,
+                                    """"${certificatePinMap[certificatePin.value]}""""
+                                )
+                            }
+                            ConfigType.INT, ConfigType.BOOLEAN -> {
+                                flavor.buildConfigField(
+                                    certificatePin.configType.type,
+                                    certificatePin.name,
+                                    certificatePinMap[certificatePin.value].toString()
+                                )
+                            }
+                        }
+                    }
+                }
+
+                ConfigType.FLAVOUR_CONFIG -> {
+                    if (configs.value == flavor.name.toLowerCase()) {
+                        val falvourMap = buildtimeConfiguration?.configuration?.get(flavor.name.toLowerCase()) as Map<*, *>
+
+                        FlavourConfigs.values().forEach { flavourConfigs ->
+                            when (flavourConfigs.configType) {
+                                ConfigType.STRING -> {
+                                    flavor.buildConfigField(
+                                        flavourConfigs.configType.type,
+                                        flavourConfigs.name,
+                                        """"${falvourMap[flavourConfigs.value]}""""
+                                    )
+                                }
+                                ConfigType.INT, ConfigType.BOOLEAN -> {
+                                    flavor.buildConfigField(
+                                        flavourConfigs.configType.type,
+                                        flavourConfigs.name,
+                                        falvourMap[flavourConfigs.value].toString()
+                                    )
+                                }
+                            }
+                        }
+                    }
+
+                }
+            }
+
         }
     }
 }

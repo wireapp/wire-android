73e00feeb471fef2f3449c21a0a51daf09fd6faa
dogaguclu <doga-guclu@hotmail.com>
2022-05-31 11:55:12 +0200

    update credentials

--- app/src/androidTest/java/com/wire/android/utils/Extensions.kt
+++ app/src/androidTest/java/com/wire/android/utils/Extensions.kt
@@ -34,6 +34,6 @@ fun ComposeTestRule.waitForExecution(timeoutMillis: Long = WAIT_TIMEOUT, block: 
 }
 
 const val WAIT_TIMEOUT = 9000L
-const val PASSWORD = "Mustafastaging1!"
-const val EMAIL = "mustafa+7@wire.com"
-const val USER_NAME = "doga7"
+const val PASSWORD = "Mustafastaging1."
+const val EMAIL = "mustafa+staging2@wire.com"
+const val USER_NAME = "MustafaStaging2"

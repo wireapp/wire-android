c4c079b5f8ca71b6c9632f744665b871cdacf8ff
Mateusz <m.pachulski94@gmail.com>
2022-05-31 12:50:22 +0200

    fix: navigate back from muting the notification bottom sheet (AR-1622) (#632)
    * remove navhost controller from the state object
    
    * pass a callback
    
    * move the display logic
    
    * navigate back
    
    * correctly navigate back
    
    * mute converastion working on back press
    
    * complete the comment
    
    * move back handler
    
    * format
    
    * bump up kalium version
    
    * remove the lambdas
    
    * conversation sheet content
    
    * remove the sealed class
    
    * pass conversation detail info
    
    * ready for review
    
    * remove the lambdas
    
    * refactor
    
    * format
    
    * format
    
    * fix ui test failing
    
    * test: update create team test steps (#617)
    
    * updated test steps
    
    * updated user info
    
    * updated email
    
    * adde wire email
    
    * fix: download and delete from gallery [AR-1149] (#620)
    
    * Allow downloading images from Gallery mode
    
    * Added message deletion class
    
    * Added deletion functionality
    
    * Added extra `CLEAR_CURRENT` backstackMode option
    
    * Fixed lint issues
    
    * Added tests
    
    * Added previousBackStackPassedArgs to NavigationCommand class
    
    * Removed clear current flag and refactored navigateBack method
    
    * Removed unused classes
    
    * Added more tests
    
    * rebase
    
    * ready for review
    
    * address pr comments
    
    * Update app/src/test/kotlin/com/wire/android/ui/home/conversations/ConversationsViewModelTest.kt
    
    Co-authored-by: Yamil Medina <yamilmedina@users.noreply.github.com>
    
    Co-authored-by: Vitor Hugo Schwaab <vitor@schwaab.xyz>
    Co-authored-by: Doga Guclu <doga-guclu@hotmail.com>
    Co-authored-by: Gonzo <gongracr@gmail.com>
    Co-authored-by: Yamil Medina <yamilmedina@users.noreply.github.com>

--- app/src/main/kotlin/com/wire/android/ui/home/conversationslist/ConversationState.kt
+++ app/src/main/kotlin/com/wire/android/ui/home/conversationslist/ConversationState.kt
@@ -1,65 +1,44 @@
 package com.wire.android.ui.home.conversationslist
 
-import androidx.compose.animation.ExperimentalAnimationApi
 import androidx.compose.material.ExperimentalMaterialApi
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.navigation.NavHostController
-import androidx.navigation.compose.rememberNavController
-import com.wire.android.ui.home.conversationslist.bottomsheet.ModalSheetContent
+import androidx.compose.runtime.setValue
+import com.wire.android.ui.home.conversationslist.bottomsheet.ConversationSheetContent
+import com.wire.android.ui.home.conversationslist.bottomsheet.ConversationTypeDetail
 import com.wire.android.ui.home.conversationslist.model.ConversationType
+import com.wire.kalium.logic.data.conversation.MutedConversationStatus
 
 @ExperimentalMaterialApi
-class ConversationState(
-    val navHostController: NavHostController,
-    val modalBottomSheetContentState: MutableState<ModalSheetContent>,
-    val isEditingMutedSetting: MutableState<Boolean>
-) {
-
-    fun toggleEditMutedSetting(enable: Boolean) {
-        isEditingMutedSetting.value = enable
-    }
+class ConversationState {
+    var conversationSheetContent: ConversationSheetContent? by mutableStateOf(null)
 
     fun changeModalSheetContentState(conversationType: ConversationType) {
         when (conversationType) {
             is ConversationType.GroupConversation -> {
                 with(conversationType) {
-                    modalBottomSheetContentState.value = ModalSheetContent.GroupConversationEdit(
+                    conversationSheetContent = ConversationSheetContent(
+                        conversationId = conversationId,
                         title = groupName,
-                        groupColorValue = groupColorValue,
-                        conversationId = this.conversationId,
-                        mutedStatus = this.mutedStatus
+                        mutingConversationState = mutedStatus,
+                        conversationTypeDetail = ConversationTypeDetail.Group(groupColorValue)
                     )
                 }
             }
             is ConversationType.PrivateConversation -> {
                 with(conversationType) {
-                    modalBottomSheetContentState.value = ModalSheetContent.PrivateConversationEdit(
+                    conversationSheetContent = ConversationSheetContent(
+                        conversationId = conversationId,
                         title = conversationInfo.name,
-                        avatarAsset = userInfo.avatarAsset,
-                        conversationId = this.conversationId,
-                        mutedStatus = this.mutedStatus
+                        mutingConversationState = mutedStatus,
+                        conversationTypeDetail = ConversationTypeDetail.Private(userInfo.avatarAsset)
                     )
                 }
             }
         }
     }
-}
 
-@OptIn(ExperimentalMaterialApi::class)
-@Composable
-fun rememberConversationState(
-    navHostController: NavHostController = rememberNavController(),
-    modalBottomSheetContentState: MutableState<ModalSheetContent> = remember {
-        mutableStateOf(ModalSheetContent.Initial)
-    },
-    isEditingMutedSetting: MutableState<Boolean> = remember { mutableStateOf(false) },
-) = remember(navHostController, modalBottomSheetContentState) {
-    ConversationState(
-        navHostController,
-        modalBottomSheetContentState,
-        isEditingMutedSetting
-    )
+    fun muteConversation(mutedConversationStatus: MutedConversationStatus) {
+        conversationSheetContent = conversationSheetContent?.copy(mutingConversationState = mutedConversationStatus)
+    }
 }

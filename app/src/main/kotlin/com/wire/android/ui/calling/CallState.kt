63395a885c7a56eaa097b1f7257a06a86cbbc7fa
Oussama Hassine <oussama.has100@gmail.com>
2022-06-02 14:13:09 +0200

    feat: Handle incoming/outgoing call buttons - PART 0 : move duplicate code to a shared viewModel (AR-1364) (#629)

--- app/src/main/kotlin/com/wire/android/ui/calling/CallState.kt
+++ app/src/main/kotlin/com/wire/android/ui/calling/CallState.kt
@@ -1,15 +1,14 @@
-package com.wire.android.ui.calling.initiating
+package com.wire.android.ui.calling
 
 import com.wire.android.model.ImageAsset.UserAvatarAsset
-import com.wire.android.ui.calling.ConversationName
 import com.wire.kalium.logic.data.call.ConversationType
 
-data class InitiatingCallState(
+data class CallState(
     val conversationName: ConversationName? = null,
     val avatarAssetId: UserAvatarAsset? = null,
-    val isMuted: Boolean = true,
+    val isMuted: Boolean = false,
     val isCameraOn: Boolean = false,
-    val isCameraFlipped: Boolean = false,
     val isSpeakerOn: Boolean = false,
+    val isCameraFlipped: Boolean = false,
     val conversationType: ConversationType = ConversationType.OneOnOne
 )
--- app/src/test/kotlin/com/wire/android/ui/calling/initiating/InitiatingCallViewModelTest.kt
+++ app/src/test/kotlin/com/wire/android/ui/calling/initiating/InitiatingCallViewModelTest.kt
@@ -37,13 +37,10 @@ class InitiatingCallViewModelTest {
     private lateinit var startCall: StartCallUseCase
 
     @MockK
-    private lateinit var endCall: EndCallUseCase
+    private lateinit var observeConversationDetailsUseCase: ObserveConversationDetailsUseCase
 
     @MockK
     private lateinit var callRinger: CallRinger
-
-    @MockK
-    private lateinit var observeConversationDetails: ObserveConversationDetailsUseCase
 
     private lateinit var initiatingCallViewModel: InitiatingCallViewModel
 
@@ -60,25 +57,11 @@ class InitiatingCallViewModelTest {
         initiatingCallViewModel = InitiatingCallViewModel(
             savedStateHandle = savedStateHandle,
             navigationManager = navigationManager,
+            conversationDetails = observeConversationDetailsUseCase,
             allCalls = allCalls,
             startCall = startCall,
-            endCall = endCall,
-            conversationDetails = observeConversationDetails,
             callRinger = callRinger
         )
-    }
-
-    @Test
-    fun `given active call, when user end call, then invoke endCall useCase`() {
-        coEvery { navigationManager.navigateBack() } returns Unit
-        coEvery { endCall.invoke(any()) } returns Unit
-        every { callRinger.stop() } returns Unit
-
-        runTest { initiatingCallViewModel.hangUpCall() }
-
-        coVerify(exactly = 1) { endCall.invoke(any()) }
-        coVerify(exactly = 1) { callRinger.stop() }
-        coVerify(exactly = 1) { navigationManager.navigateBack() }
     }
 
 }

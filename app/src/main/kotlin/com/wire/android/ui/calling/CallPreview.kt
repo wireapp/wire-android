786a25fc810e50b6774a39b49708e2c84bcc6df1
Oussama <oussama.has100@gmail.com>
2022-06-08 09:50:34 +0200

    chore: resolve conflicts

--- app/src/main/kotlin/com/wire/android/ui/calling/CallPreview.kt
+++ app/src/main/kotlin/com/wire/android/ui/calling/CallPreview.kt
@@ -4,11 +4,10 @@ import android.view.View
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.Text
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier

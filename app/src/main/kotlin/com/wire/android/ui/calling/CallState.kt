83ea387144cc102f1cd1280138d25aeaf866a7f9
Oussama Hassine <oussama.has100@gmail.com>
2022-06-20 10:25:08 +0200

    feat: 1:1 call participants view - Part 2: display participants info (AR-1370) (#665)

--- app/src/main/kotlin/com/wire/android/ui/calling/CallState.kt
+++ app/src/main/kotlin/com/wire/android/ui/calling/CallState.kt
@@ -2,10 +2,12 @@ package com.wire.android.ui.calling
 
 import com.wire.android.model.ImageAsset.UserAvatarAsset
 import com.wire.kalium.logic.data.call.ConversationType
+import com.wire.kalium.logic.data.call.Participant
 
 data class CallState(
     val conversationName: ConversationName? = null,
     val avatarAssetId: UserAvatarAsset? = null,
+    val participants: List<Participant> = listOf(),
     val isMuted: Boolean = false,
     val isCameraOn: Boolean = false,
     val isSpeakerOn: Boolean = false,

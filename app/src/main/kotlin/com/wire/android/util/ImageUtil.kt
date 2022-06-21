17908a2879fe88e127a95293ff2411dfcbf4bde2
Tommaso Piazza <196761+tmspzz@users.noreply.github.com>
2022-06-21 17:21:47 +0200

    chore: address PR comments

--- app/src/main/kotlin/com/wire/android/util/ImageUtil.kt
+++ app/src/main/kotlin/com/wire/android/util/ImageUtil.kt
@@ -16,7 +16,7 @@ import kotlin.math.sqrt
 const val DEFAULT_IMAGE_MIME_TYPE = "image/jpeg"
 const val DEFAULT_FILE_MIME_TYPE = "file/*"
 
-sealed class ImageUtil private constructor() {
+object ImageUtil {
 
     /**
      * The use size class of an image. Medium for images in conversations, small for Avatars
@@ -25,79 +25,89 @@ sealed class ImageUtil private constructor() {
         Medium, Small
     }
 
-    companion object {
-        // We will not require scaling if the image is within 30% of the target size
-        private const val SCALE_FUDGE_FACTOR = 1.3F
 
-        @Suppress("MagicNumber")
-        private fun dimensionForSizeClass(sizeClass: ImageSizeClass): Float {
-            return when (sizeClass) {
-                ImageSizeClass.Small -> 280F
-                ImageSizeClass.Medium -> 1448F
-            }
-        }
+    /**
+     * Attempts to read the width and height of an image represented by the input parameter
+     */
+    fun extractImageWidthAndHeight(imageRawData: ByteArray): Pair<Int, Int> {
+        val exifInterface = ExifInterface(imageRawData.inputStream())
+        val exifWidth: Int = exifInterface.getAttributeInt(ExifInterface.TAG_IMAGE_WIDTH, 0)
+        val exifHeight: Int = exifInterface.getAttributeInt(ExifInterface.TAG_IMAGE_LENGTH, 0)
+        return exifWidth to exifHeight
+    }
 
-        @Suppress("MagicNumber")
-        private fun qualityForSizeClass(sizeClass: ImageSizeClass): Int {
-            return when (sizeClass) {
-                ImageSizeClass.Small -> 75
-                ImageSizeClass.Medium -> 45
-            }
-        }
-
-        fun extractImageWidthAndHeight(imageRawData: ByteArray): Pair<Int, Int> {
-            val exifInterface = ExifInterface(imageRawData.inputStream())
-            val exifWidth: Int = exifInterface.getAttributeInt(ExifInterface.TAG_IMAGE_WIDTH, 0)
-            val exifHeight: Int = exifInterface.getAttributeInt(ExifInterface.TAG_IMAGE_LENGTH, 0)
-            return exifWidth to exifHeight
-        }
-
-        /**
-         * Resamples, downscales and normalizes rotation of an image based on it's intended [ImageSizeClass] use.
-         * Works on JPEGS Only.
-         *
-         * @param byteArray the ByteArray representing the image
-         * @param sizeClass the indented size class use case
-         * @return ByteArray the resampled, downscaled and rotation normalized image
-         */
-        fun resample(byteArray: ByteArray, sizeClass: ImageSizeClass): ByteArray {
-            val bitmap = BitmapFactory.decodeByteArray(byteArray, 0, byteArray.size)
+    /**
+     * Resamples, downscales and normalizes rotation of an image based on its intended [ImageSizeClass] use.
+     * Works on JPEGS Only.
+     *
+     * @param byteArray the ByteArray representing the image
+     * @param sizeClass the indented size class use case
+     * @return ByteArray the resampled, downscaled and rotation normalized image
+     */
+    fun resample(byteArray: ByteArray, sizeClass: ImageSizeClass): ByteArray {
+        val bitmap = BitmapFactory.decodeByteArray(byteArray, 0, byteArray.size)
+        val targetDimension = dimensionForSizeClass(sizeClass)
+        if (shouldScale(bitmap, targetDimension)) {
             val exifInterface = ExifInterface(byteArray.inputStream())
-            val targetDimension = dimensionForSizeClass(sizeClass)
-            if (shouldScale(bitmap, targetDimension)) {
-                val size = scaledSizeForBitmap(bitmap, targetDimension)
-                val resizedImage = Bitmap
-                    .createScaledBitmap(bitmap, size.first.toInt(), size.second.toInt(), true)
-                    .rotateImageToNormalOrientation(exifInterface)
-                val output = ByteArrayOutputStream()
-                resizedImage.compress(Bitmap.CompressFormat.JPEG, qualityForSizeClass(sizeClass), output)
-                return output.toByteArray()
-            }
-            return byteArray
-        }
-
-        @Suppress("MagicNumber")
-        private fun scaledSizeForBitmap(bitmap: Bitmap, targetDimension: Float): Pair<Float, Float> {
-            val scale1 = java.lang.Float.max(targetDimension / bitmap.width, targetDimension / bitmap.height)
-            val scale2 = targetDimension / sqrt((bitmap.width * bitmap.height).toFloat())
-            val scale: Float = if (scale2.isFinite() && (scale2 < scale1)) {
-                0.5F * (scale2 + scale1)
+            val size = scaledSizeForBitmap(bitmap, targetDimension)
+            val resizedImage = Bitmap
+                .createScaledBitmap(bitmap, size.first.toInt(), size.second.toInt(), true)
+                .rotateImageToNormalOrientation(exifInterface)
+            val output = ByteArrayOutputStream()
+            if (resizedImage.hasAlpha()) {
+                resizedImage.compress(Bitmap.CompressFormat.PNG, 0, output)
             } else {
-                scale1
+                resizedImage.compress(Bitmap.CompressFormat.JPEG, compressionFactorForSizeClass(sizeClass), output)
             }
-            val width = ceil(scale * bitmap.width)
-            val height = round(width / bitmap.width * bitmap.height)
-            return width to height
+            return output.toByteArray()
         }
+        return byteArray
+    }
 
-        private fun shouldScale(bitmap: Bitmap, targetDimension: Float): Boolean {
-            val oneSizeIsTooLong = (bitmap.width > SCALE_FUDGE_FACTOR * targetDimension)
-                    || (bitmap.height > SCALE_FUDGE_FACTOR * targetDimension)
-            val maxPixelCount = SCALE_FUDGE_FACTOR * targetDimension * targetDimension
-            val pixelCountIsTooBig = bitmap.width * bitmap.height > maxPixelCount
-            return (oneSizeIsTooLong && pixelCountIsTooBig)
+    // region Private
+
+    // We will not require scaling if the image is within 30% of the target size
+    private const val SCALE_FUDGE_FACTOR = 1.3F
+
+    @Suppress("MagicNumber")
+    private fun dimensionForSizeClass(sizeClass: ImageSizeClass): Float {
+        return when (sizeClass) {
+            ImageSizeClass.Small -> 280F
+            ImageSizeClass.Medium -> 1448F
         }
     }
+
+    @Suppress("MagicNumber")
+    private fun compressionFactorForSizeClass(sizeClass: ImageSizeClass): Int {
+        return when (sizeClass) {
+            ImageSizeClass.Small -> 75
+            ImageSizeClass.Medium -> 45
+        }
+    }
+
+    @Suppress("MagicNumber")
+    private fun scaledSizeForBitmap(bitmap: Bitmap, targetDimension: Float): Pair<Float, Float> {
+        val scale1 = java.lang.Float.max(targetDimension / bitmap.width, targetDimension / bitmap.height)
+        val scale2 = targetDimension / sqrt((bitmap.width * bitmap.height).toFloat())
+        val scale: Float = if (scale2.isFinite() && (scale2 < scale1)) {
+            0.5F * (scale2 + scale1)
+        } else {
+            scale1
+        }
+        val width = ceil(scale * bitmap.width)
+        val height = round(width / bitmap.width * bitmap.height)
+        return width to height
+    }
+
+    private fun shouldScale(bitmap: Bitmap, targetDimension: Float): Boolean {
+        val oneSizeIsTooLong = (bitmap.width > SCALE_FUDGE_FACTOR * targetDimension)
+                || (bitmap.height > SCALE_FUDGE_FACTOR * targetDimension)
+        val maxPixelCount = SCALE_FUDGE_FACTOR * targetDimension * targetDimension
+        val pixelCountIsTooBig = bitmap.width * bitmap.height > maxPixelCount
+        return (oneSizeIsTooLong && pixelCountIsTooBig)
+    }
+
+    // endregion
 }
 
 /**

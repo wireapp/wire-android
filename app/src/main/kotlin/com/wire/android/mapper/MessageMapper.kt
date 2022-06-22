d75d5bd4797020fcfd29b88a1e717aa2affde90e
Mateusz <m.pachulski94@gmail.com>
2022-06-22 11:00:54 +0200

    fix user type

--- app/src/main/kotlin/com/wire/android/mapper/MessageMapper.kt
+++ app/src/main/kotlin/com/wire/android/mapper/MessageMapper.kt
@@ -56,7 +56,7 @@ class MessageMapper @Inject constructor(
                 messageHeader = MessageHeader(
                     // TODO: Designs for deleted users?
                     username = sender?.name?.let { UIText.DynamicString(it) } ?: UIText.StringResource(R.string.member_name_deleted_label),
-                    membership = if (sender is MemberDetails.Other) userTypeMapper.toMembership(sender.userType) else Membership.None,
+                    membership = if (sender is MemberDetails.Other) userTypeMapper.toMembership(sender.otherUser.userType) else Membership.None,
                     isLegalHold = false,
                     time = message.date.uiMessageDateTime() ?: "",
                     messageStatus = if (message.status == Message.Status.FAILED) MessageStatus.SendFailure else MessageStatus.Untouched,

9689eafcd80800b00113da1cd3fba8c35eb24367
Mojtaba Chenani <chenani@outlook.com>
2022-06-23 16:28:44 +0200

    feat(new-mls-conversation): add protocol to new group (AR-1775) #573 (#647)
    * feat: add draft drop-down to create-conversation-screen
    
    * feat: commite changes
    
    * feat: clean up wireDropDown
    
    * chore: refactor wireDropDown
    
    * update kalium reference
    
    * chore: add default indicator to the selected item
    
    * chore: refactor naming and remove unneeded files
    
    * fix: imports after refactoring
    
    * refactor: replaced dropDownMenu with custom list
    
    * style: fix selected item colors
    
    * chore: add space before default indicator
    
    * chore: add animations to expand/collapse dropdown
    
    * chore: remove unneeded imports
    
    * chore: refactor styles and apply changes from PR review

--- app/src/main/kotlin/com/wire/android/ui/home/newconversation/newgroup/NewGroupState.kt
+++ app/src/main/kotlin/com/wire/android/ui/home/newconversation/newgroup/NewGroupState.kt
@@ -1,9 +1,11 @@
 package com.wire.android.ui.home.newconversation.newgroup
 
 import androidx.compose.ui.text.input.TextFieldValue
+import com.wire.kalium.logic.data.conversation.ConversationOptions
 
 data class NewGroupState(
     val groupName: TextFieldValue = TextFieldValue(""),
+    var groupProtocol: ConversationOptions.Protocol = ConversationOptions.Protocol.PROTEUS,
     val animatedGroupNameError: Boolean = false,
     val continueEnabled: Boolean = false,
     val isLoading : Boolean = false,

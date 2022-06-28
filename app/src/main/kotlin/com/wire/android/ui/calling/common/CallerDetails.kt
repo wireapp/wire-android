57a5948cd33974bab84db16431a4ab22e4719cef
Oussama <oussama.has100@gmail.com>
2022-06-28 17:47:18 +0200

    fix(calling): update calling screen to fit design specs

--- app/src/main/kotlin/com/wire/android/ui/calling/common/CallerDetails.kt
+++ app/src/main/kotlin/com/wire/android/ui/calling/common/CallerDetails.kt
@@ -16,7 +16,6 @@ import androidx.compose.ui.draw.rotate
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.unit.dp
 import com.wire.android.R
 import com.wire.android.model.ImageAsset
 import com.wire.android.model.UserAvatarData
@@ -41,7 +40,7 @@ fun CallerDetails(
         val context = LocalContext.current
         IconButton(
             modifier = Modifier
-                .padding(top = 20.dp, start = 5.dp)
+                .padding(top = dimensions().spacing16x, start = dimensions().spacing6x)
                 .align(Alignment.Start)
                 .rotate(180f),
             onClick = {

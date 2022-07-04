f809120562f35f15fe82bedca0b72d19da459a65
Mateusz <m.pachulski94@gmail.com>
2022-07-04 17:05:06 +0200

    fix: handle failure state when getting all known contacts

---
+++ app/src/main/kotlin/com/wire/android/ui/home/newconversation/search/widget/SearchFailureWidget.kt
@@ -0,0 +1,30 @@
+package com.wire.android.ui.home.newconversation.search.widget
+
+import androidx.annotation.StringRes
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
+import com.wire.android.ui.theme.wireColorScheme
+import com.wire.android.ui.theme.wireTypography
+
+@Composable
+fun SearchFailureBox(@StringRes failureMessage: Int){
+    Box(
+        Modifier
+            .fillMaxWidth()
+            .height(224.dp)
+    ) {
+        Text(
+            stringResource(id = failureMessage),
+            modifier = Modifier.align(Alignment.Center),
+            style = MaterialTheme.wireTypography.label04.copy(color = MaterialTheme.wireColorScheme.secondaryText)
+        )
+    }
+}

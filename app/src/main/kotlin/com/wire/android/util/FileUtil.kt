268919a727942cad533ff5ea2e842a1fbba4bdd6
Gonzalo Gran Crespo <gongracr@gmail.com>
2022-07-06 13:52:11 +0200

    Removed unused method

--- app/src/main/kotlin/com/wire/android/util/FileUtil.kt
+++ app/src/main/kotlin/com/wire/android/util/FileUtil.kt
@@ -115,40 +115,6 @@ fun Uri.copyToTempPath(context: Context, tempCachePath: Path): Long {
     return size
 }
 
-fun Uri.length(contentResolver: ContentResolver): Long {
-    val assetFileDescriptor = try {
-        contentResolver.openAssetFileDescriptor(this, "r")
-    } catch (e: FileNotFoundException) {
-        null
-    }
-
-    // uses ParcelFileDescriptor#getStatSize underneath if failed
-    val length = assetFileDescriptor?.use { it.length } ?: -1L
-    if (length != -1L) {
-        return length
-    }
-
-    // if "content://" uri scheme, try contentResolver table
-    if (scheme.equals(ContentResolver.SCHEME_CONTENT)) {
-        return contentResolver.query(this, arrayOf(OpenableColumns.SIZE), null, null, null)
-            ?.use { cursor ->
-                // maybe shouldn't trust ContentResolver for size: https://stackoverflow.com/questions/48302972/content-resolver-returns-wrong-size
-                val sizeIndex = cursor.getColumnIndex(OpenableColumns.SIZE)
-                if (sizeIndex == -1) {
-                    return@use -1L
-                }
-                cursor.moveToFirst()
-                return try {
-                    cursor.getLong(sizeIndex)
-                } catch (_: Throwable) {
-                    -1L
-                }
-            } ?: -1L
-    } else {
-        return -1L
-    }
-}
-
 fun Context.getFileName(uri: Uri): String? = when (uri.scheme) {
     ContentResolver.SCHEME_CONTENT -> getContentFileName(uri)
     else -> uri.path?.let(::File)?.name

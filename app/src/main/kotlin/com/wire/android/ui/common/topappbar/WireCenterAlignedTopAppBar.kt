7c34b455e6cc58282d8bae10f0a3b424ef6cf17e
Oussama <oussama.has100@gmail.com>
2022-07-18 15:24:12 +0100

    fix(calling): show ellipsis for long names

--- app/src/main/kotlin/com/wire/android/ui/common/topappbar/WireCenterAlignedTopAppBar.kt
+++ app/src/main/kotlin/com/wire/android/ui/common/topappbar/WireCenterAlignedTopAppBar.kt
@@ -3,14 +3,18 @@ package com.wire.android.ui.common.topappbar
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.ColumnScope
 import androidx.compose.foundation.layout.RowScope
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBarColors
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
 import com.wire.android.ui.theme.wireDimensions
 import com.wire.android.ui.theme.wireTypography
 
@@ -18,6 +22,8 @@ import com.wire.android.ui.theme.wireTypography
 fun WireCenterAlignedTopAppBar(
     title: String,
     titleStyle: TextStyle = MaterialTheme.wireTypography.title01,
+    maxLines: Int = 2,
+    titleStartPadding: Dp = 0.dp,
     onNavigationPressed: () -> Unit = {},
     navigationIconType: NavigationIconType? = NavigationIconType.Back,
     elevation: Dp = MaterialTheme.wireDimensions.topBarShadowElevation,
@@ -31,7 +37,14 @@ fun WireCenterAlignedTopAppBar(
     ) {
         Column {
             CenterAlignedTopAppBar(
-                title = { WireTopAppBarTitle(title = title, titleStyle) },
+                title = {
+                    WireTopAppBarTitle(
+                        title = title,
+                        style = titleStyle,
+                        maxLines = maxLines,
+                        titleStartPadding = titleStartPadding
+                    )
+                },
                 navigationIcon = { navigationIconType?.let { NavigationIconButton(iconType = it) { onNavigationPressed() } } },
                 colors = colors,
                 actions = actions
@@ -44,10 +57,15 @@ fun WireCenterAlignedTopAppBar(
 @Composable
 fun WireTopAppBarTitle(
     title: String,
-    style: TextStyle
+    style: TextStyle,
+    maxLines: Int = 2,
+    titleStartPadding: Dp = 0.dp
 ) {
     Text(
+        modifier = Modifier.padding(start = titleStartPadding),
         text = title,
-        style = style
+        style = style,
+        maxLines = maxLines,
+        overflow = TextOverflow.Ellipsis
     )
 }

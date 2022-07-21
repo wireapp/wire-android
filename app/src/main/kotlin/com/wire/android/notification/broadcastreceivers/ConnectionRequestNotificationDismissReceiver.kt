38d725b662c01272f8c263976288de60f54d3d1a
boris <boris.safonov@appunite.com>
2022-07-21 22:32:10 +0300

    fix: Notifications summary fix (#782)
    * fix: Notifications summary fix
    
    * fix: Notifications summary fix: ready for review
    
    * fix: Notifications summary fix: fixed build
    
    * fix: Notifications summary fix: removed useles code

--- app/src/main/kotlin/com/wire/android/notification/broadcastreceivers/ConnectionRequestNotificationDismissReceiver.kt
+++ app/src/main/kotlin/com/wire/android/notification/broadcastreceivers/ConnectionRequestNotificationDismissReceiver.kt
@@ -3,11 +3,11 @@ package com.wire.android.notification.broadcastreceivers
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
+import com.wire.android.appLogger
 import com.wire.android.di.KaliumCoreLogic
 import com.wire.android.util.dispatchers.DispatcherProvider
 import com.wire.kalium.logic.CoreLogic
 import com.wire.kalium.logic.data.id.QualifiedID
-import com.wire.kalium.logic.data.id.parseIntoQualifiedID
 import com.wire.kalium.logic.data.id.parseIntoQualifiedID
 import com.wire.kalium.logic.feature.session.CurrentSessionResult
 import dagger.hilt.android.AndroidEntryPoint
@@ -27,6 +27,7 @@ class ConnectionRequestNotificationDismissReceiver : BroadcastReceiver() {
 
     override fun onReceive(context: Context, intent: Intent) {
         val requesterUserId: String? = intent.getStringExtra(EXTRA_CONNECTION_REQUESTER_USER_ID)
+        appLogger.i("ConnectionRequestNotificationDismissReceiver: onReceive, requesterUserId: $requesterUserId")
         val userId: QualifiedID? =
             intent.getStringExtra(EXTRA_RECEIVER_USER_ID)?.parseIntoQualifiedID()
 

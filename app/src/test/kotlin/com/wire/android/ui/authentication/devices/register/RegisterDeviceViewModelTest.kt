841e7ff18b0dda314978705bb5cbc7002d09bb88
Mohamad Jaara <mohamad.jaara@wire.com>
2022-08-03 10:25:13 +0200

    fix tests

--- app/src/test/kotlin/com/wire/android/ui/authentication/devices/register/RegisterDeviceViewModelTest.kt
+++ app/src/test/kotlin/com/wire/android/ui/authentication/devices/register/RegisterDeviceViewModelTest.kt
@@ -2,6 +2,7 @@ package com.wire.android.ui.authentication.devices.register
 
 import androidx.compose.ui.text.input.TextFieldValue
 import com.wire.android.config.CoroutineTestExtension
+import com.wire.android.config.TestDispatcherProvider
 import com.wire.android.config.mockUri
 import com.wire.android.navigation.BackStackMode
 import com.wire.android.navigation.NavigationCommand
@@ -13,11 +14,11 @@ import com.wire.kalium.logic.NetworkFailure
 import com.wire.kalium.logic.data.client.Client
 import com.wire.kalium.logic.data.client.ClientType
 import com.wire.kalium.logic.data.conversation.ClientId
-import com.wire.kalium.logic.feature.auth.ValidatePasswordUseCase
 import com.wire.kalium.logic.feature.client.RegisterClientResult
 import com.wire.kalium.logic.feature.client.RegisterClientUseCase
 import com.wire.kalium.logic.feature.session.RegisterTokenResult
 import com.wire.kalium.logic.feature.session.RegisterTokenUseCase
+import com.wire.kalium.logic.feature.user.IsPasswordRequiredUseCase
 import io.mockk.MockKAnnotations
 import io.mockk.coEvery
 import io.mockk.coVerify
@@ -44,13 +45,13 @@ class RegisterDeviceViewModelTest {
     private lateinit var navigationManager: NavigationManager
 
     @MockK
-    private lateinit var validatePasswordUseCase: ValidatePasswordUseCase
-
-    @MockK
     private lateinit var registerClientUseCase: RegisterClientUseCase
 
     @MockK
     private lateinit var registerTokenUseCase: RegisterTokenUseCase
+
+    @MockK
+    private lateinit var isPasswordRequiredUseCase: IsPasswordRequiredUseCase
 
     private lateinit var registerDeviceViewModel: RegisterDeviceViewModel
 
@@ -58,13 +59,19 @@ class RegisterDeviceViewModelTest {
     fun setup() {
         MockKAnnotations.init(this)
         mockUri()
+        coEvery { isPasswordRequiredUseCase() } returns IsPasswordRequiredUseCase.Result.Success(true)
         registerDeviceViewModel =
-            RegisterDeviceViewModel(navigationManager, validatePasswordUseCase, registerClientUseCase, registerTokenUseCase)
+            RegisterDeviceViewModel(
+                navigationManager,
+                registerClientUseCase,
+                registerTokenUseCase,
+                isPasswordRequiredUseCase,
+                TestDispatcherProvider()
+            )
     }
 
     @Test
     fun `given empty string, when entering the password to register, then button is disabled`() {
-        coEvery { validatePasswordUseCase(String.EMPTY) } returns false
         registerDeviceViewModel.onPasswordChange(TextFieldValue(String.EMPTY))
         registerDeviceViewModel.state.continueEnabled shouldBeEqualTo false
         registerDeviceViewModel.state.loading shouldBeEqualTo false
@@ -72,7 +79,6 @@ class RegisterDeviceViewModelTest {
 
     @Test
     fun `given non-empty string, when entering the password to register, then button is disabled`() {
-        coEvery { validatePasswordUseCase("abc") } returns true
         registerDeviceViewModel.onPasswordChange(TextFieldValue("abc"))
         registerDeviceViewModel.state.continueEnabled shouldBeEqualTo true
         registerDeviceViewModel.state.loading shouldBeEqualTo false
@@ -80,7 +86,6 @@ class RegisterDeviceViewModelTest {
 
     @Test
     fun `given button is clicked, when registering the client, then show loading`() {
-        coEvery { validatePasswordUseCase(any()) } returns true
         coEvery {
             registerClientUseCase(any())
         } returns RegisterClientResult.Success(CLIENT)
@@ -98,7 +103,6 @@ class RegisterDeviceViewModelTest {
         val scheduler = TestCoroutineScheduler()
         val password = "abc"
         Dispatchers.setMain(StandardTestDispatcher(scheduler))
-        coEvery { validatePasswordUseCase(any()) } returns true
         coEvery {
             registerClientUseCase(
                 any()
@@ -113,7 +117,6 @@ class RegisterDeviceViewModelTest {
 
         runTest { registerDeviceViewModel.onContinue() }
 
-        coVerify(exactly = 1) { validatePasswordUseCase(password) }
         coVerify(exactly = 1) {
             registerClientUseCase(any())
         }
@@ -128,7 +131,6 @@ class RegisterDeviceViewModelTest {
         val scheduler = TestCoroutineScheduler()
         val password = "abc"
         Dispatchers.setMain(StandardTestDispatcher(scheduler))
-        coEvery { validatePasswordUseCase(any()) } returns true
         coEvery {
             registerClientUseCase(any())
         } returns RegisterClientResult.Failure.TooManyClients
@@ -137,7 +139,6 @@ class RegisterDeviceViewModelTest {
 
         runTest { registerDeviceViewModel.onContinue() }
 
-        coVerify(exactly = 1) { validatePasswordUseCase(password) }
         coVerify(exactly = 1) {
             registerClientUseCase(any())
         }
@@ -148,7 +149,6 @@ class RegisterDeviceViewModelTest {
 
     @Test
     fun `given button is clicked, when password is invalid, then UsernameInvalidError is passed`() {
-        coEvery { validatePasswordUseCase(any()) } returns false
         coEvery {
             registerClientUseCase(any())
         } returns RegisterClientResult.Failure.InvalidCredentials
@@ -161,7 +161,6 @@ class RegisterDeviceViewModelTest {
     @Test
     fun `given button is clicked, when request returns Generic error, then GenericError is passed`() {
         val networkFailure = NetworkFailure.NoNetworkConnection(null)
-        coEvery { validatePasswordUseCase(any()) } returns true
         coEvery { registerClientUseCase(any()) } returns
                 RegisterClientResult.Failure.Generic(networkFailure)
 
@@ -175,7 +174,6 @@ class RegisterDeviceViewModelTest {
     @Test
     fun `given dialog is dismissed, when state error is DialogError, then hide error`() {
         val networkFailure = NetworkFailure.NoNetworkConnection(null)
-        coEvery { validatePasswordUseCase(any()) } returns true
         coEvery { registerClientUseCase(any()) } returns
                 RegisterClientResult.Failure.Generic(networkFailure)
 

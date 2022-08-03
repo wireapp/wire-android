c78fb90a4402e6d17e31e6391af20ef2645f5ecb
Boris Safonov <boris.safonov@appunite.com>
2022-08-03 12:16:18 +0300

    feat: Display blocked user properly: small design fix

--- app/src/main/kotlin/com/wire/android/ui/common/BlockedLabel.kt
+++ app/src/main/kotlin/com/wire/android/ui/common/BlockedLabel.kt
@@ -1,6 +1,5 @@
 package com.wire.android.ui.common
 
-import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.padding
@@ -15,32 +14,26 @@ import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import com.wire.android.R
-import com.wire.android.ui.theme.wireColorScheme
-import com.wire.android.ui.theme.wireDimensions
 import com.wire.android.ui.theme.wireTypography
 
 @Composable
 fun BlockedLabel(modifier: Modifier = Modifier) {
     Box(
         modifier = modifier.border(
-            width = MaterialTheme.wireDimensions.spacing1x,
-            shape = RoundedCornerShape(MaterialTheme.wireDimensions.spacing4x),
-            color = MaterialTheme.wireColorScheme.secondaryButtonDisabledOutline
+            width = dimensions().spacing1x,
+            shape = RoundedCornerShape(dimensions().spacing4x),
+            color = colorsScheme().secondaryButtonDisabledOutline
         )
     ) {
         Text(
             text = stringResource(id = R.string.label_user_blocked),
-            color = MaterialTheme.wireColorScheme.labelText,
+            color = colorsScheme().labelText,
             style = MaterialTheme.wireTypography.label03.copy(textAlign = TextAlign.Center),
             maxLines = 1,
             overflow = TextOverflow.Ellipsis,
             modifier = Modifier
                 .wrapContentWidth()
-                .background(
-                    color = MaterialTheme.wireColorScheme.secondaryButtonDisabled,
-                    shape = RoundedCornerShape(MaterialTheme.wireDimensions.corner4x)
-                )
-                .padding(horizontal = MaterialTheme.wireDimensions.spacing4x, vertical = MaterialTheme.wireDimensions.spacing2x)
+                .padding(horizontal = dimensions().spacing4x, vertical = dimensions().spacing2x)
         )
     }
 }

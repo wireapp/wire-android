30a4c845a996a228ed236db705d0f0d7cf764ddf
Jakub Żerko <zerasop@gmail.com>
2022-08-11 11:04:32 +0200

    Imports fix

--- app/src/main/kotlin/com/wire/android/ui/calling/common/CallerDetails.kt
+++ app/src/main/kotlin/com/wire/android/ui/calling/common/CallerDetails.kt
@@ -4,9 +4,7 @@ import android.widget.Toast
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.Text
 import androidx.compose.material3.IconButton
@@ -26,7 +24,6 @@ import com.wire.android.ui.calling.ConversationName
 import com.wire.android.ui.common.MembershipQualifierLabel
 import com.wire.android.ui.common.UserProfileAvatar
 import com.wire.android.ui.common.dimensions
-import com.wire.android.ui.common.spacers.Height8x
 import com.wire.android.ui.common.spacers.VerticalSpace
 import com.wire.android.ui.home.conversationslist.model.Membership
 import com.wire.android.ui.home.conversationslist.model.hasLabel

abba5a41b3b377a8a2d26f97f7af8f6e9255349a
alexandreferris <ferris.alexandre@gmail.com>
2022-08-17 11:56:39 +0200

    chore: add audio permission check in join button view component

--- app/src/main/kotlin/com/wire/android/ui/calling/controlButtons/JoinButton.kt
+++ app/src/main/kotlin/com/wire/android/ui/calling/controlButtons/JoinButton.kt
@@ -10,11 +10,13 @@ import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.Dp
 import com.wire.android.R
+import com.wire.android.appLogger
 import com.wire.android.ui.common.button.WireButtonState
 import com.wire.android.ui.common.dimensions
 import com.wire.android.ui.common.textfield.WirePrimaryButton
 import com.wire.android.ui.theme.wireDimensions
 import com.wire.android.ui.theme.wireTypography
+import com.wire.android.util.permission.rememberCallingRecordAudioBluetoothRequestFlow
 
 @Composable
 fun JoinButton(
@@ -23,8 +25,10 @@ fun JoinButton(
     minHeight: Dp = MaterialTheme.wireDimensions.buttonMinSize.height,
     minWidth: Dp = MaterialTheme.wireDimensions.buttonMinSize.width
 ) {
+    val audioPermissionCheck = AudioBluetoothPermissionCheckFlow { buttonClick() }
+
     WirePrimaryButton(
-        onClick = buttonClick,
+        onClick = { audioPermissionCheck.launch() },
         fillMaxWidth = false,
         shape = RoundedCornerShape(size = MaterialTheme.wireDimensions.corner12x),
         text = stringResource(R.string.calling_button_label_join_call),
@@ -42,6 +46,17 @@ fun JoinButton(
             vertical = dimensions().spacing4x
         )
     )
+}
+
+@Composable
+private fun AudioBluetoothPermissionCheckFlow(
+    onJoinCall: () -> Unit
+) = rememberCallingRecordAudioBluetoothRequestFlow(onAudioBluetoothPermissionGranted = {
+    appLogger.d("Join Call Button - Permissions granted")
+    onJoinCall()
+}) {
+    appLogger.d("Join Call Button - Permissions denied")
+    // TODO: Add a message that user needs permission to join call?
 }
 
 @Preview

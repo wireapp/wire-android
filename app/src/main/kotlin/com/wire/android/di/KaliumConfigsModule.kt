4d089b7ea2598c565957b03db6a6d4d9218f0dea
Mojtaba Chenani <chenani@outlook.com>
2022-08-18 17:30:55 +0200

    update check for internal builds

--- app/src/main/kotlin/com/wire/android/di/KaliumConfigsModule.kt
+++ app/src/main/kotlin/com/wire/android/di/KaliumConfigsModule.kt
@@ -50,8 +50,8 @@ class KaliumConfigsModule {
             appLockTimeout = BuildConfig.APP_LOCK_TIMEOUT,
             // we use upsert, available from SQL3.24, which is supported from Android API30, so for older APIs we have to use SQLCipher
             shouldEncryptData = !BuildConfig.DEBUG || Build.VERSION.SDK_INT < Build.VERSION_CODES.R,
-            lowerKeyPackageLimits = BuildConfig.DEBUG,
-            lowerKeyingMaterialsUpdateThreshold = BuildConfig.DEBUG
+            lowerKeyPackageLimits = BuildConfig.FLAVOR in setOf("dev", "internal"),
+            lowerKeyingMaterialsUpdateThreshold = BuildConfig.FLAVOR in setOf("dev", "internal")
         )
     }
 

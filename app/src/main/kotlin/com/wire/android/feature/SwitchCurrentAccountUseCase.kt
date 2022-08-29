11b5e29e9fa78743a1bcbbc0e2a984f73552c538
Mohamad Jaara <mohamad.jaara@wire.com>
2022-08-29 21:48:29 +0200

    fix string res

--- app/src/main/kotlin/com/wire/android/feature/SwitchCurrentAccountUseCase.kt
+++
@@ -1,6 +0,0 @@
-package com.wire.android.feature
-
-import com.wire.kalium.logic.data.user.UserId
-import com.wire.kalium.logic.feature.session.UpdateCurrentSessionUseCase
-import dagger.hilt.android.scopes.ViewModelScoped
-import javax.inject.Inject

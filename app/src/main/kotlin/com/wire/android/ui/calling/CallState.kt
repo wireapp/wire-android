a63c5f8c62a89ac20f0f0359eab17236af388423
Oussama <oussama.has100@gmail.com>
2022-09-02 17:30:51 +0200

    fix: join group a call with microphone enabled by default

--- app/src/main/kotlin/com/wire/android/ui/calling/CallState.kt
+++ app/src/main/kotlin/com/wire/android/ui/calling/CallState.kt
@@ -4,11 +4,13 @@ import com.wire.android.model.ImageAsset.UserAvatarAsset
 import com.wire.android.ui.calling.model.UICallParticipant
 import com.wire.android.ui.home.conversationslist.model.Membership
 import com.wire.kalium.logic.data.call.ConversationType
+import com.wire.kalium.logic.feature.call.CallStatus
 import com.wire.kalium.logic.feature.conversation.SecurityClassificationType
 
 data class CallState(
     val conversationName: ConversationName? = null,
     val callerName: String? = null,
+    val callStatus: CallStatus = CallStatus.CLOSED,
     val avatarAssetId: UserAvatarAsset? = null,
     val participants: List<UICallParticipant> = listOf(),
     val isMuted: Boolean? = null,
--- app/src/main/kotlin/com/wire/android/ui/calling/SharedCallingViewModel.kt
+++ app/src/main/kotlin/com/wire/android/ui/calling/SharedCallingViewModel.kt
@@ -154,12 +154,15 @@ class SharedCallingViewModel @Inject constructor(
     }
 
     private suspend fun observeOnMute() {
-        snapshotFlow { callState.isMuted }.collectLatest { isMute ->
-            isMute?.let {
-                if (it) {
-                    muteCall(conversationId)
-                } else {
-                    unMuteCall(conversationId)
+        //We should only mute established calls
+        snapshotFlow { callState.isMuted to callState.callStatus }.collectLatest { isMute ->
+            if (isMute.second == CallStatus.ESTABLISHED) {
+                isMute.first?.let {
+                    if (it) {
+                        muteCall(conversationId)
+                    } else {
+                        unMuteCall(conversationId)
+                    }
                 }
             }
         }
@@ -173,6 +176,7 @@ class SharedCallingViewModel @Inject constructor(
                         call.status != CallStatus.MISSED
             }?.let { call ->
                 callState = callState.copy(
+                    callStatus = call.status,
                     callerName = call.callerName,
                     isMuted = call.isMuted,
                     isCameraOn = call.isCameraOn,
@@ -192,6 +196,7 @@ class SharedCallingViewModel @Inject constructor(
     fun hangUpCall() {
         viewModelScope.launch {
             endCall(conversationId)
+            muteCall(conversationId)
             callRinger.stop()
         }
     }

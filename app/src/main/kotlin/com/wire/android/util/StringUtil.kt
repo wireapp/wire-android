845e0679259b98b7efe3d504c64d69d2c3af611f
Tommaso Piazza <196761+tmspzz@users.noreply.github.com>
2022-09-13 16:05:54 +0200

    Update StringUtil.kt

--- app/src/main/kotlin/com/wire/android/util/StringUtil.kt
+++ app/src/main/kotlin/com/wire/android/util/StringUtil.kt
@@ -9,6 +9,7 @@ fun String?.orDefault(default: String) = this ?: default
 
 public inline fun String.ifNotEmpty(transform: () -> String): String = if (!isEmpty()) transform() else this
 
+@Suppress("MagicNumber")
 fun String.sha256(): String {
     val md = MessageDigest.getInstance("SHA-256")
     return BigInteger(1, md.digest(toByteArray())).toString(16).padStart(32, '0')

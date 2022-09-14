444ecf9d660e9f2cbf1781a0de7f043ffbedf539
Oussama Hassine <oussama.has100@gmail.com>
2022-09-14 09:59:39 +0200

    chore: upgrade accompanist to 0.25.0 (#967)
    * chore: upgrade accompanist to 0.25.0
    
    * chore: update kalium

--- app/src/main/kotlin/com/wire/android/ui/authentication/login/LoginScreen.kt
+++ app/src/main/kotlin/com/wire/android/ui/authentication/login/LoginScreen.kt
@@ -2,11 +2,11 @@ package com.wire.android.ui.authentication.login
 
 import androidx.annotation.StringRes
 import androidx.compose.foundation.ExperimentalFoundationApi
-import androidx.compose.foundation.gestures.LocalOverScrollConfiguration
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.LocalOverscrollConfiguration
 import androidx.compose.material.ExperimentalMaterialApi
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
@@ -124,7 +124,7 @@ private fun LoginContent(
         if (loginState.showServerVersionNotSupportedDialog) {
             ServerVersionNotSupportedDialog(onClose = viewModel::dismissApiVersionNotSupportedDialog)
         }
-        CompositionLocalProvider(LocalOverScrollConfiguration provides null) {
+        CompositionLocalProvider(LocalOverscrollConfiguration provides null) {
             HorizontalPager(
                 state = pagerState,
                 count = LoginTabItem.values().size,

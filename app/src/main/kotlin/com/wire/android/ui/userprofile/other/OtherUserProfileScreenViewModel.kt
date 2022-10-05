1c5a3f9e410813935f9efedd81778feda9f04f54
Mateusz <m.pachulski94@gmail.com>
2022-10-05 13:45:47 +0200

    fix: other user profile flicker (#1062)
    * fix/user_flicker
    
    * remove the data class
    
    * fix
    
    * update ref
    
    * update ref
    
    Co-authored-by: mateusz.pachulski <mateusz.pachulski@appunite.com>

--- app/src/main/kotlin/com/wire/android/ui/userprofile/other/OtherUserProfileScreenViewModel.kt
+++ app/src/main/kotlin/com/wire/android/ui/userprofile/other/OtherUserProfileScreenViewModel.kt
@@ -157,7 +157,6 @@ class OtherUserProfileScreenViewModel @Inject constructor(
 
                             // TODO yamil: this block could be removed from here. we should loaded on user click
                             observeConversationSheetContentIfNeeded(otherUser, userAvatarAsset)
-                            observeGroupStateIfNeeded()
 
                             state = state.copy(
                                 isDataLoading = false,

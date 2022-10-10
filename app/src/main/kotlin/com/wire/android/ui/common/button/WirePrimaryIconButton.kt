81f9a1eba0bfc93a2a0bdb4e10a00b4903e66d0c
Michał Saleniuk <30429749+saleniuk@users.noreply.github.com>
2022-10-10 15:01:02 +0200

    chore: block selected actions until the sync completes [AR-2443] (#1038)
    * chore: block selected actions until the sync completes [AR-2443]
    
    * fix tests
    
    * chore: show you in participant list (#1042)
    
    * add a you label to participant list
    
    * add you label to the self user in participant list
    
    * update kalium ref
    
    * fix after merge
    
    * changes after review
    
    Co-authored-by: Mohammed Mokresh <mokresh.mohammed@gmail.com>

--- app/src/main/kotlin/com/wire/android/ui/common/button/WirePrimaryIconButton.kt
+++ app/src/main/kotlin/com/wire/android/ui/common/button/WirePrimaryIconButton.kt
@@ -4,37 +4,51 @@ import androidx.annotation.DrawableRes
 import androidx.annotation.StringRes
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Shape
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import com.wire.android.ui.common.dimensions
+import com.wire.android.ui.common.textfield.WirePrimaryButton
+import com.wire.android.ui.theme.wireDimensions
 
 @Composable
-fun WireIconButton(
+fun WirePrimaryIconButton(
     onButtonClicked: () -> Unit,
     @DrawableRes iconResource: Int,
     @StringRes contentDescription: Int,
+    shape: Shape = RoundedCornerShape(MaterialTheme.wireDimensions.buttonCornerSize),
+    minHeight: Dp = dimensions().spacing32x,
+    minWidth: Dp = dimensions().spacing40x,
     state: WireButtonState = WireButtonState.Default,
+    colors: WireButtonColors = wirePrimaryButtonColors(),
+    blockUntilSynced: Boolean = false,
     modifier: Modifier = Modifier
 ) {
-    WireSecondaryButton(
+    WirePrimaryButton(
         onClick = onButtonClicked,
         leadingIcon = {
             Icon(
                 painter = painterResource(id = iconResource),
                 contentDescription = stringResource(contentDescription),
-                modifier = modifier
-                    .size(dimensions().wireIconButtonSize)
+                modifier = Modifier.size(dimensions().wireIconButtonSize)
             )
         },
-        minHeight = dimensions().spacing32x,
-        minWidth = dimensions().spacing40x,
+        shape = shape,
+        minHeight = minHeight,
+        minWidth = minWidth,
         contentPadding = PaddingValues(horizontal = 0.dp, vertical = 0.dp),
         leadingIconAlignment = IconAlignment.Center,
         state = state,
-        fillMaxWidth = false
+        colors = colors,
+        blockUntilSynced = blockUntilSynced,
+        fillMaxWidth = false,
+        modifier = modifier
     )
 }

258beb816be1f8a8d8a72458035a9ff66bbf4ac1
Mateusz <m.pachulski94@gmail.com>
2022-10-17 17:20:33 +0300

    add preview

--- app/src/main/kotlin/com/wire/android/ui/home/settings/backup/BackupAndRestoreScreen.kt
+++ app/src/main/kotlin/com/wire/android/ui/home/settings/backup/BackupAndRestoreScreen.kt
@@ -23,6 +23,7 @@ import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.input.TextFieldValue
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import com.wire.android.R
@@ -291,9 +292,20 @@ fun RestoreDialogFlow(
         onCancelBackupRestore()
     }
 }
-//
-//@Preview
-//@Composable
-//fun BackupAndRestoreScreenPreview() {
-//    BackupAndRestoreContent({}, {}, {}, {}, {}, {})
-//}
+
+@Preview
+@Composable
+fun BackupAndRestoreScreenPreview() {
+    BackupAndRestoreContent(
+        backUpAndRestoreState = BackupAndRestoreState.INITIAL_STATE,
+        onValidateBackupPassword = {},
+        onCreateBackup = {},
+        onSaveBackup = {},
+        onCancelBackupCreation = {},
+        onCancelBackupRestore = {},
+        onChooseBackupFile = {},
+        onRestoreBackup = {},
+        onOpenConversations = {},
+        onBackPressed = {}
+    )
+}

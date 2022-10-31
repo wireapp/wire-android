10a54b8f8eddb96a719b429bc2380a5f8559eaec
Oussama <oussama.has100@gmail.com>
2022-10-31 09:32:37 +0100

    chore: cleanup

--- app/src/main/kotlin/com/wire/android/ui/home/messagecomposer/MessageComposer.kt
+++ app/src/main/kotlin/com/wire/android/ui/home/messagecomposer/MessageComposer.kt
@@ -13,7 +13,6 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.BoxWithConstraints
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.heightIn

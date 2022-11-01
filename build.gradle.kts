459fd15107127fd5f05c2a2708268d8324b623db
Jacob Persson <7156+typfel@users.noreply.github.com>
2022-11-01 16:49:13 +0100

    chore: add GitHub Packages mvn repo

--- build.gradle.kts
+++ build.gradle.kts
@@ -14,7 +14,13 @@ allprojects {
     repositories {
         google()
         mavenCentral()
-        maven(url = "https://raw.githubusercontent.com/wireapp/wire-maven/main/releases")
+        maven {
+            url = uri("https://maven.pkg.github.com/wireapp/core-crypto")
+            credentials {
+                username = property("github.package_registry.user") as? String ?: System.getenv("GITHUB_USER")
+                password = property("github.package_registry.token") as? String ?: System.getenv("GITHUB_TOKEN")
+            }
+        }
         maven { url = java.net.URI("https://jitpack.io") }
         maven(url = "https://oss.sonatype.org/content/repositories/snapshots")
     }

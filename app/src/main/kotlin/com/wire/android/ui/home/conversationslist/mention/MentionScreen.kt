01c788ce990f8924bd2d600afa1d3431d37852a5
Mateusz <m.pachulski94@gmail.com>
2022-11-11 14:48:23 +0100

    test

--- app/src/main/kotlin/com/wire/android/ui/home/conversationslist/mention/MentionScreen.kt
+++ app/src/main/kotlin/com/wire/android/ui/home/conversationslist/mention/MentionScreen.kt
@@ -3,7 +3,6 @@ package com.wire.android.ui.home.conversationslist.mention
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.LazyListState
-import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
@@ -13,6 +12,7 @@ import com.wire.android.ui.home.conversationslist.folderWithElements
 import com.wire.android.ui.home.conversationslist.model.ConversationItem
 import com.wire.kalium.logic.data.id.ConversationId
 import com.wire.kalium.logic.data.user.UserId
+import androidx.compose.foundation.lazy.rememberLazyListState
 
 @Composable
 fun MentionScreen(
@@ -24,7 +24,7 @@ fun MentionScreen(
     openConversationNotificationsSettings: (ConversationItem) -> Unit,
     onJoinCall: (ConversationId) -> Unit
 ) {
-    val lazyListState = androidx.compose.foundation.lazy.rememberLazyListState()
+    val lazyListState = rememberLazyListState()
 
     MentionContent(
         lazyListState = lazyListState,

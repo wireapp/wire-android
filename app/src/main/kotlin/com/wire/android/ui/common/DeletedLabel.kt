b47e964c986a70d69af6d32adb0963be96df2a54
Vitor Hugo Schwaab <vitor@schwaab.xyz>
2022-11-21 13:01:13 +0100

    refactor: remove unnecessary local text style

--- app/src/main/kotlin/com/wire/android/ui/common/DeletedLabel.kt
+++ app/src/main/kotlin/com/wire/android/ui/common/DeletedLabel.kt
@@ -6,11 +6,9 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material3.LocalTextStyle
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
@@ -28,29 +26,25 @@ fun StatusBox(
     statusText: String,
     modifier: Modifier = Modifier
 ) {
-    CompositionLocalProvider(
-        LocalTextStyle provides MaterialTheme.typography.labelSmall
-    ) {
-        Box(
-            modifier = modifier
-                .wrapContentSize()
-                .border(
-                    BorderStroke(
-                        width = 1.dp,
-                        color = MaterialTheme.wireColorScheme.divider
-                    ),
-                    shape = RoundedCornerShape(size = dimensions().spacing4x)
-                )
-                .padding(
-                    horizontal = dimensions().spacing4x,
-                    vertical = dimensions().spacing2x
-                )
-        ) {
-            Text(
-                text = statusText,
-                style = LocalTextStyle.current.copy(color = MaterialTheme.wireColorScheme.labelText)
+    Box(
+        modifier = modifier
+            .wrapContentSize()
+            .border(
+                BorderStroke(
+                    width = 1.dp,
+                    color = MaterialTheme.wireColorScheme.divider
+                ),
+                shape = RoundedCornerShape(size = dimensions().spacing4x)
             )
-        }
+            .padding(
+                horizontal = dimensions().spacing4x,
+                vertical = dimensions().spacing2x
+            )
+    ) {
+        Text(
+            text = statusText,
+            style = typography().label03.copy(color = MaterialTheme.wireColorScheme.labelText)
+        )
     }
 }
 

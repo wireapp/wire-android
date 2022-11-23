f93b2b0b5dc6b9cf101a1322dd3c162d2993f1c0
Mateusz <m.pachulski94@gmail.com>
2022-11-23 13:18:30 +0100

    Ready for review

--- app/src/main/kotlin/com/wire/android/ui/home/conversationslist/common/ReplyMessage.kt
+++ app/src/main/kotlin/com/wire/android/ui/home/conversationslist/common/ReplyMessage.kt
@@ -22,6 +22,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.ColorFilter
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextOverflow
@@ -56,7 +57,9 @@ fun ReplyMessage(
                         modifier = Modifier
                             .width(40.dp)
                             .height(40.dp)
-                            .clip(RoundedCornerShape(12.dp))
+                            .clip(RoundedCornerShape(8.dp)),
+                        alignment = Alignment.Center,
+                        contentScale = ContentScale.Crop
                     )
                 },
                 onCancelReply = onCancelReply

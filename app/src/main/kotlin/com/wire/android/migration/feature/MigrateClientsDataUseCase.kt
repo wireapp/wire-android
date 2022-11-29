15736a5f2013b444c0b45a48cb4ed4314b78f297
Jacob Persson <7156+typfel@users.noreply.github.com>
2022-11-29 16:32:07 +0100

    fix: client migration from old Scala app (#1190)
    * fix: replace deleted persistRegisteredClientIdUseCase with  importClient
    
    * chore: update kalium
    
    * fix: replace RegisterClientUseCase with GetOrRegisterClientUseCase
    
    * fix: replace RegisterClientUseCase with GetOrRegisterClientUseCase
    
    * fix: replace RegisterClientUseCase with GetOrRegisterClientUseCase
    
    * docs: explain the empty RegisterClientParam
    
    * fix: RegisterDeviceViewModelTest
    
    * chore: update kalium

--- app/src/main/kotlin/com/wire/android/migration/feature/MigrateClientsDataUseCase.kt
+++ app/src/main/kotlin/com/wire/android/migration/feature/MigrateClientsDataUseCase.kt
@@ -11,7 +11,8 @@ import com.wire.kalium.logic.NetworkFailure
 import com.wire.kalium.logic.StorageFailure
 import com.wire.kalium.logic.data.conversation.ClientId
 import com.wire.kalium.logic.data.user.UserId
-import com.wire.kalium.logic.feature.client.PersistRegisteredClientIdResult
+import com.wire.kalium.logic.feature.client.RegisterClientUseCase.RegisterClientParam
+import com.wire.kalium.logic.feature.client.RegisterClientResult
 import com.wire.kalium.logic.functional.Either
 import com.wire.kalium.logic.functional.flatMap
 import com.wire.kalium.logic.functional.foldToEitherWhileRight
@@ -43,13 +44,23 @@ class MigrateClientsDataUseCase @Inject constructor(
 
             // add registered client id, sync will start when the registered id is persisted
             coreLogic.sessionScope(userId) {
-                when (val result = this.client.persistRegisteredClientIdUseCase(clientId)) {
-                    is PersistRegisteredClientIdResult.Failure.Generic ->
+                // NOTE we are passing in an RegisterClientParam will null values
+                // because we don't support deleting any existing clients when migrating
+                // from the old scala app.
+                when (val result = this.client.importClient(clientId, RegisterClientParam(
+                    password = null,
+                    capabilities = null
+                ))) {
+                    is RegisterClientResult.Failure.Generic ->
                         Either.Left(result.genericFailure)
-                    is PersistRegisteredClientIdResult.Failure.ClientNotRegistered -> {
+                    is RegisterClientResult.Failure.TooManyClients ->
+                        Either.Left(MigrationFailure.ClientNotRegistered)
+                    is RegisterClientResult.Failure.InvalidCredentials ->
+                        Either.Left(MigrationFailure.ClientNotRegistered)
+                    is RegisterClientResult.Failure.PasswordAuthRequired -> {
                         Either.Left(MigrationFailure.ClientNotRegistered)
                     }
-                    is PersistRegisteredClientIdResult.Success ->
+                    is RegisterClientResult.Success ->
                         withTimeoutOrNull(SYNC_START_TIMEOUT) {
                             syncManager.waitUntilStartedOrFailure()
                         }.let {

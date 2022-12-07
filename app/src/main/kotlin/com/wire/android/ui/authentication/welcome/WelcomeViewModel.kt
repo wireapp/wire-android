aa66a295248d8d994962b875f252a1000be8c5fc
Mohammed Mokresh <mokresh.mohammed@gmail.com>
2022-12-07 15:02:57 +0100

    feat: disable create account with proxy [AR-2786] (#1213)
    * feat: disable create account with proxy
    
    * resolve pr comments

--- app/src/main/kotlin/com/wire/android/ui/authentication/welcome/WelcomeViewModel.kt
+++ app/src/main/kotlin/com/wire/android/ui/authentication/welcome/WelcomeViewModel.kt
@@ -14,7 +14,6 @@ import com.wire.kalium.logic.feature.session.GetAllSessionsResult
 import com.wire.kalium.logic.feature.session.GetSessionsUseCase
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.launch
-import kotlinx.coroutines.runBlocking
 import javax.inject.Inject
 
 @HiltViewModel
@@ -65,6 +64,8 @@ class WelcomeViewModel @Inject constructor(
             navigationManager.navigateBack()
         }
     }
+
+    fun isProxyEnabled() = state.apiProxy != null
 
     fun goToLogin() {
         navigate(NavigationCommand(NavigationItem.Login.getRouteWithArgs()))

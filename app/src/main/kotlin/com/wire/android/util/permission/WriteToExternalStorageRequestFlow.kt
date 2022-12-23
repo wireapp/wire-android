32d8780087f3e545b3a7bfe69b6beb6361036750
Gonzalo Gran Crespo <gongracr@gmail.com>
2022-12-23 15:56:46 +0100

    Do not ask for external write permissions on android 13 and above

--- app/src/main/kotlin/com/wire/android/util/permission/WriteToExternalStorageRequestFlow.kt
+++ app/src/main/kotlin/com/wire/android/util/permission/WriteToExternalStorageRequestFlow.kt
@@ -1,6 +1,7 @@
 package com.wire.android.util.permission
 
 import android.content.Context
+import android.os.Build
 import androidx.activity.compose.ManagedActivityResultLauncher
 import com.wire.android.util.extension.checkPermission
 
@@ -10,7 +11,9 @@ class WriteToExternalStorageRequestFlow(
     private val writeToExternalStoragePermissionLauncher: ManagedActivityResultLauncher<String, Boolean>
 ) {
     fun launch() {
-        if (context.checkPermission(android.Manifest.permission.WRITE_EXTERNAL_STORAGE)) {
+        if (context.checkPermission(android.Manifest.permission.WRITE_EXTERNAL_STORAGE)
+            || Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU
+        ) {
             onPermissionGranted()
         } else {
             writeToExternalStoragePermissionLauncher.launch(android.Manifest.permission.WRITE_EXTERNAL_STORAGE)

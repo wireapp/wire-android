579cc22480bab531ab54d49e067727480a009cfd
Jakub Żerko <iot.zerko@gmail.com>
2023-01-16 12:40:52 -0500

    fix: unread missed calls count in conversation last message [AR-2940] (#1330)
    * fix: unread missed calls count in conversation last message
    
    * more tests

--- app/src/test/kotlin/com/wire/android/mapper/MessagePreviewContentMapperTest.kt
+++ app/src/test/kotlin/com/wire/android/mapper/MessagePreviewContentMapperTest.kt
@@ -7,6 +7,7 @@ import com.wire.android.ui.home.conversations.model.UILastMessageContent
 import com.wire.android.util.ui.UIText
 import com.wire.kalium.logic.data.message.AssetType
 import com.wire.kalium.logic.data.message.MessagePreviewContent
+import com.wire.kalium.logic.data.message.UnreadEventType
 import com.wire.kalium.logic.data.user.UserId
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.test.runTest
@@ -19,6 +20,101 @@ import org.junit.jupiter.api.extension.ExtendWith
 @OptIn(ExperimentalCoroutinesApi::class)
 @ExtendWith(CoroutineTestExtension::class)
 class MessagePreviewContentMapperTest {
+
+    @Test
+    fun givenMultipleUnreadEvents_whenMappingToUIPreview_thenCorrectSortedUILastMessageContentShouldBeReturned() = runTest {
+        val messagePreview = TestMessage.PREVIEW.copy(
+            content = MessagePreviewContent.WithUser.Text("admin", "Hello"),
+        )
+        val mentionCount = 2
+        val missedCallCount = 3
+
+        val unreadEventCount = mapOf(UnreadEventType.MENTION to mentionCount, UnreadEventType.MISSED_CALL to missedCallCount)
+
+        val multipleMessage = messagePreview.toUIPreview(unreadEventCount).shouldBeInstanceOf<UILastMessageContent.MultipleMessage>()
+        val results = multipleMessage.messages.filterIsInstance<UIText.PluralResource>()
+
+        val sortedEventCount = unreadEventCount.toSortedMap()
+
+        results.first().count shouldBeEqualTo sortedEventCount.values.first()
+        results.last().count shouldBeEqualTo sortedEventCount.values.last()
+    }
+
+    @Test
+    fun givenMissedCalls_whenMappingToUIPreview_thenCorrectUILastMessageContentShouldBeReturned() = runTest {
+        val messagePreview = TestMessage.PREVIEW.copy(
+            content = MessagePreviewContent.WithUser.MissedCall("admin"),
+        )
+        val unreadCount = 2
+        val unreadEventCount = mapOf(UnreadEventType.MISSED_CALL to unreadCount)
+
+        val textMessage = messagePreview.toUIPreview(unreadEventCount).shouldBeInstanceOf<UILastMessageContent.TextMessage>()
+        val result = textMessage.messageBody.message.shouldBeInstanceOf<UIText.PluralResource>()
+
+        result.resId shouldBeEqualTo R.plurals.unread_event_call
+        result.count shouldBeEqualTo unreadCount
+    }
+
+    @Test
+    fun givenUnreadMentions_whenMappingToUIPreview_thenCorrectUILastMessageContentShouldBeReturned() = runTest {
+        val messagePreview = TestMessage.PREVIEW.copy(
+            content = MessagePreviewContent.WithUser.MentionedSelf("admin"),
+        )
+        val unreadCount = 2
+        val unreadEventCount = mapOf(UnreadEventType.MENTION to unreadCount)
+
+        val textMessage = messagePreview.toUIPreview(unreadEventCount).shouldBeInstanceOf<UILastMessageContent.TextMessage>()
+        val result = textMessage.messageBody.message.shouldBeInstanceOf<UIText.PluralResource>()
+
+        result.resId shouldBeEqualTo R.plurals.unread_event_mention
+        result.count shouldBeEqualTo unreadCount
+    }
+
+    @Test
+    fun givenUnreadReplies_whenMappingToUIPreview_thenCorrectUILastMessageContentShouldBeReturned() = runTest {
+        val messagePreview = TestMessage.PREVIEW.copy(
+            content = MessagePreviewContent.WithUser.MentionedSelf("admin"),
+        )
+        val unreadCount = 2
+        val unreadEventCount = mapOf(UnreadEventType.REPLY to unreadCount)
+
+        val textMessage = messagePreview.toUIPreview(unreadEventCount).shouldBeInstanceOf<UILastMessageContent.TextMessage>()
+        val result = textMessage.messageBody.message.shouldBeInstanceOf<UIText.PluralResource>()
+
+        result.resId shouldBeEqualTo R.plurals.unread_event_reply
+        result.count shouldBeEqualTo unreadCount
+    }
+
+    @Test
+    fun givenUnreadPings_whenMappingToUIPreview_thenCorrectUILastMessageContentShouldBeReturned() = runTest {
+        val messagePreview = TestMessage.PREVIEW.copy(
+            content = MessagePreviewContent.WithUser.Knock("admin"),
+        )
+        val unreadCount = 2
+        val unreadEventCount = mapOf(UnreadEventType.KNOCK to unreadCount)
+
+        val textMessage = messagePreview.toUIPreview(unreadEventCount).shouldBeInstanceOf<UILastMessageContent.TextMessage>()
+        val result = textMessage.messageBody.message.shouldBeInstanceOf<UIText.PluralResource>()
+
+        result.resId shouldBeEqualTo R.plurals.unread_event_knock
+        result.count shouldBeEqualTo unreadCount
+    }
+
+    @Test
+    fun givenUnreadMessages_whenMappingToUIPreview_thenLastTextMessageContentShouldBeReturned() = runTest {
+        val lastMessage = "See ya"
+        val messagePreview = TestMessage.PREVIEW.copy(
+            content = MessagePreviewContent.WithUser.Text("admin", lastMessage),
+        )
+
+        val unreadCount = 2
+        val unreadEventCount = mapOf(UnreadEventType.MESSAGE to unreadCount)
+
+        val senderWithMessage = messagePreview.toUIPreview(unreadEventCount).shouldBeInstanceOf<UILastMessageContent.SenderWithMessage>()
+        val result = senderWithMessage.message.shouldBeInstanceOf<UIText.DynamicString>()
+
+        result.value shouldBeEqualTo lastMessage
+    }
 
     @Test
     fun givenLastAssetAudioConversationMessage_whenMappingToUILastMessageContent_thenCorrectContentShouldBeReturned() = runTest {

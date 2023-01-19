86761d635c9b1e469f5528c47681f0e46834e4d4
Mohamad Jaara <mohamad.jaara@wire.com>
2023-01-19 18:45:19 +0100

    fix: migration fails indefinitely when if fails once (#1336)
    * fix: migration fails indefinitely if it fails once
    
    * update kalium
    
    * include the last read time in the migration
    
    * styling
    
    (cherry picked from commit e2dbd0e38cda1d5f033c43e83ec67ed4f0fbbc22)

--- app/src/main/kotlin/com/wire/android/migration/userDatabase/ScalaMessageDAO.kt
+++ app/src/main/kotlin/com/wire/android/migration/userDatabase/ScalaMessageDAO.kt
@@ -19,7 +19,46 @@ data class ScalaMessageData(
     val proto: ByteArray?,
     val assetName: String?,
     val assetSize: Int?,
-)
+) {
+    @Suppress("ComplexMethod")
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
+
+        other as ScalaMessageData
+
+        if (id != other.id) return false
+        if (conversationId != other.conversationId) return false
+        if (conversationRemoteId != other.conversationRemoteId) return false
+        if (conversationDomain != other.conversationDomain) return false
+        if (time != other.time) return false
+        if (editTime != other.editTime) return false
+        if (senderId != other.senderId) return false
+        if (senderClientId != other.senderClientId) return false
+        if (content != other.content) return false
+        if (proto != null) {
+            if (other.proto == null) return false
+            if (!proto.contentEquals(other.proto)) return false
+        } else if (other.proto != null) return false
+
+        return true
+    }
+
+    override fun hashCode(): Int {
+        var result = id.hashCode()
+        result = 31 * result + conversationId.hashCode()
+        result = 31 * result + conversationRemoteId.hashCode()
+        result = 31 * result + (conversationDomain?.hashCode() ?: 0)
+        result = 31 * result + time.hashCode()
+        result = 31 * result + (editTime?.hashCode() ?: 0)
+        result = 31 * result + senderId.hashCode()
+        result = 31 * result + (senderClientId?.hashCode() ?: 0)
+        result = 31 * result + (content?.hashCode() ?: 0)
+        result = 31 * result + (proto?.contentHashCode() ?: 0)
+        return result
+    }
+}
+
 class ScalaMessageDAO(private val db: ScalaUserDatabase) {
 
     fun messages(scalaConversations: List<ScalaConversationData>): List<ScalaMessageData> {
@@ -39,7 +78,8 @@ class ScalaMessageDAO(private val db: ScalaUserDatabase) {
         return try {
             if (!cursor.moveToFirst()) {
                 emptyList()
-            } else {                val idIndex = cursor.getColumnIndex("$MESSAGES_TABLE_NAME.$COLUMN_ID")
+            } else {
+                val idIndex = cursor.getColumnIndex("$MESSAGES_TABLE_NAME.$COLUMN_ID")
                 val timeIndex = cursor.getColumnIndex("$MESSAGES_TABLE_NAME.$COLUMN_TIME")
                 val editTimeIndex = cursor.getColumnIndex("$MESSAGES_TABLE_NAME.$COLUMN_EDIT_TIME")
                 val userIdIndex = cursor.getColumnIndex("$MESSAGES_TABLE_NAME.$COLUMN_USER_ID")

75db3071df6cbe247e4d72fe0b737bcdb05bed72
Mateusz <m.pachulski94@gmail.com>
2023-01-23 18:52:22 +0100

    fix: asset fetch failure retry with coil (#1345)
    * wip
    
    * fix stuff
    
    * complete fix
    
    * fix
    
    * further fix
    
    * working
    
    * updat ref
    
    * fix
    
    * test
    
    * fix the tests
    
    * update ref

--- app/src/main/kotlin/com/wire/android/util/ui/WireSessionImageLoader.kt
+++ app/src/main/kotlin/com/wire/android/util/ui/WireSessionImageLoader.kt
@@ -4,15 +4,21 @@ import android.content.Context
 import android.os.Build.VERSION.SDK_INT
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.Stable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.platform.LocalContext
 import coil.Coil
 import coil.ImageLoader
+import coil.compose.AsyncImagePainter
 import coil.compose.rememberAsyncImagePainter
 import coil.decode.GifDecoder
 import coil.decode.ImageDecoderDecoder
 import coil.request.ImageRequest
 import com.wire.android.model.ImageAsset
+import com.wire.kalium.logic.feature.asset.DeleteAssetUseCase
 import com.wire.kalium.logic.feature.asset.GetAvatarAssetUseCase
 import com.wire.kalium.logic.feature.asset.GetMessageAssetUseCase
 
@@ -23,41 +29,72 @@ import com.wire.kalium.logic.feature.asset.GetMessageAssetUseCase
  */
 @Stable
 class WireSessionImageLoader(private val coilImageLoader: ImageLoader) {
+    private companion object {
+        const val RETRY_INCREMENT_ATTEMPT_PER_STEP = 1
+    }
 
     /**
      * Attempts to paint an Image using [asset], falling back to [fallbackData] if [asset] is null.
      * Just like [rememberAsyncImagePainter], [fallbackData] can be anything that [Coil] accepts.
+     *
+     * currently Coil does not have a friendly API to retry a failing image request, so we have to do it ourselves.
+     * adding retry_hash is a workaround to force Coil to retry the request.
+     * see https://github.com/coil-kt/coil/issues/884
      */
     @Composable
     fun paint(
         asset: ImageAsset?,
         fallbackData: Any? = null
-    ): Painter = rememberAsyncImagePainter(
-        model = ImageRequest.Builder(LocalContext.current)
-            .memoryCacheKey(asset?.uniqueKey)
-            .data(asset ?: fallbackData)
-            .build(),
-        imageLoader = coilImageLoader
-    )
+    ): Painter {
+        var retryHash by remember { mutableStateOf(0) }
+
+        val painter = rememberAsyncImagePainter(
+            model = ImageRequest.Builder(LocalContext.current)
+                .memoryCacheKey(asset?.uniqueKey)
+                .data(asset ?: fallbackData)
+                .setParameter(
+                    key = AssetImageFetcher.OPTION_PARAMETER_RETRY_KEY,
+                    value = retryHash,
+                    memoryCacheKey = null
+                )
+                .build(),
+            imageLoader = coilImageLoader
+        )
+
+        if (painter.state is AsyncImagePainter.State.Error) {
+            retryHash += RETRY_INCREMENT_ATTEMPT_PER_STEP
+        }
+
+        return painter
+    }
 
     class Factory(
         context: Context,
         private val getAvatarAsset: GetAvatarAssetUseCase,
+        private val deleteAsset: DeleteAssetUseCase,
         private val getPrivateAsset: GetMessageAssetUseCase,
     ) {
         private val defaultImageLoader = Coil.imageLoader(context)
         private val resources = context.resources
 
-        fun newImageLoader(): WireSessionImageLoader = WireSessionImageLoader(
-            defaultImageLoader.newBuilder()
-                .components {
-                    add(AssetImageFetcher.Factory(getAvatarAsset, getPrivateAsset, resources))
-                    if (SDK_INT >= 28) {
-                        add(ImageDecoderDecoder.Factory())
-                    } else {
-                        add(GifDecoder.Factory())
-                    }
-                }.build()
-        )
+        fun newImageLoader(): WireSessionImageLoader =
+            WireSessionImageLoader(
+                defaultImageLoader.newBuilder()
+                    .components {
+                        add(
+                            AssetImageFetcher.Factory(
+                                getPublicAssetUseCase = getAvatarAsset,
+                                getPrivateAssetUseCase = getPrivateAsset,
+                                deleteAssetUseCase = deleteAsset,
+                                drawableResultWrapper = DrawableResultWrapper(resources)
+                            )
+                        )
+                        if (SDK_INT >= 28) {
+                            add(ImageDecoderDecoder.Factory())
+                        } else {
+                            add(GifDecoder.Factory())
+                        }
+                    }.build()
+            )
     }
 }

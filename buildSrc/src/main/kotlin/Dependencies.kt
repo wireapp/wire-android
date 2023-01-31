1792cf93e3889b293468fe8846b17ab77ae14576
Vitor Hugo Schwaab <vitor@schwaab.xyz>
2023-01-31 11:51:35 +0100

    chore: update version name

--- buildSrc/src/main/kotlin/Dependencies.kt
+++ buildSrc/src/main/kotlin/Dependencies.kt
@@ -34,7 +34,7 @@ object AndroidNdk {
 object AndroidClient {
     const val appId = "com.wire.android"
     val versionCode = Versionizer().versionCode
-    const val versionName = "0.0.1"
+    const val versionName = "4.0.0"
     const val testRunner = "com.wire.android.HiltAwareTestRunner"
 }
 

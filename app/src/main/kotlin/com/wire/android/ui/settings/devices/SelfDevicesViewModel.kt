61a3daed677d2fbf24fb3f6370286235dcac84a4
Yamil Medina <yamilmedina@users.noreply.github.com>
2023-02-02 10:20:19 +0100

    feat: remove devices from settings screen (AR-2963) (#1401)
    * feat: remove devices refactor
    
    * feat: remove devices refactor, reusing for device removal
    
    * feat: remove devices refactor, adjust navigation
    
    * feat: cleanup
    
    * feat: cleanup, reusage of remove for settings screen
    
    * feat: changing type of list for compose
    
    * feat: changing type of list for compose
    
    * feat: fix tests accordingly to new structure
    
    * feat: fix tests accordingly to new structure
    
    * feat: group panel read receipt toggle (#1406)
    
    * feat: add read receipt toggle in group settings
    
    * chore: fix detekt issues
    
    * fix: detekt MultiLineIfElse
    
    * fix: detekt ParameterListWrapping
    
    * chore: update kalium reference
    
    * re-run ci
    
    * test: adjust and add tests
    
    * chore: update kalium reference
    
    * test: add verify to make sure usecase are being called correctly
    
    * chore: update kalium reference
    
    * feat: fix visual recompositions
    
    * feat: pr comments
    
    ---------
    
    Co-authored-by: Alexandre Ferris <ferris.alexandre@gmail.com>

--- app/src/main/kotlin/com/wire/android/ui/settings/devices/SelfDevicesViewModel.kt
+++ app/src/main/kotlin/com/wire/android/ui/settings/devices/SelfDevicesViewModel.kt
@@ -20,51 +20,19 @@
 
 package com.wire.android.ui.settings.devices
 
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.wire.android.navigation.NavigationManager
-import com.wire.android.ui.authentication.devices.model.Device
-import com.wire.android.ui.settings.devices.model.SelfDevicesState
-import com.wire.kalium.logic.feature.client.SelfClientsResult
-import com.wire.kalium.logic.feature.client.SelfClientsUseCase
 import dagger.hilt.android.lifecycle.HiltViewModel
-import kotlinx.coroutines.launch
 import javax.inject.Inject
+import kotlinx.coroutines.launch
 
 @HiltViewModel
 class SelfDevicesViewModel @Inject constructor(
-    private val navigationManager: NavigationManager,
-    private val selfClientsUseCase: SelfClientsUseCase,
-    ) : ViewModel() {
+    private val navigationManager: NavigationManager
+) : ViewModel() {
 
-    var state: SelfDevicesState by mutableStateOf(
-        SelfDevicesState(deviceList = listOf(), isLoadingClientsList = true, currentDevice = null)
-    )
-        private set
-
-    init {
-        loadClientsList()
+    fun navigateBack() {
+        viewModelScope.launch { navigationManager.navigateBack() }
     }
-
-    private fun loadClientsList() {
-        viewModelScope.launch {
-            state = state.copy(isLoadingClientsList = true)
-            val selfClientsResult = selfClientsUseCase()
-            if (selfClientsResult is SelfClientsResult.Success)
-                state = state.copy(
-                    currentDevice = selfClientsResult.clients
-                        .firstOrNull { it.id == selfClientsResult.currentClientId }?.let { Device(it) },
-                    isLoadingClientsList = false,
-                    deviceList = selfClientsResult.clients
-                        .filter { it.id != selfClientsResult.currentClientId }
-                        .map { Device(it) },
-                )
-        }
-    }
-
-    fun navigateBack() = viewModelScope.launch { navigationManager.navigateBack() }
-
 }

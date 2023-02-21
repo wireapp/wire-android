e357c3268e0c918beebab63f5fa98550bb3efa74
Michał Saleniuk <saleniuk@gmail.com>
2023-02-21 14:20:58 +0100

    chore: log compose screens to datadog

--- app/src/staging/kotlin/com/wire/android/ExternalLoggerManager.kt
+++ app/src/staging/kotlin/com/wire/android/ExternalLoggerManager.kt
@@ -1,5 +1,6 @@
 package com.wire.android
 
+import android.app.Activity
 import android.content.Context
 import com.datadog.android.Datadog
 import com.datadog.android.DatadogSite
@@ -7,6 +8,9 @@ import com.datadog.android.core.configuration.Credentials
 import com.datadog.android.privacy.TrackingConsent
 import com.datadog.android.rum.GlobalRum
 import com.datadog.android.rum.RumMonitor
+import com.datadog.android.rum.tracking.ActivityViewTrackingStrategy
+import com.datadog.android.rum.tracking.ComponentPredicate
+import com.wire.android.ui.WireActivity
 import com.wire.android.util.getDeviceId
 import com.wire.android.util.sha256
 
@@ -25,7 +29,21 @@ object ExternalLoggerManager {
             tracesEnabled = true,
             rumEnabled = true,
             crashReportsEnabled = true,
-        ).trackInteractions()
+        )
+            .useViewTrackingStrategy(
+                ActivityViewTrackingStrategy(
+                    trackExtras = true,
+                    componentPredicate = object : ComponentPredicate<Activity> {
+                        override fun accept(component: Activity): Boolean {
+                            // reject Activities which are hosts of Compose views, so that they are not counted as views
+                            return component !is WireActivity
+                        }
+
+                        override fun getViewName(component: Activity): String? = null
+                    }
+                )
+            )
+            .trackInteractions()
             .trackBackgroundRumEvents(true)
             .trackLongTasks(LONG_TASK_THRESH_HOLD_MS)
             .useSite(DatadogSite.EU1)

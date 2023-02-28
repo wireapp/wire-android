b5cc7f444c0778edb609041152cefb60670a3bc9
yamilmedina <yamilmedina@users.noreply.github.com>
2023-02-28 19:30:46 -0300

    feat: add todo and feature flag

--- app/src/main/kotlin/com/wire/android/mapper/MessageContentMapper.kt
+++ app/src/main/kotlin/com/wire/android/mapper/MessageContentMapper.kt
@@ -234,6 +234,10 @@ class MessageContentMapper @Inject constructor(
     }
 
     private fun mapRecipientsFailure(userList: List<User>, regular: Message.Regular?): DeliveryStatusContent {
+        // TODO(federation): enable this when a new definition (jira ticket) is raised for displaying partial delivery no-clients
+        if (true) {
+            return DeliveryStatusContent.CompleteDelivery
+        }
         return when (val usersIds = regular?.deliveryStatus) {
             is DeliveryStatus.PartialDelivery -> DeliveryStatusContent.PartialDelivery(
                 failedRecipients = usersIds.recipientsFailedDelivery.map { userId ->

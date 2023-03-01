83afa625f8de9bea3687ad7f39500874c8299d60
Oussama <oussama.has100@gmail.com>
2023-03-01 18:26:05 +0100

    fix: text field not cleared after sending message

--- app/src/main/kotlin/com/wire/android/ui/common/textfield/WireTextField.kt
+++ app/src/main/kotlin/com/wire/android/ui/common/textfield/WireTextField.kt
@@ -104,7 +104,7 @@ internal fun WireTextField(
     onLineBottomYCoordinateChanged: (Float) -> Unit = { },
 ) {
     val enabled = state !is WireTextFieldState.Disabled
-    var text by remember { mutableStateOf(value) }
+    var updatedText by remember { mutableStateOf(value) }
 
     Column(modifier = modifier) {
         if (labelText != null)
@@ -112,14 +112,14 @@ internal fun WireTextField(
         BasicTextField(
             value = value,
             onValueChange = {
-                text = if (singleLine || maxLines == 1) it.copy(it.text.replace("\n", ""))
+                updatedText = if (singleLine || maxLines == 1) it.copy(it.text.replace("\n", ""))
                     else it
 
-                text = if (it.text.length > maxTextLength)
-                    TextFieldValue(text = it.text.take(maxTextLength), selection = TextRange(it.text.length - 1))
-                else it
+                updatedText = if (updatedText.text.length > maxTextLength)
+                    TextFieldValue(text = it.text.take(maxTextLength), selection = TextRange(updatedText.text.length - 1))
+                else updatedText
 
-                onValueChange(text)
+                onValueChange(updatedText)
             },
             textStyle = textStyle.copy(color = colors.textColor(state = state).value, textDirection = TextDirection.ContentOrLtr),
             keyboardOptions = keyboardOptions,

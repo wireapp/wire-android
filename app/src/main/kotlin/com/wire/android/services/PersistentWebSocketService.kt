42427774e3ffe9f2b5ee1147ed5b817cc66f9a50
Oussama <oussama.has100@gmail.com>
2023-03-01 16:16:48 +0100

    fix: stuck on connecting when disabling persistent web socket

--- app/src/main/kotlin/com/wire/android/services/PersistentWebSocketService.kt
+++ app/src/main/kotlin/com/wire/android/services/PersistentWebSocketService.kt
@@ -118,11 +118,6 @@ class PersistentWebSocketService : Service() {
                                         coreLogic.getSessionScope(persistentWebSocketStatus.userId)
                                             .setConnectionPolicy(ConnectionPolicy.KEEP_ALIVE)
                                     }
-                                } else {
-                                    runBlocking {
-                                        coreLogic.getSessionScope(persistentWebSocketStatus.userId)
-                                            .setConnectionPolicy(ConnectionPolicy.DISCONNECT_AFTER_PENDING_EVENTS)
-                                    }
                                 }
                             }
                         }

7468f11ffd1a0d8024e79e693c1c8ee0b269c346
Mohamad Jaara <mohamad.jaara@wire.com>
2023-03-30 17:06:33 +0200

    feat: redo migration on app update when failure (#1584)
    * chore: crash the migration worker if there is an error
    
    * feat: redo migration when completed with errors on a previous version
    
    * styling
    
    * trigger CI
    
    * address PR comments
    
    * styling
    
    * unit tests
    
    (cherry picked from commit 172b2bf84ee82b23f9fd5b07bb169f83adcd7404)

--- app/src/main/kotlin/com/wire/android/migration/MigrationManager.kt
+++ app/src/main/kotlin/com/wire/android/migration/MigrationManager.kt
@@ -63,6 +63,8 @@ import com.wire.kalium.logic.functional.fold
 import com.wire.kalium.logic.functional.isLeft
 import com.wire.kalium.logic.functional.isRight
 import com.wire.kalium.logic.functional.map
+import com.wire.kalium.logic.functional.onFailure
+import com.wire.kalium.logic.functional.onSuccess
 import dagger.hilt.android.qualifiers.ApplicationContext
 import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.CoroutineScope
@@ -140,20 +142,17 @@ class MigrationManager @Inject constructor(
                     }.also { report.addMessagesReport(userId, it) }
                 }
             }.fold({
-                when (it) {
+                globalDataStore.setUserMigrationStatus(userId.value, UserMigrationStatus.CompletedWithErrors)when (it) {
                     is NetworkFailure.NoNetworkConnection -> MigrationData.Result.Failure.NoNetwork
                     else -> MigrationData.Result.Failure.Messages(it.getErrorCode().toString())
                 }
-            }, {
+            }, {globalDataStore.setUserMigrationStatus(userId.value, UserMigrationStatus.Successfully)
                 MigrationData.Result.Success
             })
         } catch (e: Exception) {
+            globalDataStore.setUserMigrationStatus(userId.value, UserMigrationStatus.CompletedWithErrors)
             appLogger.e("$TAG - Migration failed for ${userId.value.obfuscateId()}")
             throw e
-        } finally {
-            // if migration crashed for any reason, we want to set migration as completed so that we don't try to migrate again
-            // and avoid any possible crash loops
-            globalDataStore.setUserMigrationStatus(userId.value, UserMigrationStatus.Completed)
         }
 
     // Because in migration conversation with last read state are inserted before messages
@@ -300,9 +299,11 @@ class MigrationManager @Inject constructor(
                     clearUnreadMessages(it, userId)
                 }.also { report.addMessagesReport(userId, it) }
                     .also {
-                        globalDataStore.setUserMigrationStatus(userId.value, UserMigrationStatus.Completed)
-
                         resultAcc[userId.value] = it
+                    }.onFailure {
+                        globalDataStore.setUserMigrationStatus(userId.value, UserMigrationStatus.CompletedWithErrors)
+                    }.onSuccess {
+                        globalDataStore.setUserMigrationStatus(userId.value, UserMigrationStatus.Successfully)
                     }
             }
         }
--- app/src/main/kotlin/com/wire/android/migration/userDatabase/ShouldTriggerMigrationForUserUserCase.kt
+++ app/src/main/kotlin/com/wire/android/migration/userDatabase/ShouldTriggerMigrationForUserUserCase.kt
@@ -1,7 +1,9 @@
 package com.wire.android.migration.userDatabase
 
 import android.content.Context
+import com.wire.android.BuildConfig
 import com.wire.android.datastore.GlobalDataStore
+import com.wire.android.di.CurrentAppVersion
 import com.wire.android.migration.failure.UserMigrationStatus
 import com.wire.android.migration.util.ScalaDBNameProvider
 import com.wire.kalium.logic.data.user.UserId
@@ -13,19 +15,34 @@ import javax.inject.Inject
 @ViewModelScoped
 class ShouldTriggerMigrationForUserUserCase @Inject constructor(
     @ApplicationContext private val applicationContext: Context,
-    private val globalDataStore: GlobalDataStore
+    private val globalDataStore: GlobalDataStore,
+    @CurrentAppVersion private val currentAppVersion: Int
 ) {
     suspend operator fun invoke(userId: UserId) = globalDataStore.getUserMigrationStatus(userId.value)
         .first().let { migrationStatus ->
             when (migrationStatus) {
                 // if the user has already been migrated, we don't need to do it again
-                UserMigrationStatus.Completed,
+                UserMigrationStatus.Successfully,
                 UserMigrationStatus.NoNeed -> return@let false
 
                 // if the user has not been migrated yet, we check if the database exists
                 // also check when null since it can mean the migration is done on 4.0.1
                 UserMigrationStatus.NotStarted,
                 null -> checkForScalaDB(userId)
+
+                UserMigrationStatus.Completed,
+                UserMigrationStatus.CompletedWithErrors -> {
+                    // if the user has been migrated but with errors, we check if the database exists
+                    // and if the app version is the same as the one that was used to migrate the user
+                    // if the app version is different, we need to migrate the user again
+                    val appVersion = globalDataStore.getUserMigrationAppVersion(userId.value)
+                    val isNotSameVersion = appVersion != currentAppVersion
+                    return@let if (!isNotSameVersion) {
+                        false
+                    } else {
+                        checkForScalaDB(userId)
+                    }
+                }
             }
         }
 

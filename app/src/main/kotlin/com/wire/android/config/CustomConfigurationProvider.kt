cfffc883750cc1813aed26b30c8350ae6c7754c2
Mateusz <m.pachulski94@gmail.com>
2023-04-13 13:03:42 +0200

    add provider

---
+++ app/src/main/kotlin/com/wire/android/config/CustomConfigurationProvider.kt
@@ -0,0 +1,29 @@
+/*
+ * Wire
+ * Copyright (C) 2023 Wire Swiss GmbH
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see http://www.gnu.org/licenses/.
+ */
+package com.wire.android.config
+
+import androidx.compose.runtime.staticCompositionLocalOf
+import com.wire.android.BuildConfig
+
+object CustomConfigurationProvider {
+    val allowAccountCreation = BuildConfig.ALLOW_ACCOUNT_CREATION ?: false
+    val resetPasswordUrl = BuildConfig.ACCOUNTS_URL ?: ""
+    val supportUrl = BuildConfig.SUPPORT_URL
+}
+
+val LocalCustomConfigurationProvider = staticCompositionLocalOf { CustomConfigurationProvider }

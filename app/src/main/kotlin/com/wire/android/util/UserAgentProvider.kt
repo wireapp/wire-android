daa0e32343f127bcc91728ec435ad5c2c5dafc77
Mohamad Jaara <mohamad.jaara@wire.com>
2023-04-25 16:34:58 +0200

    add system http agent

--- app/src/main/kotlin/com/wire/android/util/UserAgentProvider.kt
+++ app/src/main/kotlin/com/wire/android/util/UserAgentProvider.kt
@@ -9,10 +9,11 @@ import javax.inject.Singleton
 
 @Singleton class UserAgentProvider@Inject constructor(@ApplicationContext private val context: Context) {
 
-    val defaultUserAgent = "Wire/${BuildConfig.VERSION_NAME}/${context.getGitBuildId()} (Android ${getAndroidVersion()})"
+    val defaultUserAgent =
+        "Wire/${BuildConfig.VERSION_NAME}/${context.getGitBuildId()}/${getAndroidVersion()}/${System.getProperty("http.agent")}"
     private fun getAndroidVersion(): String {
         val release: String = Build.VERSION.RELEASE
         val sdkVersion: Int = Build.VERSION.SDK_INT
-        return "$sdkVersion $release"
+        return "android:$release/sdk:$sdkVersion"
     }
 }

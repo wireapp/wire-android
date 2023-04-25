2d711ba3ae89f6c0821d027aeca7a86820a2e55c
Mohamad Jaara <mohamad.jaara@wire.com>
2023-04-25 16:45:24 +0200

    try catch systemAgent

--- app/src/main/kotlin/com/wire/android/util/UserAgentProvider.kt
+++ app/src/main/kotlin/com/wire/android/util/UserAgentProvider.kt
@@ -10,10 +10,18 @@ import javax.inject.Singleton
 @Singleton class UserAgentProvider@Inject constructor(@ApplicationContext private val context: Context) {
 
     val defaultUserAgent =
-        "Wire/${BuildConfig.VERSION_NAME}/${context.getGitBuildId()}/${getAndroidVersion()}/${System.getProperty("http.agent")}"
+        "Wire/${BuildConfig.VERSION_NAME}/${context.getGitBuildId()}/${getAndroidVersion()}/${systemAgent() ?: "unknownSystemAgent"}"
+
     private fun getAndroidVersion(): String {
         val release: String = Build.VERSION.RELEASE
         val sdkVersion: Int = Build.VERSION.SDK_INT
         return "android:$release/sdk:$sdkVersion"
+    }
+
+    @Suppress("TooGenericExceptionCaught")
+    private fun systemAgent(): String? = try {
+        System.getProperty("http.agent")
+    } catch (e: Exception) {
+        null
     }
 }

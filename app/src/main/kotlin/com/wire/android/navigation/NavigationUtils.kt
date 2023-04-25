3105dad2ec9dc43deb807eaaa8675f745bd531b3
Alexandre Ferris <ferris.alexandre@gmail.com>
2023-04-25 16:00:38 +0200

    fix: add callback to handle deeplinks only when navigation graph is done (#1711)
    Co-authored-by: Oussama Hassine <oussama.has100@gmail.com>

--- app/src/main/kotlin/com/wire/android/navigation/NavigationUtils.kt
+++ app/src/main/kotlin/com/wire/android/navigation/NavigationUtils.kt
@@ -27,6 +27,7 @@ import com.wire.android.appLogger
 
 @ExperimentalMaterial3Api
 internal fun NavController.navigateToItem(command: NavigationCommand) {
+    appLogger.d("[$TAG] -> command: ${command.destination}")
     currentBackStackEntry?.savedStateHandle?.remove<Map<String, Any>>(EXTRA_BACK_NAVIGATION_ARGUMENTS)
     navigate(command.destination) {
         when (command.backStackMode) {
@@ -91,3 +92,5 @@ fun String.getPrimaryRoute(): String {
     }
     return primaryRoute
 }
+
+private const val TAG = "NavigationUtils"

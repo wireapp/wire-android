163d8b49f03fc84cba7603227c3636fccc84b19b
Gonzalo Gran Crespo <gongracr@gmail.com>
2023-04-25 17:50:23 +0200

    Reverted changes

--- app/src/main/kotlin/com/wire/android/ui/authentication/welcome/WelcomeScreen.kt
+++ app/src/main/kotlin/com/wire/android/ui/authentication/welcome/WelcomeScreen.kt
@@ -144,17 +144,21 @@ private fun WelcomeContent(viewModel: WelcomeViewModel) {
                 )
             ) {
                 LoginButton(viewModel::goToLogin)
-                FeatureDisabledWithProxyDialogContent(dialogState = enterpriseDisabledWithProxyDialogState,
+                FeatureDisabledWithProxyDialogContent(
+                    dialogState = enterpriseDisabledWithProxyDialogState,
                     onActionButtonClicked = {
                         CustomTabsHelper.launchUrl(context, viewModel.state.teams)
-                    })
+                    }
+                )
                 FeatureDisabledWithProxyDialogContent(dialogState = createPersonalAccountDisabledWithProxyDialogState)
+
                 if (LocalCustomUiConfigurationProvider.current.isAccountCreationAllowed) {
                     CreateEnterpriseAccountButton {
                         if (viewModel.isProxyEnabled()) {
                             enterpriseDisabledWithProxyDialogState.show(
                                 enterpriseDisabledWithProxyDialogState.savedState ?: FeatureDisabledWithProxyDialogState(
-                                    R.string.create_team_not_supported_dialog_description, viewModel.state.teams
+                                    R.string.create_team_not_supported_dialog_description,
+                                viewModel.state.teams
                                 )
                             )
                         } else {
@@ -164,20 +168,23 @@ private fun WelcomeContent(viewModel: WelcomeViewModel) {
                 }
             }
 
-            WelcomeFooter(modifier = Modifier.padding(horizontal = MaterialTheme.wireDimensions.welcomeTextHorizontalPadding),
-                onPrivateAccountClick = {
-                    if (viewModel.isProxyEnabled()) {
-                        createPersonalAccountDisabledWithProxyDialogState.show(
-                            createPersonalAccountDisabledWithProxyDialogState.savedState ?: FeatureDisabledWithProxyDialogState(
-                                R.string.create_personal_account_not_supported_dialog_description
+            if (LocalCustomUiConfigurationProvider.current.isAccountCreationAllowed) {
+                WelcomeFooter(
+                    modifier = Modifier.padding(horizontal = MaterialTheme.wireDimensions.welcomeTextHorizontalPadding),
+                    onPrivateAccountClick = {
+                        if (viewModel.isProxyEnabled()) {
+                            createPersonalAccountDisabledWithProxyDialogState.show(
+                                createPersonalAccountDisabledWithProxyDialogState.savedState ?: FeatureDisabledWithProxyDialogState(
+                                    R.string.create_personal_account_not_supported_dialog_description
+                                )
                             )
-                        )
-                    } else {
-                        viewModel.goToCreatePrivateAccount()
+                        } else {
+                            viewModel.goToCreatePrivateAccount()
+                        }
                     }
-                })
+                )
+            }
         }
-
     }
 }
 
@@ -201,7 +208,9 @@ private fun WelcomeCarousel() {
 
     CompositionLocalProvider(LocalOverscrollConfiguration provides null) {
         HorizontalPager(
-            state = pagerState, count = circularItemsList.size, modifier = Modifier.fillMaxWidth()
+            state = pagerState,
+            count = circularItemsList.size,
+            modifier = Modifier.fillMaxWidth()
         ) { page ->
             val (pageIconResId, pageText) = circularItemsList[page]
             WelcomeCarouselItem(pageIconResId = pageIconResId, pageText = pageText)
@@ -211,7 +220,10 @@ private fun WelcomeCarousel() {
 
 @OptIn(ExperimentalPagerApi::class, ExperimentalCoroutinesApi::class)
 private suspend fun autoScrollCarousel(
-    pageState: PagerState, initialPage: Int, circularItemsList: List<CarouselPageData>, delay: Long
+    pageState: PagerState,
+    initialPage: Int,
+    circularItemsList: List<CarouselPageData>,
+    delay: Long
 ) = snapshotFlow { pageState.currentPage }.distinctUntilChanged()
     .scan(initialPage to initialPage) { (_, previousPage), currentPage -> previousPage to currentPage }
     .flatMapLatest { (previousPage, currentPage) ->
@@ -231,7 +243,11 @@ private suspend fun autoScrollCarousel(
                 circularItemsList.lastIndex
             ) -> flow { emit(CarouselScrollData(scrollToPage = circularItemsList.lastIndex - 1, animate = false)) }
 
-            else -> flow { emit(CarouselScrollData(scrollToPage = pageState.currentPage + 1, animate = true)) }.onEach { delay(delay) }
+            else -> flow { emit(CarouselScrollData(scrollToPage = pageState.currentPage + 1, animate = true)) }.onEach {
+                delay(
+                delay
+            )
+            }
         }
     }.collect { (scrollToPage, animate) ->
         if (pageState.pageCount != 0) {
@@ -288,7 +304,6 @@ private fun CreateEnterpriseAccountButton(onClick: () -> Unit) {
 @Composable
 private fun WelcomeFooter(modifier: Modifier, onPrivateAccountClick: () -> Unit) {
     Column(modifier = modifier) {
-
         Text(
             text = stringResource(R.string.welcome_footer_text),
             style = MaterialTheme.wireTypography.body02,
@@ -297,12 +312,18 @@ private fun WelcomeFooter(modifier: Modifier, onPrivateAccountClick: () -> Unit)
         )
 
         Text(
-            text = stringResource(R.string.welcome_button_create_personal_account), style = MaterialTheme.wireTypography.body02.copy(
-                textDecoration = TextDecoration.Underline, color = MaterialTheme.colorScheme.primary
-            ), textAlign = TextAlign.Center, modifier = Modifier
+            text = stringResource(R.string.welcome_button_create_personal_account),
+            style = MaterialTheme.wireTypography.body02.copy(
+                textDecoration = TextDecoration.Underline,
+                color = MaterialTheme.colorScheme.primary
+            ),
+            textAlign = TextAlign.Center,
+            modifier = Modifier
                 .fillMaxWidth()
                 .clickable(
-                    interactionSource = remember { MutableInteractionSource() }, indication = null, onClick = onPrivateAccountClick
+                    interactionSource = remember { MutableInteractionSource() },
+                    indication = null,
+                    onClick = onPrivateAccountClick
                 )
         )
 
@@ -336,4 +357,3 @@ fun PreviewWelcomeScreen() {
 
 private data class CarouselScrollData(val scrollToPage: Int, val animate: Boolean)
 private data class CarouselPageData(@DrawableRes val icon: Int, val text: String)
-

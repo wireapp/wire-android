d769358bd5a46c6bf7e6e4de26ab51133a518746
Tommaso Piazza <196761+tmspzz@users.noreply.github.com>
2023-07-13 12:38:17 +0200

    chore: bump version to 4.2.2

--- buildSrc/src/main/kotlin/Dependencies.kt
+++ buildSrc/src/main/kotlin/Dependencies.kt
@@ -35,7 +35,7 @@ object AndroidClient {
     const val appId = "com.wire.android"
     const val namespace = appId
     val versionCode = Versionizer().versionCode
-    const val versionName = "4.2.1"
+    const val versionName = "4.2.2"
     const val testRunner = "androidx.test.runner.AndroidJUnitRunner"
 }
 

455015c59afb2f635ce9552175dd0dbfa1992c29
alexandreferris <ferris.alexandre@gmail.com>
2023-07-13 15:37:00 +0200

    chore: remove unused import

--- app/src/main/kotlin/com/wire/android/ui/home/messagecomposer/AttachmentOptions.kt
+++ app/src/main/kotlin/com/wire/android/ui/home/messagecomposer/AttachmentOptions.kt
@@ -34,7 +34,6 @@ import androidx.compose.runtime.derivedStateOf
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview

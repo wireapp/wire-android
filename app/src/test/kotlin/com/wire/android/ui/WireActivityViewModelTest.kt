7add3648fe5c4e4a9fb704dab09471e5f84f588b
Michał Saleniuk <saleniuk@gmail.com>
2023-07-14 14:38:25 +0200

    detekt

--- app/src/test/kotlin/com/wire/android/ui/WireActivityViewModelTest.kt
+++ app/src/test/kotlin/com/wire/android/ui/WireActivityViewModelTest.kt
@@ -18,6 +18,8 @@
  *
  */
 
+@file:Suppress("MaximumLineLength")
+
 package com.wire.android.ui
 
 import android.content.Intent
@@ -78,7 +80,7 @@ import org.amshove.kluent.`should be equal to`
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.extension.ExtendWith
 
-@Suppress("MaxLineLength", "MaximumLineLength")
+@Suppress("MaxLineLength")
 @OptIn(ExperimentalCoroutinesApi::class)
 @ExtendWith(CoroutineTestExtension::class)
 class WireActivityViewModelTest {

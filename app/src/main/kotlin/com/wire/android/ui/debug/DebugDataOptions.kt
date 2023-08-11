ba5902ff277e72b8e8d1c05e20f0ba41bed3727f
mohamad.jaara <mohamad.jaara@wire.com>
2023-08-11 10:50:46 +0200

    fix: adjust call join button dimensions to spec

--- app/src/main/kotlin/com/wire/android/ui/debug/DebugDataOptions.kt
+++ app/src/main/kotlin/com/wire/android/ui/debug/DebugDataOptions.kt
@@ -309,8 +309,8 @@ private fun DevelopmentApiVersioningOptions(
         },
         actions = {
             WirePrimaryButton(
-                minHeight = MaterialTheme.wireDimensions.buttonMinSize.height,
-                minWidth = MaterialTheme.wireDimensions.buttonMinSize.width,
+                minHeight = MaterialTheme.wireDimensions.buttonMediumMinSize.height,
+                minWidth = MaterialTheme.wireDimensions.buttonMediumMinSize.width,
                 onClick = onForceLatestDevelopmentApiChange,
                 text = stringResource(R.string.debug_settings_force_api_versioning_update_button_text),
                 fillMaxWidth = false
@@ -370,6 +370,8 @@ private fun MLSOptions(
             },
             actions = {
                 WirePrimaryButton(
+                    minHeight = MaterialTheme.wireDimensions.buttonMediumMinSize.height,
+                    minWidth = MaterialTheme.wireDimensions.buttonMediumMinSize.width,
                     onClick = restartSlowSyncForRecovery,
                     text = stringResource(R.string.restart_slowsync_for_recovery_button),
                     fillMaxWidth = false

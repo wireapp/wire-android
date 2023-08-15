014a520da7ac84e6981125f10831a72589b1927a
Oussama <oussama.has100@gmail.com>
2023-08-15 15:05:26 +0200

    chore: cleanup

--- app/src/main/kotlin/com/wire/android/ui/common/topappbar/CommonTopAppBarViewModel.kt
+++ app/src/main/kotlin/com/wire/android/ui/common/topappbar/CommonTopAppBarViewModel.kt
@@ -144,7 +144,6 @@ class CommonTopAppBarViewModel @Inject constructor(
         }
     }
 
-
     private companion object {
         const val WAITING_TIME_TO_SHOW_ONGOING_CALL_BANNER = 600L
     }

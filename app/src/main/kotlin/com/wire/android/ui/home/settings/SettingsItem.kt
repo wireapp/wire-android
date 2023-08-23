c4817568576f12013b428c0d1e7e5af5dce8dbea
yamilmedina <yamilmedina@users.noreply.github.com>
2023-08-23 09:50:08 +0200

    fix: detekt and merge conflicts

--- app/src/main/kotlin/com/wire/android/ui/home/settings/SettingsItem.kt
+++ app/src/main/kotlin/com/wire/android/ui/home/settings/SettingsItem.kt
@@ -31,7 +31,6 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.unit.dp
 import com.ramcosta.composedestinations.spec.Direction
 import com.wire.android.R
 import com.wire.android.model.Clickable

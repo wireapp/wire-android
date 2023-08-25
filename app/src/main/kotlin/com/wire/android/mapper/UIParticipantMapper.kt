7f703644609950f37f957e3adb76aa02d56a2f98
Jakub Żerko <iot.zerko@gmail.com>
2023-08-25 10:03:31 +0200

    feat(debug): migration progress [WPB-3523] (#2128)

--- app/src/main/kotlin/com/wire/android/mapper/UIParticipantMapper.kt
+++ app/src/main/kotlin/com/wire/android/mapper/UIParticipantMapper.kt
@@ -55,7 +55,8 @@ class UIParticipantMapper @Inject constructor(
             unavailable = unavailable,
             isDeleted = (user is OtherUser && user.deleted),
             botService = (user as? OtherUser)?.botService,
-            isDefederated = (user is OtherUser && user.defederated)
+            isDefederated = (user is OtherUser && user.defederated),
+            supportedProtocolList = supportedProtocols.orEmpty().toList()
         )
     }
 
@@ -69,7 +70,8 @@ class UIParticipantMapper @Inject constructor(
             unavailable = !userSummary.isUserDeleted && userSummary.userName.orEmpty().isEmpty(),
             isDeleted = userSummary.isUserDeleted,
             isSelf = isSelfUser,
-            isDefederated = false
+            isDefederated = false,
+            supportedProtocolList = listOf()
         )
     }
 
@@ -84,7 +86,8 @@ class UIParticipantMapper @Inject constructor(
             isDeleted = userSummary.isUserDeleted,
             isSelf = false,
             readReceiptDate = date,
-            isDefederated = false
+            isDefederated = false,
+            supportedProtocolList = listOf()
         )
     }
 

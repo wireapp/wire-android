64d1a0b8ea5890d57242cb1805ed0f53a6112f1d
alexandreferris <ferris.alexandre@gmail.com>
2023-08-28 17:13:30 +0200

    chore: adjust detekt

--- app/src/main/kotlin/com/wire/android/ui/home/conversationslist/common/EventBadge.kt
+++ app/src/main/kotlin/com/wire/android/ui/home/conversationslist/common/EventBadge.kt
@@ -36,7 +36,6 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.ColorFilter
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.unit.dp
 import com.wire.android.R
 import com.wire.android.ui.common.BlockedLabel
 import com.wire.android.ui.common.DeletedLabel

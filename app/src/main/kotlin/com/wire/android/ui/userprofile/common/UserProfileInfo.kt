d9e7663f7ac21568c45522efc4709ea9b0be0b50
Boris Safonov <boris.safonov@appunite.com>
2023-08-28 18:06:32 +0300

    fix: Always allow avatar changeing : code style

--- app/src/main/kotlin/com/wire/android/ui/userprofile/common/UserProfileInfo.kt
+++ app/src/main/kotlin/com/wire/android/ui/userprofile/common/UserProfileInfo.kt
@@ -35,7 +35,6 @@ import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Alignment.Companion.CenterHorizontally
 import androidx.compose.ui.Modifier

8307478ef829310a33fd1871399eb06c87f006e5
Oussama <oussama.has100@gmail.com>
2023-08-31 16:35:12 +0200

    chore: cleanup

--- app/src/main/kotlin/com/wire/android/ui/calling/controlbuttons/StartCallButton.kt
+++ app/src/main/kotlin/com/wire/android/ui/calling/controlbuttons/StartCallButton.kt
@@ -24,7 +24,6 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.wire.android.R
 import com.wire.android.appLogger

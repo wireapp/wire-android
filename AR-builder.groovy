d593a25f4a9b5d7be2d090ea38b34390655383a8
Oussama <oussama.has100@gmail.com>
2023-09-04 15:38:40 +0200

    chore(CI): add som logs

--- AR-builder.groovy
+++ AR-builder.groovy
@@ -305,6 +305,8 @@ pipeline {
                 withGradle() {
                     sh './gradlew runAcceptanceTests'
                 }
+                sh ls -l /app/build/reports/androidTests/connected
+                sh ls -l /app/build/reports/androidTests/connected/flavors
                 publishHTML(allowMissing: true, alwaysLinkToLastBuild: true, keepAll: true, reportDir: "app/build/reports/androidTests/connected/${params.BUILD_TYPE.toLowerCase()}/flavors/${params.FLAVOR.toLowerCase()}", reportFiles: 'index.html', reportName: 'Acceptance Test Report', reportTitles: 'Acceptance Test')
                 zip archive: true, defaultExcludes: false, dir: "app/build/reports/androidTests/connected/${params.BUILD_TYPE.toLowerCase()}/flavors/${params.FLAVOR.toLowerCase()}", overwrite: true, glob: "", zipFile: "integration-tests-android.zip"
             }

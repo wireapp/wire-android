9a47a50804390a18b707f637894b80f3f47e9ca3
Oussama <oussama.has100@gmail.com>
2023-09-05 11:50:13 +0200

    chore: add buid type to the path

--- AR-builder.groovy
+++ AR-builder.groovy
@@ -306,8 +306,8 @@ pipeline {
                     sh './gradlew runAcceptanceTests'
                 }
 
-                publishHTML(allowMissing: true, alwaysLinkToLastBuild: true, keepAll: true, reportDir: "app/build/reports/androidTests/connected/flavors/${params.FLAVOR.toLowerCase()}", reportFiles: 'index.html', reportName: 'Acceptance Test Report', reportTitles: 'Acceptance Test')
-                zip archive: true, defaultExcludes: false, dir: "app/build/reports/androidTests/connected/flavors/${params.FLAVOR.toLowerCase()}", overwrite: true, glob: "", zipFile: "integration-tests-android.zip"
+                publishHTML(allowMissing: true, alwaysLinkToLastBuild: true, keepAll: true, reportDir: "app/build/reports/androidTests/connected/${params.BUILD_TYPE.toLowerCase()}/flavors/${params.FLAVOR.toLowerCase()}", reportFiles: 'index.html', reportName: 'Acceptance Test Report', reportTitles: 'Acceptance Test')
+                zip archive: true, defaultExcludes: false, dir: "app/build/reports/androidTests/connected/${params.BUILD_TYPE.toLowerCase()}/flavors/${params.FLAVOR.toLowerCase()}", overwrite: true, glob: "", zipFile: "integration-tests-android.zip"
             }
         }
 

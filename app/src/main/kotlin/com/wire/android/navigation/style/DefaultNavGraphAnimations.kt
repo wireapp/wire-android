a3378c18691bc373d5ac552d3a910f4ec4a83729
Michał Saleniuk <30429749+saleniuk@users.noreply.github.com>
2023-09-05 12:49:53 +0200

    fix(RC): adjust transition animations according to the design documentation [WPB-1817] (#2169)

---
+++ app/src/main/kotlin/com/wire/android/navigation/style/DefaultNavGraphAnimations.kt
@@ -0,0 +1,38 @@
+/*
+ * Wire
+ * Copyright (C) 2023 Wire Swiss GmbH
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see http://www.gnu.org/licenses/.
+ */
+package com.wire.android.navigation.style
+
+import androidx.compose.animation.ExperimentalAnimationApi
+import com.ramcosta.composedestinations.animations.defaults.NestedNavGraphDefaultAnimations
+import com.ramcosta.composedestinations.animations.defaults.RootNavGraphDefaultAnimations
+
+@OptIn(ExperimentalAnimationApi::class)
+val DefaultRootNavGraphAnimations = RootNavGraphDefaultAnimations(
+    enterTransition = { with(DefaultNavigationAnimation) { enterTransition() } },
+    exitTransition = { with(DefaultNavigationAnimation) { exitTransition() } },
+    popEnterTransition = { with(DefaultNavigationAnimation) { popEnterTransition() } },
+    popExitTransition = { with(DefaultNavigationAnimation) { popExitTransition() } },
+)
+
+@OptIn(ExperimentalAnimationApi::class)
+val DefaultNestedNavGraphAnimations = NestedNavGraphDefaultAnimations(
+    enterTransition = { with(DefaultNavigationAnimation) { enterTransition() } },
+    exitTransition = { with(DefaultNavigationAnimation) { exitTransition() } },
+    popEnterTransition = { with(DefaultNavigationAnimation) { popEnterTransition() } },
+    popExitTransition = { with(DefaultNavigationAnimation) { popExitTransition() } },
+)

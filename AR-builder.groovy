e29c40f8157f2871f24fc5bca7b6f58da94f6355
Oussama <oussama.has100@gmail.com>
2023-09-05 09:24:57 +0200

    chore: import groovy logger

--- AR-builder.groovy
+++ AR-builder.groovy
@@ -15,6 +15,7 @@
  * You should have received a copy of the GNU General Public License
  * along with this program. If not, see http://www.gnu.org/licenses/.
  */
+import java.util.logging.Logger
 
 String shellQuote(String s) {
     // Quote a string so it's suitable to pass to the shell

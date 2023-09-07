4aa3858df93934ab3df5c2d47651d0397c4cba4a
Michał Saleniuk <saleniuk@gmail.com>
2023-09-07 17:40:55 +0200

    remove unused imports

--- app/src/main/kotlin/com/wire/android/ui/common/button/WirePrimaryIconButton.kt
+++ app/src/main/kotlin/com/wire/android/ui/common/button/WirePrimaryIconButton.kt
@@ -34,7 +34,6 @@ import androidx.compose.ui.graphics.Shape
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.DpSize
 import androidx.compose.ui.unit.dp
 import com.wire.android.R

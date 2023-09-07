9510f5bf36621f78b95ae356ca2b0102f53b4014
Oussama Hassine <oussama.has100@gmail.com>
2023-09-07 10:08:04 +0200

    chore: add app startup benchmark and baseline profiler (WPB-274) (#2181)
    Co-authored-by: Mohamad Jaara <mohamad.jaara@wire.com>

--- buildSrc/src/main/kotlin/scripts/variants.gradle.kts
+++ buildSrc/src/main/kotlin/scripts/variants.gradle.kts
@@ -38,6 +38,7 @@ object BuildTypes {
     const val RELEASE = "release"
     const val COMPAT = "compat"
     const val COMPAT_RELEASE = "compatrelease"
+    const val BENCHMARK = "benchmark"
 }
 
 object Default {
@@ -90,6 +91,12 @@ android {
                 keyAlias = System.getenv("KEYSTORE_KEY_NAME_COMPAT_RELEASE")
                 keyPassword = System.getenv("KEYPWD_COMPAT_RELEASE")
             }
+            maybeCreate(BuildTypes.BENCHMARK).apply {
+                storeFile = file(System.getenv("KEYSTORE_FILE_PATH_DEBUG"))
+                storePassword = System.getenv("KEYSTOREPWD_DEBUG")
+                keyAlias = System.getenv("KEYSTORE_KEY_NAME_DEBUG")
+                keyPassword = System.getenv("KEYPWD_DEBUG")
+            }
         }
     }
 
@@ -127,6 +134,14 @@ android {
             matchingFallbacks.add("release")
             if (enableSigning)
                 signingConfig = signingConfigs.getByName("compatrelease")
+        }
+        create(BuildTypes.BENCHMARK) {
+            initWith(getByName(BuildTypes.RELEASE))
+            isMinifyEnabled = true
+            proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
+            isDebuggable = false
+            matchingFallbacks.add("release")
+            signingConfig = signingConfigs.getByName("debug")
         }
     }
 

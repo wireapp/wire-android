e5a7455b5551f25334cc172bee127a098c2965df
Jacob Persson <7156+typfel@users.noreply.github.com>
2023-10-17 14:26:23 +0200

    chore: remove github packages repository

--- build.gradle.kts
+++ build.gradle.kts
@@ -37,13 +37,6 @@ allprojects {
         mavenLocal()
         google()
         mavenCentral()
-        maven {
-            url = uri("https://maven.pkg.github.com/wireapp/core-crypto")
-            credentials {
-                username = getLocalProperty("github.package_registry.user", System.getenv("GITHUB_USER"))
-                password = getLocalProperty("github.package_registry.token", System.getenv("GITHUB_TOKEN"))
-            }
-        }
         maven { url = java.net.URI("https://jitpack.io") }
         maven(url = "https://oss.sonatype.org/content/repositories/snapshots")
     }

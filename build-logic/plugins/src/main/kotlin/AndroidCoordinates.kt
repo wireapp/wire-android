badfb870fd5606da5b2414f592465035a7b66e04
Vitor Hugo Schwaab <vitor@schwaab.dev>
2023-10-19 15:41:01 +0200

    chore: bump version to 4.4.2

--- build-logic/plugins/src/main/kotlin/AndroidCoordinates.kt
+++ build-logic/plugins/src/main/kotlin/AndroidCoordinates.kt
@@ -25,6 +25,6 @@ object AndroidSdk {
 
 object AndroidApp {
     const val id = "com.wire.android"
-    const val versionName = "4.4.1"
+    const val versionName = "4.4.2"
     val versionCode = Versionizer().versionCode
 }

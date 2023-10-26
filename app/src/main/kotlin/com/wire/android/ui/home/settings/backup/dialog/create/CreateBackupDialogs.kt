a60a8f8b9e08f2691f3323a39c61b0ef41fdb288
Jakub Żerko <iot.zerko@gmail.com>
2023-10-26 11:31:23 +0200

    feat: backup password validation [WPB-4374] (#2370)

--- app/src/main/kotlin/com/wire/android/ui/home/settings/backup/dialog/create/CreateBackupDialogs.kt
+++ app/src/main/kotlin/com/wire/android/ui/home/settings/backup/dialog/create/CreateBackupDialogs.kt
@@ -25,6 +25,7 @@ import androidx.compose.animation.core.animateFloatAsState
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.LinearProgressIndicator
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
@@ -42,18 +43,20 @@ import com.wire.android.ui.common.WireDialog
 import com.wire.android.ui.common.WireDialogButtonProperties
 import com.wire.android.ui.common.WireDialogButtonType
 import com.wire.android.ui.common.button.WireButtonState
+import com.wire.android.ui.common.dimensions
 import com.wire.android.ui.common.spacers.VerticalSpace
 import com.wire.android.ui.common.textfield.WirePasswordTextField
 import com.wire.android.ui.common.textfield.WireTextFieldState
 import com.wire.android.ui.common.wireDialogPropertiesBuilder
 import com.wire.android.ui.theme.wireTypography
 import com.wire.android.util.permission.rememberCreateFileFlow
+import com.wire.kalium.logic.feature.auth.ValidatePasswordResult
 import java.util.Locale
 import kotlin.math.roundToInt
 
 @Composable
 fun SetBackupPasswordDialog(
-    isBackupPasswordValid: Boolean,
+    passwordValidation: ValidatePasswordResult,
     onBackupPasswordChanged: (TextFieldValue) -> Unit,
     onCreateBackup: (String) -> Unit,
     onDismissDialog: () -> Unit
@@ -73,12 +76,14 @@ fun SetBackupPasswordDialog(
             onClick = { onCreateBackup(backupPassword.text) },
             text = stringResource(id = R.string.backup_dialog_create_backup_now),
             type = WireDialogButtonType.Primary,
-            state = if (!isBackupPasswordValid) WireButtonState.Disabled else WireButtonState.Default
+            state = if (passwordValidation.isValid) WireButtonState.Default else WireButtonState.Disabled
         )
     ) {
         WirePasswordTextField(
+            modifier = Modifier.padding(bottom = dimensions().spacing16x),
             labelText = stringResource(R.string.label_textfield_optional_password).uppercase(Locale.getDefault()),
-            state = if (!isBackupPasswordValid) WireTextFieldState.Error("some error") else WireTextFieldState.Default,
+            descriptionText = stringResource(R.string.create_account_details_password_description),
+            state = if (passwordValidation.isValid) WireTextFieldState.Default else WireTextFieldState.Error(),
             value = backupPassword,
             onValueChange = {
                 backupPassword = it

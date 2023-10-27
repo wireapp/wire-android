456e21e9c82840282eff73cd16ca13c46bb1a769
Jakub Żerko <iot.zerko@gmail.com>
2023-10-27 16:22:01 +0200

    detekt fix

--- app/src/main/kotlin/com/wire/android/ui/common/AttachmentButton.kt
+++ app/src/main/kotlin/com/wire/android/ui/common/AttachmentButton.kt
@@ -27,7 +27,6 @@ import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.CircleShape

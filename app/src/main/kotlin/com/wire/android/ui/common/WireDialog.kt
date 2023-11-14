6502dfb25807ce173957041f3a43e0f40ac9758b
Mojtaba Chenani <chenani@outlook.com>
2023-11-14 16:25:53 +0100

    remove unused imports

--- app/src/main/kotlin/com/wire/android/ui/common/WireDialog.kt
+++ app/src/main/kotlin/com/wire/android/ui/common/WireDialog.kt
@@ -31,9 +31,7 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text

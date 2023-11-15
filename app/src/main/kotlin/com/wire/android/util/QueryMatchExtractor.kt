8a33e728c39a4c60becbc1a468a00c6c7ed2b413
Jakub Żerko <iot.zerko@gmail.com>
2023-11-15 14:14:08 +0800

    feat: search message higlight

--- app/src/main/kotlin/com/wire/android/util/QueryMatchExtractor.kt
+++ app/src/main/kotlin/com/wire/android/util/QueryMatchExtractor.kt
@@ -20,9 +20,6 @@
 
 package com.wire.android.util
 
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.withContext
-
 object QueryMatchExtractor {
     /**
      * extractHighLightIndexes is a recursive function returning a list of the start index and end index
@@ -30,35 +27,35 @@ object QueryMatchExtractor {
      * [resultMatches] contains a list of QueryResult with startIndex and endIndex if the match is found.
      * [startIndex] is a index from which we start the search
      */
-    suspend fun extractQueryMatchIndexes(
+    fun extractQueryMatchIndexes(
         resultMatches: List<MatchQueryResult> = emptyList(),
         startIndex: Int = 0,
         matchText: String,
         text: String
-    ): List<MatchQueryResult> =
-        withContext(Dispatchers.Default) {
-            if (matchText.isEmpty()) {
-                return@withContext listOf()
-            }
-            val index = text.indexOf(matchText, startIndex = startIndex, ignoreCase = true)
-
-            if (isIndexFound(index)) {
-                extractQueryMatchIndexes(
-                    resultMatches = resultMatches + MatchQueryResult(
-                        startIndex = index,
-                        endIndex = index + matchText.length
-                    ),
-                    // we are incrementing the startIndex by 1 for the next recursion
-                    // to start looking for the match from the next index that we ended up
-                    // finding the match for the matchText
-                    startIndex = index + 1,
-                    matchText = matchText,
-                    text = text
-                )
-            } else {
-                resultMatches
-            }
+    ): List<MatchQueryResult> {
+        if (matchText.isEmpty()) {
+            return listOf()
         }
+        val index = text.indexOf(matchText, startIndex = startIndex, ignoreCase = true)
+
+        return if (isIndexFound(index)) {
+            extractQueryMatchIndexes(
+                resultMatches = resultMatches + MatchQueryResult(
+                    startIndex = index,
+                    endIndex = index + matchText.length
+                ),
+                // we are incrementing the startIndex by 1 for the next recursion
+                // to start looking for the match from the next index that we ended up
+                // finding the match for the matchText
+                startIndex = index + 1,
+                matchText = matchText,
+                text = text
+            )
+        } else {
+            resultMatches
+        }
+    }
+
 
     private fun isIndexFound(index: Int): Boolean {
         return index != -1

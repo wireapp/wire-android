f6b4479e424b6a0996f64c93719d913b37478767
Mohamad Jaara <mohamad.jaara@wire.com>
2023-11-24 11:40:32 +0100

    fix: app lock not disabled when team enforce is lifted (#2466)
    (cherry picked from commit f593408bebc8864bff22ce07c924966c7e18f1f3)

--- app/src/main/kotlin/com/wire/android/ui/WireActivity.kt
+++ app/src/main/kotlin/com/wire/android/ui/WireActivity.kt
@@ -292,6 +292,7 @@ class WireActivity : AppCompatActivity() {
                         } else {
                             with(featureFlagNotificationViewModel) {
                                 markTeamAppLockStatusAsNot()
+                                confirmAppLockNotEnforced()
                             }
                         }
                     }

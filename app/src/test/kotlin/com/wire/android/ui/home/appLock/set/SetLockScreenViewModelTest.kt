1eef3528bad14c674dea8f6ed6d3d10433021d49
Alexandre Ferris <ferris.alexandre@gmail.com>
2023-11-28 14:07:46 +0100

    fix: disable back press when app lock team enforced (WPB-5644) (#2474)

--- app/src/test/kotlin/com/wire/android/ui/home/appLock/set/SetLockScreenViewModelTest.kt
+++ app/src/test/kotlin/com/wire/android/ui/home/appLock/set/SetLockScreenViewModelTest.kt
@@ -33,6 +33,7 @@ import io.mockk.every
 import io.mockk.impl.annotations.MockK
 import io.mockk.verify
 import kotlinx.coroutines.flow.flowOf
+import kotlinx.coroutines.test.runTest
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.extension.ExtendWith
 
@@ -40,7 +41,7 @@ import org.junit.jupiter.api.extension.ExtendWith
 class SetLockScreenViewModelTest {
 
     @Test
-    fun `given new password input, when valid,then should update state`() {
+    fun `given new password input, when valid,then should update state`() = runTest {
         val (arrangement, viewModel) = Arrangement()
             .withValidPassword()
             .arrange()
@@ -54,7 +55,7 @@ class SetLockScreenViewModelTest {
     }
 
     @Test
-    fun `given new password input, when invalid,then should update state`() {
+    fun `given new password input, when invalid,then should update state`() = runTest {
         val (arrangement, viewModel) = Arrangement()
             .withInvalidPassword()
             .arrange()
@@ -90,6 +91,7 @@ class SetLockScreenViewModelTest {
             coEvery { observeAppLockConfig() } returns flowOf(
                 AppLockConfig.Disabled(ObserveAppLockConfigUseCase.DEFAULT_APP_LOCK_TIMEOUT)
             )
+            coEvery { isAppLockEditableUseCase() } returns true
         }
 
         fun withValidPassword() = apply {

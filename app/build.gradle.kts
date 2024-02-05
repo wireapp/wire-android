445a70347e5b9fe98ed2e0db34fb120480e33c81
yamilmedina <yamilmedina@users.noreply.github.com>
2024-02-05 18:24:50 +0100

    chore: add config to support junit4 with vintage

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -164,6 +164,7 @@ dependencies {
     testImplementation(libs.kluent.core)
     testImplementation(libs.turbine)
     testImplementation(libs.okio.fakeFileSystem)
+    testImplementation(libs.robolectric)
     testRuntimeOnly(libs.junit5.vintage.engine)
     testRuntimeOnly(libs.junit5.engine)
     testImplementation(libs.androidx.paging.testing)

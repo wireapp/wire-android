cab10f5b331a99909b4c48693ebc762e4f76ea50
Oussama Hassine <oussama.has100@gmail.com>
2024-02-12 11:45:40 +0100

    fix: Use IDP clientId from remote

--- app/src/main/kotlin/com/wire/android/util/UriUtil.kt
+++ app/src/main/kotlin/com/wire/android/util/UriUtil.kt
@@ -66,3 +66,12 @@ fun URI.removeQueryParams(): URI {
     val regex = Regex("[?&][^=]+=[^&]*")
     return URI(this.toString().replace(regex, ""))
 }
+
+fun URI.findParameterValue(parameterName: String): String? {
+    return rawQuery.split('&').map {
+        val parts = it.split('=')
+        val name = parts.firstOrNull() ?: ""
+        val value = parts.drop(1).firstOrNull() ?: ""
+        Pair(name, value)
+    }.firstOrNull { it.first == parameterName }?.second
+}

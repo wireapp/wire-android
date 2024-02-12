475264d549ccde3d5e7de89312355b336e18c25d
MohamadJaara <mohamad.jaara@wire.com>
2024-02-12 10:08:40 +0100

    chore: update app version to 4.5.5

--- build-logic/plugins/src/main/kotlin/AndroidCoordinates.kt
+++ build-logic/plugins/src/main/kotlin/AndroidCoordinates.kt
@@ -25,6 +25,6 @@ object AndroidSdk {
 
 object AndroidApp {
     const val id = "com.wire.android"
-    const val versionName = "4.5.4"
+    const val versionName = "4.5.5"
     val versionCode = Versionizer().versionCode
 }

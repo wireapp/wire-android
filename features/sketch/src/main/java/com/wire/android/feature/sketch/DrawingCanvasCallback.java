a84c26ba592d03111ee8fe9fcf48c1403f003db6
yamilmedina <yamilmedina@users.noreply.github.com>
2024-02-20 17:35:43 +0100

    chore: add as is classes

---
+++ features/sketch/src/main/java/com/wire/android/feature/sketch/DrawingCanvasCallback.java
@@ -0,0 +1,36 @@
+/**
+ * Wire
+ * Copyright (C) 2018 Wire Swiss GmbH
+ * <p>
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * <p>
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * <p>
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package com.wire.android.feature.sketch;
+
+public interface DrawingCanvasCallback {
+    void drawingAdded();
+
+    void drawingCleared();
+
+    void reserveBitmapMemory(int width, int height);
+
+    void onScaleChanged(float scale);
+
+    void onScaleStart();
+
+    void onScaleEnd();
+
+    void onTextChanged(String text, int x, int y, float scale);
+
+    void onTextRemoved();
+}

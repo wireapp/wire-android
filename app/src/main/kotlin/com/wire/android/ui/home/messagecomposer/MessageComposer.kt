8518c9be9900330a24518ecc40555aec339a1370
Michał Saleniuk <saleniuk@gmail.com>
2024-03-04 14:37:47 +0100

    remove unused import

--- app/src/main/kotlin/com/wire/android/ui/home/messagecomposer/MessageComposer.kt
+++ app/src/main/kotlin/com/wire/android/ui/home/messagecomposer/MessageComposer.kt
@@ -27,7 +27,6 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Divider
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme

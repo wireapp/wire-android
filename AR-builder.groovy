4aad1581dd484d62451f5ef84f8cd7687fd2241f
Sven Jost <sven@wire.com>
2024-03-04 10:33:51 +0100

    build: Keep builds on Jenkins if certain branches were build

--- AR-builder.groovy
+++ AR-builder.groovy
@@ -121,6 +121,16 @@ pipeline {
                         }
                     }
                 }
+
+                stage('Keep builds forever if important') {
+                    steps {
+                        script {
+                            if (params.SOURCE_BRANCH == "develop" || params.SOURCE_BRANCH == "release/candidate" || params.SOURCE_BRANCH == "main" || params.SOURCE_BRANCH == "prod") {
+                                currentBuild.keepLog = true
+                            }
+                        }
+                    }
+                }
             }
         }
 

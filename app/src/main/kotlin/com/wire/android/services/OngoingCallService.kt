d236f01aed8cd2ee7ce7106a634ee5239ca38e37
Oussama Hassine <oussama.has100@gmail.com>
2024-03-11 13:10:12 +0100

    chore: detekt

--- app/src/main/kotlin/com/wire/android/services/OngoingCallService.kt
+++ app/src/main/kotlin/com/wire/android/services/OngoingCallService.kt
@@ -23,7 +23,6 @@ import android.app.Service
 import android.content.Context
 import android.content.Intent
 import android.content.pm.ServiceInfo
-import android.os.Build
 import android.os.IBinder
 import com.wire.android.appLogger
 import com.wire.android.di.KaliumCoreLogic

6f53ebe09b24679b9c3be6a16c3e4f92a27b9236
Vitor Hugo Schwaab <vitor@schwaab.dev>
2024-04-02 13:04:08 +0200

    chore: inline buildSrc dependencies

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -16,18 +16,6 @@
  * along with this program. If not, see http://www.gnu.org/licenses/.
  */
 
-private object Dependencies {
-    val kotlinGradlePlugin = "org.jetbrains.kotlin:kotlin-gradle-plugin:${libs.versions.kotlin.get()}"
-    val detektGradlePlugin = "io.gitlab.arturbosch.detekt:detekt-gradle-plugin:${klibs.versions.detekt.get()}"
-    val koverGradlePlugin = "org.jetbrains.kotlinx:kover-gradle-plugin:${klibs.versions.kover.get()}"
-    val junit = "junit:junit:${libs.versions.junit4.get()}"
-    val kluent = "org.amshove.kluent:kluent:${libs.versions.kluent.get()}"
-    const val spotless = "com.diffplug.spotless:spotless-plugin-gradle:6.1.2"
-    const val junit5 = "de.mannodermaus.gradle.plugins:android-junit5:1.9.3.0"
-    const val grgit = "org.ajoberstar.grgit:grgit-core:5.2.0"
-    const val javapoet = "com.squareup:javapoet:1.13.0"
-}
-
 plugins {
     `kotlin-dsl`
     `kotlin-dsl-precompiled-script-plugins`
@@ -44,13 +32,13 @@ repositories {
 dependencies {
     implementation("com.android.tools.build:gradle:${klibs.versions.agp.get()}")
     implementation(libs.kotlin.gradlePlugin)
-    implementation(Dependencies.detektGradlePlugin)
-    implementation(Dependencies.koverGradlePlugin)
-    implementation(Dependencies.spotless)
-    implementation(Dependencies.junit5)
+    implementation("io.gitlab.arturbosch.detekt:detekt-gradle-plugin:${klibs.versions.detekt.get()}")
+    implementation("org.jetbrains.kotlinx:kover-gradle-plugin:${klibs.versions.kover.get()}")
+    implementation(libs.spotless.gradlePlugin)
+    implementation(libs.android.junit5)
 
-    testImplementation(Dependencies.junit)
-    testImplementation(Dependencies.kluent)
-    implementation(Dependencies.grgit)
-    implementation(Dependencies.javapoet) // https://github.com/google/dagger/issues/3068
+    testImplementation("junit:junit:${libs.versions.junit4.get()}")
+    testImplementation("org.amshove.kluent:kluent:${libs.versions.kluent.get()}")
+    implementation(libs.grgit.core)
+    implementation(libs.squareup.javapoet) // https://github.com/google/dagger/issues/3068
 }

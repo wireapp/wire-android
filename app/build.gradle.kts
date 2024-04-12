844f58f4ad71c176bcfba8897ed739640b86099f
Lisa Marie Maginnis <lisa@westvirginia.express>
2024-04-12 17:54:11 +0200

    fix: startup crash with fdroid [WPB-7286] (#2845)
    Co-authored-by: Mohamad Jaara <mohamad.jaara@wire.com>

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -162,10 +162,15 @@ dependencies {
     implementation(libs.bundlizer.core)
 
     // firebase
-    implementation(platform(libs.firebase.bom))
-    implementation(libs.firebase.fcm)
+    var fdroidBuild = gradle.startParameter.taskRequests.toString().lowercase().contains("fdroid")
+	if (!fdroidBuild) {
+        implementation(platform(libs.firebase.bom))
+        implementation(libs.firebase.fcm)
+        implementation(libs.googleGms.location)
+    } else {
+        println("Excluding FireBase for FDroid build")
+    }
     implementation(libs.androidx.work)
-    implementation(libs.googleGms.location)
 
     // commonMark
     implementation(libs.commonmark.core)

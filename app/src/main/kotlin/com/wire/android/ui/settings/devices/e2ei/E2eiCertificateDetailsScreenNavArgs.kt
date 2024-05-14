968539a113b9cba273c0caba0767250105ee0f11
Mojtaba Chenani <chenani@outlook.com>
2024-05-14 11:30:33 +0200

    add serial name

--- app/src/main/kotlin/com/wire/android/ui/settings/devices/e2ei/E2eiCertificateDetailsScreenNavArgs.kt
+++ app/src/main/kotlin/com/wire/android/ui/settings/devices/e2ei/E2eiCertificateDetailsScreenNavArgs.kt
@@ -18,6 +18,7 @@
 package com.wire.android.ui.settings.devices.e2ei
 
 import com.wire.kalium.logic.feature.e2ei.E2eiCertificate
+import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
 
 data class E2eiCertificateDetailsScreenNavArgs(val certificateDetails: E2EICertificateDetails)
@@ -25,7 +26,7 @@ data class E2eiCertificateDetailsScreenNavArgs(val certificateDetails: E2EICerti
 @Serializable
 sealed class E2EICertificateDetails {
     @Serializable
-    data class AfterLoginCertificateDetails(val certificate: E2eiCertificate) : E2EICertificateDetails()
+    data class AfterLoginCertificateDetails(@SerialName("certificate") val certificate: E2eiCertificate) : E2EICertificateDetails()
     @Serializable
-    data class DuringLoginCertificateDetails(val certificate: String) : E2EICertificateDetails()
+    data class DuringLoginCertificateDetails(@SerialName("certificate") val certificate: String) : E2EICertificateDetails()
 }

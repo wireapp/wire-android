2616c8cae232d37d83fea26ae955f418abac29f7
Michał Saleniuk <saleniuk@gmail.com>
2024-05-22 13:04:12 +0200

    fix after merge

--- app/src/main/kotlin/com/wire/android/ui/common/textfield/WirePasswordTextField.kt
+++ app/src/main/kotlin/com/wire/android/ui/common/textfield/WirePasswordTextField.kt
@@ -38,6 +38,8 @@ import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.Stable
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue

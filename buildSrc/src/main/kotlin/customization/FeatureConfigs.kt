d04f89f9abfa1ba08655bb88771a3e0ecc73bfd5
alexandreferris <ferris.alexandre@gmail.com>
2024-06-05 10:09:24 +0200

    feat: add FeatureConfigs for analytics
    Signed-off-by: alexandreferris <ferris.alexandre@gmail.com>

--- buildSrc/src/main/kotlin/customization/FeatureConfigs.kt
+++ buildSrc/src/main/kotlin/customization/FeatureConfigs.kt
@@ -102,9 +102,9 @@ enum class FeatureConfigs(val value: String, val configType: ConfigType) {
     LIMIT_TEAM_MEMBERS_FETCH_DURING_SLOW_SYNC("limit_team_members_fetch_during_slow_sync", ConfigType.INT),
 
     /**
-     * Countly Analytics
+     * Anonymous Analytics
      */
-    COUNTLY_ENABLED("countly_enabled", ConfigType.BOOLEAN),
-    COUNTLY_APP_KEY("countly_app_key", ConfigType.STRING),
-    COUNTLY_SERVER_URL("countly_server_url", ConfigType.STRING)
+    ANALYTICS_ENABLED("analytics_enabled", ConfigType.BOOLEAN),
+    ANALYTICS_APP_KEY("analytics_app_key", ConfigType.STRING),
+    ANALYTICS_SERVER_URL("analytics_server_url", ConfigType.STRING)
 }

f0221d32fc59a8f2c25a57bef7cd188a0ebbff38
Vitor Hugo Schwaab <vitor@schwaab.dev>
2024-06-14 16:03:33 +0200

    chore: upgrade to kotlin 2.0

--- build-logic/plugins/src/main/kotlin/AndroidApplicationConventionPlugin.kt
+++ build-logic/plugins/src/main/kotlin/AndroidApplicationConventionPlugin.kt
@@ -28,6 +28,7 @@ class AndroidApplicationConventionPlugin : Plugin<Project> {
         with(pluginManager) {
             apply("com.android.application")
             apply("org.jetbrains.kotlin.android")
+            apply("org.jetbrains.kotlin.plugin.compose")
         }
 
         extensions.configure<ApplicationExtension> {

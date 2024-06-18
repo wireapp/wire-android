407e42c7e2aa3e6719f6347ecc2b23111059cba4
boris <safonovboris@gmail.com>
2024-06-18 07:31:53 +0000

    Commit with unresolved merge conflicts outside of submodules

--- app/src/test/kotlin/com/wire/android/ui/settings/devices/SelfDevicesViewModelTest.kt
+++ app/src/test/kotlin/com/wire/android/ui/settings/devices/SelfDevicesViewModelTest.kt
@@ -31,7 +31,11 @@ import com.wire.kalium.logic.feature.client.SelfClientsResult
 import com.wire.kalium.logic.feature.e2ei.usecase.GetUserE2eiCertificatesUseCase
 import com.wire.kalium.logic.feature.user.IsE2EIEnabledUseCase
 import io.mockk.coEvery
+<<<<<<< HEAD
 import io.mockk.MockKAnnotations
+=======
+import io.mockk.coVerify
+>>>>>>> fc8d8ad9d (fix: Re-fetching of devices certificates on comming back from DeviceDetails [WPB-6970] (#3103))
 import io.mockk.impl.annotations.MockK
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.flow.flowOf
@@ -49,12 +53,28 @@ class SelfDevicesViewModelTest {
             // given
             val (_, viewModel) = Arrangement()
                 .arrange()
+            val currentDevice = Device(TestClient.CLIENT)
 
             // when
-            val currentDevice = Device(TestClient.CLIENT)
+            viewModel.loadCertificates()
 
             // then
             assert(!viewModel.state.deviceList.contains(currentDevice))
+        }
+
+    @Test
+    fun `given a self client id, when loadCertificates is called, then E2EI Certificates is fetched again`() =
+        runTest {
+            // given
+            val (arragne, viewModel) = Arrangement()
+                .arrange()
+
+            // when
+            viewModel.loadCertificates()
+            viewModel.loadCertificates()
+
+            // then
+            coVerify(exactly = 2) { arragne.getUserE2eiCertificates(any()) }
         }
 
     private class Arrangement {

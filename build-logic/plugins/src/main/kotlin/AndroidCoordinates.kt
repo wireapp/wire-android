435deb9b5e3dbf7d20390467c736dad9cad4c494
Yamil Medina <yamilmedina@users.noreply.github.com>
2024-06-26 11:58:49 +0200

    chore: bump to 4.7.2

--- build-logic/plugins/src/main/kotlin/AndroidCoordinates.kt
+++ build-logic/plugins/src/main/kotlin/AndroidCoordinates.kt
@@ -25,6 +25,6 @@ object AndroidSdk {
 
 object AndroidApp {
     const val id = "com.wire.android"
-    const val versionName = "4.7.1"
+    const val versionName = "4.7.2"
     val versionCode = Versionizer().versionCode
 }

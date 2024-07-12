37bce40768e31dda0cab929128ef5cb91fef0869
yamilmedina <yamilmedina@users.noreply.github.com>
2024-07-12 11:06:03 +0200

    feat: cleanup code mapper

--- app/src/main/kotlin/com/wire/android/util/DateTimeUtil.kt
+++ app/src/main/kotlin/com/wire/android/util/DateTimeUtil.kt
@@ -41,12 +41,6 @@ private val mediumOnlyDateTimeFormat = DateFormat.getDateInstance(DateFormat.MED
     .apply { timeZone = TimeZone.getDefault() }
 private val messageTimeFormatter = DateFormat.getTimeInstance(DateFormat.SHORT, Locale.getDefault())
     .apply { timeZone = TimeZone.getDefault() }
-private const val ONE_MINUTE_FROM_MILLIS = 60 * 1000
-private const val THIRTY_MINUTES = 30
-private const val ONE_WEEK_IN_DAYS = 7
-private const val ONE_DAY = 1
-private const val FORTY_FIVE_MINUTES_DIFFERENCE = 45
-private const val MINIMUM_DAYS_DIFFERENCE = 1
 
 private val readReceiptDateTimeFormat = SimpleDateFormat(
     "MMM dd yyyy,  hh:mm a",
@@ -104,92 +98,6 @@ fun serverDateOld(stringDate: String): Date? = try {
     null
 }
 
-/**
- * Transforms received Long to Calendar
- *
- * @return Calendar
- */
-private fun Long.getCalendar(): Calendar = Calendar.getInstance().apply {
-    timeInMillis = this@getCalendar
-}
-
-/**
- * Verifies if received dates (date, now) are the same day
- *
- * @param date: Long - message date
- * @param now: Long - current user date when checking the message
- *
- * @return Boolean
- */
-private fun isDatesSameDay(date: Long, now: Long): Boolean {
-    val messageCalendar = date.getCalendar()
-    val nowCalendar = now.getCalendar()
-
-    return nowCalendar.get(Calendar.DAY_OF_MONTH) == messageCalendar.get(Calendar.DAY_OF_MONTH)
-            && nowCalendar.get(Calendar.MONTH) == messageCalendar.get(Calendar.MONTH)
-            && nowCalendar.get(Calendar.YEAR) == messageCalendar.get(Calendar.YEAR)
-}
-
-/**
- * Verifies if received dates (date, (now -1 day)) are the same, meaning its yesterday
- *
- * @param date: Long - message date
- * @param now: Long - current user date when checking the message
- *
- * @return Boolean
- */
-private fun isYesterday(date: Long, now: Long): Boolean {
-    val messageCalendar = date.getCalendar()
-    val nowCalendar = now.getCalendar().apply {
-        add(Calendar.DATE, -ONE_DAY)
-    }
-
-    return nowCalendar.get(Calendar.DAY_OF_MONTH) == messageCalendar.get(Calendar.DAY_OF_MONTH)
-            && nowCalendar.get(Calendar.MONTH) == messageCalendar.get(Calendar.MONTH)
-            && nowCalendar.get(Calendar.YEAR) == messageCalendar.get(Calendar.YEAR)
-}
-
-/**
- * Verifies if received dates (date, (now -7 days)) are within the same week.
- * Checks if message date is equals or after (now -7 days)
- *
- * @param date: Long - message date
- * @param now: Long - current user date when checking the message
- *
- * @return Boolean
- */
-private fun isDatesWithinWeek(date: Long, now: Long): Boolean =
-    date.getCalendar().after(
-        now.getCalendar().apply {
-            add(Calendar.DATE, -ONE_WEEK_IN_DAYS)
-        }
-    )
-
-/**
- * Verifies if received dates are the same year
- *
- * @param date: Long - message date
- * @param now: Long - current user date when checking the message
- *
- * @return Boolean
- */
-private fun isDatesSameYear(date: Long, now: Long): Boolean =
-    date.getCalendar().get(Calendar.YEAR) == now.getCalendar().get(Calendar.YEAR)
-
-sealed interface MessageDateTimeGroup {
-    data object Now : MessageDateTimeGroup
-    data object Within30Minutes : MessageDateTimeGroup
-    data class Daily(val type: Type, val date: LocalDate) : MessageDateTimeGroup {
-        enum class Type {
-            Today,
-            Yesterday,
-            WithinWeek,
-            NotWithinWeekButSameYear,
-            Other
-        }
-    }
-}
-
 @Deprecated(
     message = "This implementation will be removed in the future as it uses discouraged/outdated SimpleDateFormat",
     replaceWith = ReplaceWith("DateAndTimeParsers.uiMessageDateTime() or String.uiMessageDateTime()"),
@@ -197,65 +105,6 @@ sealed interface MessageDateTimeGroup {
 fun String.uiMessageDateTimeOld(): String? = this
     .serverDate()?.let { serverDate ->
         messageTimeFormatter.format(serverDate)
-    }
-
-fun String.shouldDisplayDatesDifferenceDivider(previousDate: String): Boolean {
-    val currentDate = this@shouldDisplayDatesDifferenceDivider
-
-    val currentLocalDateTime = currentDate.serverDate()?.toInstant()?.atZone(ZoneId.systemDefault())?.toLocalDateTime()
-    val previousLocalDateTime = previousDate.serverDate()?.toInstant()?.atZone(ZoneId.systemDefault())?.toLocalDateTime()
-
-    val differenceInMinutes = ChronoUnit.MINUTES.between(
-        currentLocalDateTime,
-        previousLocalDateTime
-    )
-
-    val differenceInDays = ChronoUnit.DAYS.between(
-        currentLocalDateTime,
-        previousLocalDateTime
-    )
-
-    return differenceInMinutes > FORTY_FIVE_MINUTES_DIFFERENCE || differenceInDays >= MINIMUM_DAYS_DIFFERENCE
-}
-
-fun String.groupedUIMessageDateTime(now: Long): MessageDateTimeGroup? = this
-    .serverDate()?.let { serverDate ->
-        val localDate = serverDate.toInstant().atZone(ZoneId.systemDefault()).toLocalDate()
-        val serverDateInMillis = serverDate.time
-        val differenceBetweenServerDateAndNow = now - serverDateInMillis
-        val differenceInMinutes: Long = differenceBetweenServerDateAndNow / ONE_MINUTE_FROM_MILLIS
-        val isSameDay = isDatesSameDay(date = serverDateInMillis, now = now)
-        val withinWeek = isDatesWithinWeek(date = serverDateInMillis, now = now)
-        val isSameYear = isDatesSameYear(date = serverDateInMillis, now = now)
-
-        when {
-            differenceBetweenServerDateAndNow < ONE_MINUTE_FROM_MILLIS -> MessageDateTimeGroup.Now
-            differenceInMinutes <= THIRTY_MINUTES -> MessageDateTimeGroup.Within30Minutes
-            differenceInMinutes > THIRTY_MINUTES && isSameDay -> MessageDateTimeGroup.Daily(
-                type = MessageDateTimeGroup.Daily.Type.Today,
-                date = localDate
-            )
-
-            isYesterday(serverDateInMillis, now) -> MessageDateTimeGroup.Daily(
-                type = MessageDateTimeGroup.Daily.Type.Yesterday,
-                date = localDate
-            )
-
-            withinWeek -> MessageDateTimeGroup.Daily(
-                type = MessageDateTimeGroup.Daily.Type.WithinWeek,
-                date = localDate
-            )
-
-            !withinWeek && isSameYear -> MessageDateTimeGroup.Daily(
-                type = MessageDateTimeGroup.Daily.Type.NotWithinWeekButSameYear,
-                date = localDate
-            )
-
-            else -> MessageDateTimeGroup.Daily(
-                type = MessageDateTimeGroup.Daily.Type.Other,
-                date = localDate
-            )
-        }
     }
 
 @Deprecated(
---
+++ app/src/test/kotlin/com/wire/android/mapper/MessageDateGroupingMapperTest.kt
@@ -0,0 +1,148 @@
+/*
+ * Wire
+ * Copyright (C) 2024 Wire Swiss GmbH
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see http://www.gnu.org/licenses/.
+ */
+package com.wire.android.mapper
+
+import org.junit.jupiter.api.AfterAll
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.BeforeAll
+import org.junit.jupiter.api.Test
+import java.util.Calendar
+import java.util.Locale
+import java.util.TimeZone
+
+class MessageDateGroupingMapperTest {
+
+    @Test
+    fun `return MessageDateTime_Now when a valid date is calling groupedUIMessageDateTime`() {
+        val result = "2024-01-20T07:00:00.000Z".groupedUIMessageDateTime(getDummyCalendar().timeInMillis)
+        assertEquals(MessageDateTimeGroup.Now, result)
+    }
+
+    @Test
+    fun `return MessageDateTime_Within30Minutes when a valid date within 10 minutes is calling groupedUIMessageDateTime`() {
+        val result = "2024-01-20T07:00:00.000Z".groupedUIMessageDateTime(
+            getDummyCalendar().apply {
+                add(Calendar.MINUTE, 10)
+            }.timeInMillis
+        )
+        assertEquals(MessageDateTimeGroup.Within30Minutes, result)
+    }
+
+    @Test
+    fun `return MessageDateTime_Today when a valid date over 30 minutes is calling groupedUIMessageDateTime`() {
+        val result = "2024-01-20T07:00:00.000Z".groupedUIMessageDateTime(
+            getDummyCalendar().apply {
+                add(Calendar.MINUTE, 31)
+            }.timeInMillis
+        )
+        assertEquals(
+            MessageDateTimeGroup.Daily.Type.Today,
+            (result as MessageDateTimeGroup.Daily).type
+        )
+        assertEquals(
+            "2024-01-20",
+            result.date.toString()
+        )
+    }
+
+    @Test
+    fun `return MessageDateTime_Yesterday when a valid date over 1 day is calling groupedUIMessageDateTime`() {
+        val result = "2024-01-20T07:00:00.000Z".groupedUIMessageDateTime(
+            getDummyCalendar().apply {
+                add(Calendar.DATE, 1)
+            }.timeInMillis
+        )
+
+        assertEquals(
+            MessageDateTimeGroup.Daily.Type.Yesterday,
+            (result as MessageDateTimeGroup.Daily).type
+        )
+    }
+
+    @Test
+    fun `return MessageDateTime_WithinWeek when a valid date within 7 days 1 day is calling groupedUIMessageDateTime`() {
+        val result = "2024-01-20T07:00:00.000Z".groupedUIMessageDateTime(
+            getDummyCalendar().apply {
+                add(Calendar.DATE, 3)
+            }.timeInMillis
+        )
+
+        assertEquals(
+            MessageDateTimeGroup.Daily.Type.WithinWeek,
+            (result as MessageDateTimeGroup.Daily).type
+        )
+    }
+
+    @Test
+    fun `return MessageDateTime_NotWithinWeekButSameYear when date over 7 days and same year is calling groupedUIMessageDateTime`() {
+        val result = "2024-01-20T07:00:00.000Z".groupedUIMessageDateTime(
+            getDummyCalendar().apply {
+                add(Calendar.DATE, 10)
+            }.timeInMillis
+        )
+        assertEquals(
+            MessageDateTimeGroup.Daily.Type.NotWithinWeekButSameYear,
+            (result as MessageDateTimeGroup.Daily).type
+        )
+    }
+
+    @Test
+    fun `return MessageDateTime_Other given valid date, when a valid date and different year is calling groupedUIMessageDateTime`() {
+        val result = "2024-01-20T07:00:00.000Z".groupedUIMessageDateTime(
+            getDummyCalendar().apply {
+                set(Calendar.YEAR, 2025)
+            }.timeInMillis
+        )
+        assertEquals(
+            MessageDateTimeGroup.Daily.Type.Other,
+            (result as MessageDateTimeGroup.Daily).type
+        )
+    }
+
+    private fun getDummyCalendar(): Calendar = Calendar.getInstance().apply {
+        set(Calendar.SECOND, 0)
+        set(Calendar.MINUTE, 0)
+        set(Calendar.HOUR, 7)
+        set(Calendar.AM_PM, Calendar.AM)
+        set(Calendar.MONTH, Calendar.JANUARY)
+        set(Calendar.DAY_OF_MONTH, 20)
+        set(Calendar.YEAR, 2024)
+    }
+
+    companion object {
+        private var systemDefaultLocale: Locale? = null
+        private var systemDefaultTimeZone: TimeZone? = null
+
+        @JvmStatic
+        @BeforeAll
+        fun setup() {
+            systemDefaultTimeZone = TimeZone.getDefault()
+            systemDefaultLocale = Locale.getDefault()
+            TimeZone.setDefault(TimeZone.getTimeZone("UTC"))
+            Locale.setDefault(Locale.US)
+        }
+
+        @JvmStatic
+        @AfterAll
+        fun tearDown() {
+            TimeZone.setDefault(systemDefaultTimeZone!!)
+            Locale.setDefault(systemDefaultLocale!!)
+        }
+    }
+
+}

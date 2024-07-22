320b580c04c4d15bbd50b664b9793dd0995ff3ed
Michał Saleniuk <30429749+saleniuk@users.noreply.github.com>
2024-07-22 10:02:00 +0200

    fix: "All Android Release Notes" item is not clickable - 🍒 v4.6 [WPB-10125]  (#3211)

--- app/src/main/kotlin/com/wire/android/ui/home/whatsnew/WhatsNewScreen.kt
+++ app/src/main/kotlin/com/wire/android/ui/home/whatsnew/WhatsNewScreen.kt
@@ -65,13 +65,14 @@ fun WhatsNewScreen(
 @Composable
 fun WhatsNewScreenContent(
     state: WhatsNewState,
-    lazyListState: LazyListState = rememberLazyListState(),
-    onItemClicked: (WhatsNewItem) -> Unit
+    onItemClicked: (WhatsNewItem) -> Unit,
+    modifier: Modifier = Modifier,
+    lazyListState: LazyListState = rememberLazyListState()
 ) {
     val context = LocalContext.current
     LazyColumn(
         state = lazyListState,
-        modifier = Modifier.fillMaxSize()
+        modifier = modifier.fillMaxSize()
     ) {
         folderWithElements(
             items = buildList {
@@ -85,6 +86,7 @@ fun WhatsNewScreenContent(
             header = context.getString(R.string.whats_new_release_notes_group_title),
             items = buildList {
                 if (state.isLoading) {
+                    // placeholders with shimmer effect
                     for (i in 0..3) {
                         add(
                             WhatsNewItem.AndroidReleaseNotes(
@@ -96,6 +98,7 @@ fun WhatsNewScreenContent(
                             )
                         )
                     }
+                    add(WhatsNewItem.AllAndroidReleaseNotes(id = "placeholder_all"))
                 } else {
                     state.releaseNotesItems.forEach {
                         add(
@@ -108,8 +111,8 @@ fun WhatsNewScreenContent(
                             )
                         )
                     }
+                    add(WhatsNewItem.AllAndroidReleaseNotes())
                 }
-                add(WhatsNewItem.AllAndroidReleaseNotes)
             },
             onItemClicked = onItemClicked,
             isLoading = state.isLoading,
@@ -131,7 +134,7 @@ private fun LazyListScope.folderWithElements(
             title = item.title.asString(),
             boldTitle = item.boldTitle,
             text = item.text?.asString(),
-            onRowPressed = remember { Clickable(enabled = !isLoading) { onItemClicked(item) } },
+            onRowPressed = remember(isLoading) { Clickable(enabled = !isLoading) { onItemClicked(item) } },
             trailingIcon = R.drawable.ic_arrow_right,
             isLoading = isLoading,
         )
@@ -141,11 +144,24 @@ private fun LazyListScope.folderWithElements(
 @Preview(showBackground = false)
 @Composable
 fun PreviewWhatsNewScreen() {
-    WhatsNewScreenContent(WhatsNewState(isLoading = false)) {}
+    WhatsNewScreenContent(
+        state = WhatsNewState(
+            isLoading = false,
+            releaseNotesItems = buildList {
+                for (i in 0..3) {
+                    add(ReleaseNotesItem(i.toString(), "Title $i", "https://www.example.com", "01 Jan 2024"))
+                }
+            }
+        ),
+        onItemClicked = {}
+    )
 }
 
 @Preview(showBackground = false)
 @Composable
 fun PreviewWhatsNewScreenLoading() {
-    WhatsNewScreenContent(WhatsNewState(isLoading = true)) {}
+    WhatsNewScreenContent(
+        state = WhatsNewState(isLoading = true),
+        onItemClicked = {}
+    )
 }

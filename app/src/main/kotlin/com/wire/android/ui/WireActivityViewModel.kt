a29f836dd5d7baf795b038e4f7434870c0e85822
Jakub Zerko <iot.zerko@gmail.com>
2024-07-22 14:05:58 +0200

    fix: user logged out listener

--- app/src/main/kotlin/com/wire/android/ui/WireActivityViewModel.kt
+++ app/src/main/kotlin/com/wire/android/ui/WireActivityViewModel.kt
@@ -84,13 +84,13 @@ import kotlinx.coroutines.async
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.SharingStarted
 import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.collectLatest
 import kotlinx.coroutines.flow.distinctUntilChanged
 import kotlinx.coroutines.flow.first
 import kotlinx.coroutines.flow.flatMapLatest
 import kotlinx.coroutines.flow.flowOf
 import kotlinx.coroutines.flow.flowOn
 import kotlinx.coroutines.flow.map
-import kotlinx.coroutines.flow.onEach
 import kotlinx.coroutines.flow.shareIn
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
@@ -131,13 +131,6 @@ class WireActivityViewModel @Inject constructor(
     private val userIdDeferred: Deferred<UserId?> = viewModelScope.async(dispatchers.io()) {
         currentSessionFlow.get().invoke()
             .distinctUntilChanged()
-            .onEach {
-                if (it is CurrentSessionResult.Success) {
-                    if (it.accountInfo.isValid().not()) {
-                        handleInvalidSession((it.accountInfo as AccountInfo.Invalid).logoutReason)
-                    }
-                }
-            }
             .map { result ->
                 if (result is CurrentSessionResult.Success) {
                     if (result.accountInfo.isValid()) {
@@ -160,6 +153,7 @@ class WireActivityViewModel @Inject constructor(
         observeNewClientState()
         observeScreenshotCensoringConfigState()
         observeAppThemeState()
+        observeLogoutState()
     }
 
     @Suppress("TooGenericExceptionCaught")
@@ -203,6 +197,20 @@ class WireActivityViewModel @Inject constructor(
             } catch (e: NullPointerException) {
                 appLogger.e("Error while observing sync state: $e")
             }
+        }
+    }
+
+    private fun observeLogoutState() {
+        viewModelScope.launch(dispatchers.io()) {
+            currentSessionFlow.get().invoke()
+                .distinctUntilChanged()
+                .collectLatest {
+                    if (it is CurrentSessionResult.Success) {
+                        if (it.accountInfo.isValid().not()) {
+                            handleInvalidSession((it.accountInfo as AccountInfo.Invalid).logoutReason)
+                        }
+                    }
+                }
         }
     }
 

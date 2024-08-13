16526627e0a017181c660bcaf22e37550c71bdc0
yamilmedina <yamilmedina@users.noreply.github.com>
2024-08-13 18:10:10 +0200

    feat: adjust linter

--- app/src/main/kotlin/com/wire/android/ui/userprofile/common/UserProfileInfo.kt
+++ app/src/main/kotlin/com/wire/android/ui/userprofile/common/UserProfileInfo.kt
@@ -78,6 +78,7 @@ import kotlin.time.Duration
 import kotlin.time.Duration.Companion.hours
 import kotlin.time.Duration.Companion.milliseconds
 
+@SuppressLint("ComposeParameterOrder")
 @Composable
 fun UserProfileInfo(
     userId: UserId?,

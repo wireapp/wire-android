876ee1a02bddad297b03135d49c5ac124faffcdd
Boris Safonov <safonovboris@gmail.com>
2024-08-26 15:06:37 +0300

    Code style fix

--- app/src/main/kotlin/com/wire/android/ui/WireActivityDialogs.kt
+++ app/src/main/kotlin/com/wire/android/ui/WireActivityDialogs.kt
@@ -29,7 +29,6 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text

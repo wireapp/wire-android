375b7222fbed0b277af8c15be59b0faa4595cdca
mohamadjaara <mohamad.jaara@wire.com>
2024-08-28 23:45:20 +0200

    chore: bump app version

--- build-logic/plugins/src/main/kotlin/AndroidCoordinates.kt
+++ build-logic/plugins/src/main/kotlin/AndroidCoordinates.kt
@@ -26,7 +26,7 @@ object AndroidSdk {
 
 object AndroidApp {
     const val id = "com.wire.android"
-    const val versionName = "4.8.3"
+    const val versionName = "4.8.4"
 
     private lateinit var _rootDir: File
 

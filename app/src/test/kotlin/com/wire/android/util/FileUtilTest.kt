c2317fb98fff2aa1fede5088f51b98fc9a165cd3
Michał Saleniuk <30429749+saleniuk@users.noreply.github.com>
2024-09-09 15:06:38 +0200

    fix: some files not visible in downloads folder [WPB-1854] (#3421)

--- app/src/test/kotlin/com/wire/android/util/FileUtilTest.kt
+++ app/src/test/kotlin/com/wire/android/util/FileUtilTest.kt
@@ -18,44 +18,71 @@
 
 package com.wire.android.util
 
+import android.app.Application
+import android.app.DownloadManager
+import android.content.ContentResolver
+import android.content.Context
+import android.net.Uri
+import android.os.Build
+import android.os.Environment
+import androidx.core.content.FileProvider
+import io.mockk.MockKAnnotations
+import io.mockk.coEvery
+import io.mockk.impl.annotations.MockK
+import io.mockk.mockkStatic
+import io.mockk.verify
+import io.mockk.verifyOrder
+import kotlinx.coroutines.test.runTest
+import okio.Path.Companion.toOkioPath
 import org.amshove.kluent.internal.assertEquals
-import org.junit.jupiter.api.Test
-import org.junit.jupiter.api.io.TempDir
+import org.junit.Rule
+import org.junit.Test
+import org.junit.rules.TemporaryFolder
+import org.junit.runner.RunWith
+import org.robolectric.RobolectricTestRunner
+import org.robolectric.annotation.Config
 import java.io.File
 
+@RunWith(RobolectricTestRunner::class)
+/*
+ * Run tests in isolation, use basic Application class instead of initializing WireApplication.
+ * It won't work with WireApplication because of Datadog - for each test new WireApplication instance is created but Datadog uses
+ * singleton and initializes itself only once for the first instance of WireApplication which then crashes for other instances.
+ */
+@Config(application = Application::class)
 class FileUtilTest {
 
-    @TempDir
-    lateinit var tempDir: File
+    @get:Rule
+    val tempDir: TemporaryFolder = TemporaryFolder()
 
     @Test
     fun `given file does not exist when finding first unique name in directory then return this name`() {
         val desired = "abc.jpg"
         val expected = "abc.jpg"
 
-        val result = findFirstUniqueName(tempDir, desired)
+        val result = findFirstUniqueName(tempDir.root, desired)
         assertEquals(expected, result)
     }
 
     @Test
     fun `given file already exists when finding unique name in directory then return next available name`() {
-        File(tempDir, "abc.jpg").createNewFile()
+        File(tempDir.root, "abc.jpg").createNewFile()
         val desired = "abc.jpg"
         val expected = "abc (1).jpg"
 
-        val result = findFirstUniqueName(tempDir, desired)
+        val result = findFirstUniqueName(tempDir.root, desired)
         assertEquals(expected, result)
     }
 
     @Test
     fun `given file and its copies already exist when finding unique name in directory then return next available name`() {
-        File(tempDir, "abc.jpg").createNewFile()
-        File(tempDir, "abc (1).jpg").createNewFile()
-        File(tempDir, "abc (2).jpg").createNewFile()
+        File(tempDir.root, "abc.jpg").createNewFile()
+        File(tempDir.root, "abc (1).jpg").createNewFile()
+        File(tempDir.root, "abc (2).jpg").createNewFile()
         val desired = "abc.jpg"
         val expected = "abc (3).jpg"
 
-        val result = findFirstUniqueName(tempDir, desired)
+        val result = findFirstUniqueName(tempDir.root, desired)
         assertEquals(expected, result)
     }
 
@@ -66,5 +93,98 @@ class FileUtilTest {
 
         val result = desired.sanitizeFilename()
         assertEquals(expected, result)
+    }
+
+    @Config(sdk = [Build.VERSION_CODES.Q])
+    @Test
+    fun `given Android 10, when saving file to downloads folder, then use correct order of executions`() = runTest {
+        // given
+        val arrangement = Arrangement()
+            .withUriMimeType("text/plain")
+            .arrange()
+        val downloadedFilePath = tempDir.newFile("filename.txt").toOkioPath()
+        // when
+        saveFileToDownloadsFolder("name", downloadedFilePath, 100L, arrangement.context)
+        // then
+        verifyOrder {
+            arrangement.contentResolver.insert(any(), any())
+            arrangement.contentResolver.copyFile(any(), any())
+        }
+        verify(exactly = 0) {
+            arrangement.downloadManager.addCompletedDownload(any(), any(), any(), any(), any(), any(), any())
+        }
+    }
+
+    @Config(sdk = [Build.VERSION_CODES.P])
+    @Test
+    fun `given Android 9, when saving file to downloads folder, then use correct order of executions`() = runTest {
+        // given
+        val arrangement = Arrangement()
+            .withUriMimeType("text/plain")
+            .arrange()
+        val downloadedFilePath = tempDir.newFile("filename.txt").toOkioPath()
+        // when
+        saveFileToDownloadsFolder("name", downloadedFilePath, 100L, arrangement.context)
+        // then
+        verifyOrder {
+            arrangement.contentResolver.copyFile(any(), any())
+            arrangement.downloadManager.addCompletedDownload(any(), any(), any(), any(), any(), any(), any())
+        }
+        verify(exactly = 0) {
+            arrangement.contentResolver.insert(any(), any())
+        }
+    }
+
+    @Config(sdk = [Build.VERSION_CODES.P])
+    @Test
+    fun `given Android 9 and null mimeType, when saving file to downloads folder, then use mimeType indicating all types`() = runTest {
+        // given
+        val arrangement = Arrangement()
+            .withUriMimeType(null)
+            .arrange()
+        val downloadedFilePath = tempDir.newFile("filename.txt").toOkioPath()
+        // when
+        saveFileToDownloadsFolder("name", downloadedFilePath, 100L, arrangement.context)
+        // then
+        verify {
+            arrangement.downloadManager.addCompletedDownload(any(), any(), any(), eq("*/*"), any(), any(), any())
+        }
+    }
+
+    inner class Arrangement {
+
+        @MockK
+        lateinit var context: Context
+
+        @MockK
+        lateinit var contentResolver: ContentResolver
+
+        @MockK
+        lateinit var downloadManager: DownloadManager
+
+        @MockK
+        lateinit var uri: Uri
+
+        init {
+            MockKAnnotations.init(this, relaxUnitFun = true)
+            mockkStatic(Environment::class)
+            mockkStatic(FileProvider::class)
+            mockkStatic("com.wire.android.util.FileUtilKt")
+            coEvery { context.packageName } returns "com.wire"
+            coEvery { context.contentResolver } returns contentResolver
+            coEvery { context.getSystemService(Context.DOWNLOAD_SERVICE) } returns downloadManager
+            coEvery { Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS) } returns tempDir.root
+            coEvery { FileProvider.getUriForFile(any(), any(), any()) } returns uri
+            coEvery { contentResolver.insert(any(), any()) } returns uri
+            coEvery { downloadManager.addCompletedDownload(any(), any(), any(), any(), any(), any(), any()) } returns 1L
+            coEvery { contentResolver.copyFile(any(), any()) } returns Unit
+            withUriMimeType(null)
+        }
+
+        fun withUriMimeType(mimeType: String?) = apply {
+            coEvery { uri.getMimeType(context) } returns mimeType
+        }
+
+        fun arrange() = this
     }
 }

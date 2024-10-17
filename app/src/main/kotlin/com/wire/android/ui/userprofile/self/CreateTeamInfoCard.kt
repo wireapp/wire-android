45f5e683fbe300aff903a0ba99e991a4bb619be5
Oussama Hassine <oussama.has100@gmail.com>
2024-10-17 18:02:00 +0200

    chore: address comments

--- app/src/main/kotlin/com/wire/android/ui/userprofile/self/CreateTeamInfoCard.kt
+++ app/src/main/kotlin/com/wire/android/ui/userprofile/self/CreateTeamInfoCard.kt
@@ -32,7 +32,6 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.unit.dp
 import com.wire.android.R
 import com.wire.android.ui.common.button.WireSecondaryButton
 import com.wire.android.ui.common.colorsScheme

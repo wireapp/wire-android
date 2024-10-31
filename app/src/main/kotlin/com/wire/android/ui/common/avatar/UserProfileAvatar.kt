99ddc9ed6f2bffb01816a6c01e395ebaafcb1c16
Boris Safonov <safonovboris@gmail.com>
2024-10-31 14:55:44 +0200

    Fixed code-style

--- app/src/main/kotlin/com/wire/android/ui/common/avatar/UserProfileAvatar.kt
+++ app/src/main/kotlin/com/wire/android/ui/common/avatar/UserProfileAvatar.kt
@@ -71,7 +71,6 @@ import com.wire.android.model.Clickable
 import com.wire.android.model.NameBasedAvatar
 import com.wire.android.model.UserAvatarData
 import com.wire.android.ui.common.clickable
-import com.wire.android.ui.common.clickableDescriptions
 import com.wire.android.ui.common.colorsScheme
 import com.wire.android.ui.common.dimensions
 import com.wire.android.ui.home.conversationslist.model.Membership

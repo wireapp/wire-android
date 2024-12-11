e97a24fe0ad5ba53b709f5e36a42f235bf7bcdb0
ohassine <oussama.has100@gmail.com>
2024-12-11 15:27:25 +0100

    chore: revert unit test

--- app/src/test/kotlin/com/wire/android/ui/home/messagecomposer/MessageComposerStateHolderTest.kt
+++ app/src/test/kotlin/com/wire/android/ui/home/messagecomposer/MessageComposerStateHolderTest.kt
@@ -19,12 +19,12 @@
 package com.wire.android.ui.home.messagecomposer
 
 import android.content.Context
+import androidx.compose.foundation.text.input.TextFieldState
+import androidx.compose.foundation.text.input.setTextAndPlaceCursorAtEnd
 import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.State
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.ui.focus.FocusRequester
-import androidx.compose.ui.text.TextRange
-import androidx.compose.ui.text.input.TextFieldValue
 import com.wire.android.config.CoroutineTestExtension
 import com.wire.android.config.SnapshotExtension
 import com.wire.android.framework.TestConversation
@@ -65,7 +65,7 @@ class MessageComposerStateHolderTest {
     private lateinit var messageCompositionHolder: State<MessageCompositionHolder>
     private lateinit var additionalOptionStateHolder: AdditionalOptionStateHolder
     private lateinit var state: MessageComposerStateHolder
-    private lateinit var messageTextFieldValue: MutableState<TextFieldValue>
+    private lateinit var messageTextState: TextFieldState
 
     @BeforeEach
     fun before() {
@@ -74,16 +74,16 @@ class MessageComposerStateHolderTest {
         every { focusRequester.captureFocus() } returns true
         messageComposerViewState = mutableStateOf(MessageComposerViewState())
         messageComposition = mutableStateOf(MessageComposition(TestConversation.ID))
-        messageTextFieldValue = mutableStateOf(TextFieldValue())
+        messageTextState = TextFieldState()
         messageCompositionInputStateHolder = MessageCompositionInputStateHolder(
-            messageTextFieldValue = messageTextFieldValue,
+            messageTextState = messageTextState,
             keyboardController = null,
             focusRequester = focusRequester
         )
         messageCompositionHolder = mutableStateOf(
             MessageCompositionHolder(
                 messageComposition = messageComposition,
-                messageTextFieldValue = messageTextFieldValue,
+                messageTextState = messageTextState,
                 onClearDraft = {},
                 onSaveDraft = {},
                 onSearchMentionQueryChanged = {},
@@ -134,13 +134,9 @@ class MessageComposerStateHolderTest {
             editMessageText = "edit_message_text",
             mentions = listOf()
         )
-
-        state.messageCompositionHolder.value.messageTextFieldValue.value =
-            messageTextFieldValue.value.copy(
-                text = messageTextFieldValue.value.text + "some text",
-                selection = TextRange(messageTextFieldValue.value.text.length + "some text".length)
-            )
-
+        state.messageCompositionHolder.value.messageTextState.edit {
+            append("some text")
+        }
         assertInstanceOf(InputType.Editing::class.java, messageCompositionInputStateHolder.inputType).also {
             assertEquals(true, it.isEditButtonEnabled)
         }
@@ -154,7 +150,7 @@ class MessageComposerStateHolderTest {
             state.toReply(mockMessageWithText)
 
             // then
-            assertEquals(String.EMPTY, messageCompositionHolder.value.messageTextFieldValue.value.text)
+            assertEquals(String.EMPTY, messageCompositionHolder.value.messageTextState.text.toString())
             assertInstanceOf(InputType.Composing::class.java, messageCompositionInputStateHolder.inputType)
         }
 
@@ -162,15 +158,13 @@ class MessageComposerStateHolderTest {
     fun `given some message was being composed, when setting toReply, then input continues with the current text`() = runTest {
         // given
         val currentText = "Potato"
-        messageTextFieldValue.value = messageTextFieldValue.value.copy(
-            text = currentText,
-            selection = TextRange(currentText.length)
-        )
+        messageCompositionHolder.value.messageTextState.setTextAndPlaceCursorAtEnd(currentText)
+
         // when
         state.toReply(mockMessageWithText)
 
         // then
-        assertEquals(currentText, messageCompositionHolder.value.messageTextFieldValue.value.text)
+        assertEquals(currentText, messageCompositionHolder.value.messageTextState.text.toString())
     }
 
     @Test
@@ -197,7 +191,7 @@ class MessageComposerStateHolderTest {
         // then
         assertEquals(
             String.EMPTY,
-            messageCompositionHolder.value.messageTextFieldValue.value.text
+            messageCompositionHolder.value.messageTextState.text.toString()
         )
         assertEquals(
             null,

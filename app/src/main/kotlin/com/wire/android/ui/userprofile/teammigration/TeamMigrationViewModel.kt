945b2f4b2f50383abdf56089cb5834aa2a4b4a87
Oussama Hassine <oussama.has100@gmail.com>
2025-01-03 09:33:57 +0100

    feat: show Guest label for personal users after migration in conversations screen - RC (WPB-14871)  (#3781)

--- app/src/main/kotlin/com/wire/android/ui/userprofile/teammigration/TeamMigrationViewModel.kt
+++ app/src/main/kotlin/com/wire/android/ui/userprofile/teammigration/TeamMigrationViewModel.kt
@@ -105,6 +105,10 @@ class TeamMigrationViewModel @Inject constructor(
         )
     }
 
+    fun setIsMigratingState(isMigrating: Boolean) {
+        teamMigrationState = teamMigrationState.copy(isMigrating = isMigrating)
+    }
+
     fun migrateFromPersonalToTeamAccount(onSuccess: () -> Unit) {
         viewModelScope.launch {
             migrateFromPersonalToTeam.invoke(

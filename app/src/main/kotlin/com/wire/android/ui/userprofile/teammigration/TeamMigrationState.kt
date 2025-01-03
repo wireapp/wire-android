b0ddec1604869ad45cc0f903507a199f732a2f20
Oussama Hassine <oussama.has100@gmail.com>
2025-01-03 08:55:08 +0000

    feat: show Guest label for personal users after migration in conversations screen - RC (WPB-14871)  (#3781)

--- app/src/main/kotlin/com/wire/android/ui/userprofile/teammigration/TeamMigrationState.kt
+++ app/src/main/kotlin/com/wire/android/ui/userprofile/teammigration/TeamMigrationState.kt
@@ -23,6 +23,7 @@ import com.wire.kalium.logic.feature.user.migration.MigrateFromPersonalToTeamFai
 data class TeamMigrationState(
     val teamNameTextState: TextFieldState = TextFieldState(),
     val shouldShowMigrationLeaveDialog: Boolean = false,
+    val isMigrating: Boolean = false,
     val currentStep: Int = 0,
     val migrationFailure: MigrateFromPersonalToTeamFailure? = null
 )

f826e56f37c12dde953350556eee5cc52409e864
ohassine <oussama.has100@gmail.com>
2025-03-17 16:21:16 +0100

    feat: detekt

--- core/ui-common/src/main/kotlin/com/wire/android/ui/theme/GroupAvatarColors.kt
+++ core/ui-common/src/main/kotlin/com/wire/android/ui/theme/GroupAvatarColors.kt
@@ -15,6 +15,9 @@
  * You should have received a copy of the GNU General Public License
  * along with this program. If not, see http://www.gnu.org/licenses/.
  */
+
+@file:Suppress("MatchingDeclarationName")
+
 package com.wire.android.ui.theme
 
 import androidx.compose.ui.graphics.Color

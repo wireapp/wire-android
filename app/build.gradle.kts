1eb47c2851b9c735f918aa655c225024e04a6b24
yamilmedina <yamilmedina@users.noreply.github.com>
2025-03-17 15:43:09 +0100

    fix: broken tests for flavored runs

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -247,6 +247,7 @@ dependencies {
         if (configs["analytics_enabled"] as? Boolean == true) {
             println(">> Adding Anonymous Analytics dependency to [$key] flavor")
             add("${key}Implementation", project(":core:analytics-enabled"))
+            add("test${key.capitalize()}Implementation", project(":core:analytics-disabled"))
         } else {
             add("${key}Implementation", project(":core:analytics-disabled"))
         }

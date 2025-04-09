99b06837b8914116d6a32a443600e27dbcba4133
Michał Saleniuk <30429749+saleniuk@users.noreply.github.com>
2025-04-09 11:50:43 +0200

    fix: disable entering username for enterprise login flow [WPB-17025] (#3975)

--- app/src/main/kotlin/com/wire/android/ui/newauthentication/login/password/NewLoginPasswordScreen.kt
+++ app/src/main/kotlin/com/wire/android/ui/newauthentication/login/password/NewLoginPasswordScreen.kt
@@ -27,6 +27,7 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.foundation.text.input.TextFieldState
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
@@ -36,10 +37,12 @@ import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalSoftwareKeyboardController
 import androidx.compose.ui.platform.testTag
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.semantics.semantics
 import androidx.compose.ui.semantics.testTagsAsResourceId
+import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.text.style.TextDecoration
 import androidx.hilt.navigation.compose.hiltViewModel
@@ -62,10 +65,8 @@ import com.wire.android.ui.authentication.login.email.ForgotPasswordLabel
 import com.wire.android.ui.authentication.login.email.LoginButton
 import com.wire.android.ui.authentication.login.email.LoginEmailState
 import com.wire.android.ui.authentication.login.email.LoginEmailViewModel
-import com.wire.android.ui.authentication.login.email.PasswordInput
 import com.wire.android.ui.authentication.login.email.ProxyIdentifierInput
 import com.wire.android.ui.authentication.login.email.ProxyPasswordInput
-import com.wire.android.ui.authentication.login.email.UserIdentifierInput
 import com.wire.android.ui.authentication.login.isProxyAuthRequired
 import com.wire.android.ui.authentication.login.toLoginDialogErrorData
 import com.wire.android.ui.authentication.welcome.isProxyEnabled
@@ -73,6 +74,12 @@ import com.wire.android.ui.common.colorsScheme
 import com.wire.android.ui.common.dialogs.EmailAlreadyInUseClaimedDomainDialog
 import com.wire.android.ui.common.dimensions
 import com.wire.android.ui.common.preview.EdgeToEdgePreview
+import com.wire.android.ui.common.textfield.DefaultEmailNext
+import com.wire.android.ui.common.textfield.DefaultPassword
+import com.wire.android.ui.common.textfield.WireAutoFillType
+import com.wire.android.ui.common.textfield.WirePasswordTextField
+import com.wire.android.ui.common.textfield.WireTextField
+import com.wire.android.ui.common.textfield.WireTextFieldState
 import com.wire.android.ui.common.textfield.clearAutofillTree
 import com.wire.android.ui.common.typography
 import com.wire.android.ui.common.visbility.rememberVisibilityState
@@ -177,13 +184,13 @@ internal fun LoginPasswordContent(
                     testTagsAsResourceId = true
                 }
             ) {
-                UserIdentifierInput(
+                EmailInput(
                     modifier = Modifier
                         .fillMaxWidth()
                         .padding(bottom = dimensions().spacing8x),
                     userIdentifierState = userIdentifierTextState,
                     error = when (loginEmailState.flowState) {
-                        is LoginState.Error.TextFieldError.InvalidValue -> stringResource(R.string.login_error_invalid_user_identifier)
+                        is LoginState.Error.TextFieldError.InvalidValue -> stringResource(R.string.login_error_invalid_email)
                         else -> null
                     },
                     isEnabled = loginEmailState.userIdentifierEnabled,
@@ -241,6 +248,44 @@ internal fun LoginPasswordContent(
             }
         }
     }
+}
+
+@Composable
+fun EmailInput(
+    userIdentifierState: TextFieldState,
+    error: String?,
+    isEnabled: Boolean,
+    modifier: Modifier = Modifier,
+) {
+    WireTextField(
+        autoFillType = WireAutoFillType.Login,
+        textState = userIdentifierState,
+        placeholderText = stringResource(R.string.login_email_placeholder),
+        labelText = stringResource(R.string.login_email_label),
+        state = when {
+            !isEnabled -> WireTextFieldState.Disabled
+            error != null -> WireTextFieldState.Error(error)
+            else -> WireTextFieldState.Default
+        },
+        semanticDescription = stringResource(R.string.content_description_login_email_field),
+        keyboardOptions = KeyboardOptions.DefaultEmailNext,
+        modifier = modifier.testTag("emailField"),
+        testTag = "userIdentifierInput"
+    )
+}
+
+@Composable
+fun PasswordInput(passwordState: TextFieldState, modifier: Modifier = Modifier) {
+    val keyboardController = LocalSoftwareKeyboardController.current
+    WirePasswordTextField(
+        textState = passwordState,
+        keyboardOptions = KeyboardOptions.DefaultPassword.copy(imeAction = ImeAction.Done),
+        onKeyboardAction = { keyboardController?.hide() },
+        semanticDescription = stringResource(R.string.content_description_login_password_field),
+        modifier = modifier.testTag("passwordField"),
+        autoFill = true,
+        testTag = "PasswordInput"
+    )
 }
 
 @Composable

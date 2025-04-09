e2de85c42199b7f13674e8a47ad1aa15c43d9015
yamilmedina <yamilmedina@users.noreply.github.com>
2025-04-09 11:59:44 +0200

    fix: make sure coroutine is init before using it

--- core/analytics/src/main/kotlin/com/wire/android/feature/analytics/AnonymousAnalyticsManagerStub.kt
+++ core/analytics/src/main/kotlin/com/wire/android/feature/analytics/AnonymousAnalyticsManagerStub.kt
@@ -24,7 +24,6 @@ import com.wire.android.feature.analytics.handler.AnalyticsPropagationHandler
 import com.wire.android.feature.analytics.model.AnalyticsEvent
 import com.wire.android.feature.analytics.model.AnalyticsResult
 import com.wire.android.feature.analytics.model.AnalyticsSettings
-import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.flow.Flow
 
 open class AnonymousAnalyticsManagerStub : AnonymousAnalyticsManager {

472ebf619b148726d3e3004e4a83baf3c81c612c
Mohamad Jaara <mohamad.jaara@wire.com>
2025-06-17 10:31:30 +0200

    feat: update compile SDK version to 35 and dependencies for compatibility

--- core/ui-common/src/main/kotlin/com/wire/android/ui/common/preview/EdgeToEdgePreview.kt
+++ core/ui-common/src/main/kotlin/com/wire/android/ui/common/preview/EdgeToEdgePreview.kt
@@ -17,13 +17,10 @@
  */
 package com.wire.android.ui.common.preview
 
-import android.content.res.Configuration.UI_MODE_NIGHT_NO
-import android.content.res.Configuration.UI_MODE_NIGHT_YES
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.CompositionLocalProvider
-import androidx.compose.ui.platform.LocalConfiguration
 import de.drick.compose.edgetoedgepreviewlib.CameraCutoutMode
 import de.drick.compose.edgetoedgepreviewlib.EdgeToEdgeTemplate
+import de.drick.compose.edgetoedgepreviewlib.InsetMode
 import de.drick.compose.edgetoedgepreviewlib.NavigationMode
 
 @Composable
@@ -31,21 +28,15 @@ fun EdgeToEdgePreview(
     useDarkIcons: Boolean,
     content: @Composable () -> Unit,
 ) {
-    // isAppearanceLightStatusBars cannot be set for previews, so we need to apply it manually
-    // TODO: remove LocalConfiguration and just pass `isDarkMode` into EdgeToEdgeTemplate after updating edgetoedgepreviewlib to 0.6.0
-    CompositionLocalProvider(
-        LocalConfiguration provides LocalConfiguration.current.apply {
-            uiMode = if (useDarkIcons) UI_MODE_NIGHT_NO else UI_MODE_NIGHT_YES
-        }
-    ) {
-        EdgeToEdgeTemplate(
-            navMode = NavigationMode.Gesture,
-            cameraCutoutMode = CameraCutoutMode.Middle,
-            isInvertedOrientation = false,
-            showInsetsBorder = false,
-            isNavigationBarVisible = true,
-            isStatusBarVisible = true,
-            content = content,
-        )
-    }
+    EdgeToEdgeTemplate(
+        navMode = NavigationMode.Gesture,
+        cameraCutoutMode = CameraCutoutMode.Middle,
+        isInvertedOrientation = false,
+        showInsetsBorder = false,
+        content = content,
+        isDarkMode = useDarkIcons,
+        navigationBarMode = InsetMode.Visible,
+        statusBarMode = InsetMode.Visible,
+        isNavigationBarContrastEnforced = true,
+    )
 }

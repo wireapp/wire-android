c42db1108dcb8d9824a7afacc73be123b44ab875
ohassine <oussama.has100@gmail.com>
2025-07-04 09:47:16 +0100

    chore: detekt

--- features/cells/src/main/java/com/wire/android/feature/cells/ui/CellListItem.kt
+++ features/cells/src/main/java/com/wire/android/feature/cells/ui/CellListItem.kt
@@ -38,10 +38,6 @@ import androidx.compose.material.icons.filled.MoreVert
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip

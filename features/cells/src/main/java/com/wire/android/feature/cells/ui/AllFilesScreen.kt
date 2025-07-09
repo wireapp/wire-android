b007ae02bbb2f91ec7bae163d959efee9b5f9463
ohassine <oussama.has100@gmail.com>
2025-07-09 13:17:22 +0100

    feat: show folder in global view

--- features/cells/src/main/java/com/wire/android/feature/cells/ui/AllFilesScreen.kt
+++ features/cells/src/main/java/com/wire/android/feature/cells/ui/AllFilesScreen.kt
@@ -21,6 +21,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.paging.compose.collectAsLazyPagingItems
+import com.wire.android.feature.cells.ui.destinations.ConversationFilesScreenDestination.invoke
 import com.wire.android.feature.cells.ui.destinations.PublicLinkScreenDestination
 import com.wire.android.navigation.NavigationCommand
 import com.wire.android.navigation.WireNavigator
@@ -55,13 +56,12 @@ fun AllFilesScreen(
     }
 
     searchBarState.searchVisibleChanged(isSearchVisible)
-
     CellScreenContent(
         actionsFlow = viewModel.actions,
         pagingListItems = pagingListItems,
         sendIntent = { viewModel.sendIntent(it) },
         onFolderClick = {
-            // TODO: Handle folder click later
+            navigator.navigate(NavigationCommand(com.wire.android.feature.cells.ui.destinations.ConversationFilesScreenDestination(it.remotePath)))
         },
         downloadFileState = viewModel.downloadFileSheet,
         menuState = viewModel.menu,

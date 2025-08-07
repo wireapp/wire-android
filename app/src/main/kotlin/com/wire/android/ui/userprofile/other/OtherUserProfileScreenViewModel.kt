12b36680677abac3d749bb591dfddb31ff780172
Michał Saleniuk <saleniuk@gmail.com>
2025-08-07 18:25:11 +0200

    add snackbar message after removing

--- app/src/main/kotlin/com/wire/android/ui/userprofile/other/OtherUserProfileScreenViewModel.kt
+++ app/src/main/kotlin/com/wire/android/ui/userprofile/other/OtherUserProfileScreenViewModel.kt
@@ -37,6 +37,7 @@ import com.wire.android.ui.userprofile.common.UsernameMapper.fromOtherUser
 import com.wire.android.ui.userprofile.group.RemoveConversationMemberState
 import com.wire.android.ui.userprofile.other.OtherUserProfileInfoMessageType.ChangeGroupRoleError
 import com.wire.android.ui.userprofile.other.OtherUserProfileInfoMessageType.RemoveConversationMemberError
+import com.wire.android.ui.userprofile.other.OtherUserProfileInfoMessageType.RemoveConversationMemberSuccess
 import com.wire.android.util.dispatchers.DispatcherProvider
 import com.wire.kalium.common.functional.getOrNull
 import com.wire.kalium.logic.data.conversation.Conversation
@@ -215,9 +216,9 @@ class OtherUserProfileScreenViewModel @Inject constructor(
                     userId
                 )
             }
-
-            if (response is RemoveMemberFromConversationUseCase.Result.Failure) {
-                onMessage(RemoveConversationMemberError)
+            when (response) {
+                is RemoveMemberFromConversationUseCase.Result.Failure -> onMessage(RemoveConversationMemberError)
+                is RemoveMemberFromConversationUseCase.Result.Success -> onMessage(RemoveConversationMemberSuccess(state.userName))
             }
             removeConversationMemberDialogState.dismiss()
         }

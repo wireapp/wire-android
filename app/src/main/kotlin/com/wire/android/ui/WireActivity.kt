863ce8a7c12401d0240a18ad005c18bdb869df8a
Michał Saleniuk <30429749+saleniuk@users.noreply.github.com>
2025-08-18 10:49:10 +0200

    fix: accept call from the notification does not start the call [WPB-17676] (#4188)
    Co-authored-by: Yamil Medina <yamilmedina@users.noreply.github.com>

--- app/src/main/kotlin/com/wire/android/ui/WireActivity.kt
+++ app/src/main/kotlin/com/wire/android/ui/WireActivity.kt
@@ -309,7 +309,7 @@ class WireActivity : AppCompatActivity() {
                 getIncomingCallIntent(
                     this@WireActivity,
                     it.conversationId.toString(),
-                    null
+                    it.userId.toString(),
                 ).run {
                     startActivity(this)
                 }

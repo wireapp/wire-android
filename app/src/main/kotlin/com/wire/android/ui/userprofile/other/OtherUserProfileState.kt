850da731cb7cc84afe8482a733ea8b52d2552834
Michał Saleniuk <30429749+saleniuk@users.noreply.github.com>
2025-08-21 15:05:00 +0200

    fix: search and media access missing on other users profile [WPB-19073] (#4205)

--- app/src/main/kotlin/com/wire/android/ui/userprofile/other/OtherUserProfileState.kt
+++ app/src/main/kotlin/com/wire/android/ui/userprofile/other/OtherUserProfileState.kt
@@ -33,7 +33,7 @@ import kotlinx.serialization.Serializable
 
 data class OtherUserProfileState(
     val userId: UserId,
-    val conversationId: ConversationId? = null,
+    val groupConversationId: ConversationId? = null,
     val activeOneOnOneConversationId: ConversationId? = null,
     val userAvatarAsset: UserAvatarAsset? = null,
     val isDataLoading: Boolean = false,

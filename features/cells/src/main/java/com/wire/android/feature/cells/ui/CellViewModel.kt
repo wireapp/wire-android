5c3ae3f1720f66d4ad0dc6c9984f99a835682922
Sergey Bakhtiarov <sbakhtiarov@gmail.com>
2025-09-23 10:02:12 +0200

    chore: show different delete message for files and folders (WPB-20489) (#4292)

--- features/cells/src/main/java/com/wire/android/feature/cells/ui/CellViewModel.kt
+++ features/cells/src/main/java/com/wire/android/feature/cells/ui/CellViewModel.kt
@@ -453,7 +453,12 @@ class CellViewModel @Inject constructor(
             .onSuccess {
                 _isDeleteInProgress.value = false
                 sendAction(HideDeleteConfirmation)
-                sendAction(ShowFileDeletedMessage(isRecycleBin()))
+                sendAction(
+                    ShowFileDeletedMessage(
+                        isFile = node is CellNodeUi.File,
+                        permanently = isRecycleBin()
+                    )
+                )
                 refreshNodes()
                 // Wait until refresh completes
                 pagingRefreshDone.first()
@@ -564,7 +569,7 @@ internal data class ShowMoveToFolderScreen(val currentPath: String, val nodeToMo
 internal data object ShowUnableToRestoreDialog : CellViewAction
 internal data class ShowRestoreParentFolderDialog(val cellNode: CellNodeUi) : CellViewAction
 internal data object HideRestoreParentFolderDialog : CellViewAction
-internal data class ShowFileDeletedMessage(val permanently: Boolean) : CellViewAction
+internal data class ShowFileDeletedMessage(val isFile: Boolean, val permanently: Boolean) : CellViewAction
 internal data object RefreshData : CellViewAction
 
 internal enum class CellError(val message: Int) {

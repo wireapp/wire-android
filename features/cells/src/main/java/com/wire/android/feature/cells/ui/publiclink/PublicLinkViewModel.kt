810581f081234a13b59ceaa711d592c109d9da93
sergei.bakhtiarov <sbakhtiarov@gmail.com>
2025-11-18 09:36:09 +0100

    feat: secure public link ui update (WPB-20914)

--- features/cells/src/main/java/com/wire/android/feature/cells/ui/publiclink/PublicLinkViewModel.kt
+++ features/cells/src/main/java/com/wire/android/feature/cells/ui/publiclink/PublicLinkViewModel.kt
@@ -27,6 +27,7 @@ import com.wire.kalium.cells.domain.model.PublicLink
 import com.wire.kalium.cells.domain.usecase.publiclink.CreatePublicLinkUseCase
 import com.wire.kalium.cells.domain.usecase.publiclink.DeletePublicLinkUseCase
 import com.wire.kalium.cells.domain.usecase.publiclink.GetPublicLinkUseCase
+import com.wire.kalium.cells.domain.usecase.publiclink.SECURE_PUBLIC_LINK_ENABLED
 import com.wire.kalium.common.functional.onFailure
 import com.wire.kalium.common.functional.onSuccess
 import dagger.hilt.android.lifecycle.HiltViewModel
@@ -47,31 +48,29 @@ class PublicLinkViewModel @Inject constructor(
 
     private val navArgs: PublicLinkNavArgs = savedStateHandle.navArgs()
 
-    private val _state = MutableStateFlow(PublicLinkViewState())
+    private val _state = MutableStateFlow(
+        PublicLinkViewState(
+            isEnabled = navArgs.publicLinkId != null,
+            isFolder = navArgs.isFolder,
+            isLinkAvailable = navArgs.publicLinkId != null,
+        )
+    )
     internal val state = _state.asStateFlow()
     private var publicLink: PublicLink? = null
 
     init {
 
-        navArgs.publicLinkId?.let { linkUuid ->
-            _state.update {
-                it.copy(enabled = true)
-            }
+        if (SECURE_PUBLIC_LINK_ENABLED) {
+            // Enable settings for UI testing
+            _state.update { it.copy(settings = PublicLinkSettings()) }
+        }
 
+        navArgs.publicLinkId?.let { linkUuid ->
             loadPublicLink(linkUuid)
         }
     }
 
-    fun isFolder(): Boolean = navArgs.isFolder
-
     fun onEnabled(enabled: Boolean) {
-        _state.update {
-            it.copy(
-                enabled = enabled,
-                url = publicLink?.url
-            )
-        }
-
         if (enabled) {
             if (publicLink == null) {
                 createPublicLink()
@@ -84,14 +83,22 @@ class PublicLinkViewModel @Inject constructor(
     }
 
     private fun createPublicLink() = viewModelScope.launch {
+
+        _state.update { it.copy(isEnabled = true) }
+
         createPublicLink(navArgs.assetId, navArgs.fileName)
             .onSuccess { link ->
                 publicLink = link
-                _state.update { it.copy(url = link.url) }
+                _state.update {
+                    it.copy(
+                        isLinkAvailable = true,
+                        linkState = PublicLinkState.READY
+                    )
+                }
             }
             .onFailure {
                 sendAction(ShowError(R.string.error_create_public_link))
-                _state.update { it.copy(enabled = false) }
+                _state.update { it.copy(isEnabled = false) }
             }
     }
 
@@ -99,44 +106,83 @@ class PublicLinkViewModel @Inject constructor(
         getPublicLinkUseCase(linkUuid)
             .onSuccess { link ->
                 publicLink = link
-                _state.update { it.copy(url = link.url) }
+                _state.update { it.copy(linkState = PublicLinkState.READY) }
             }
             .onFailure {
                 sendAction(ShowError(R.string.error_load_public_link, true))
-                _state.update { it.copy(enabled = false) }
+                _state.update { it.copy(linkState = PublicLinkState.ERROR) }
             }
     }
 
     private fun deletePublicLink(linkUuid: String) = viewModelScope.launch {
+        _state.update { it.copy(isEnabled = false) }
         deletePublicLinkUseCase(linkUuid)
             .onSuccess {
                 publicLink = null
                 _state.update {
                     it.copy(
-                        url = null
+                        isLinkAvailable = false
                     )
                 }
             }
             .onFailure {
                 sendAction(ShowError(R.string.error_delete_public_link))
-                _state.update { it.copy(enabled = true) }
+                _state.update { it.copy(isEnabled = true) }
             }
     }
 
-    fun shareLink(url: String) {
-        fileHelper.shareUrlChooser(
-            url = url,
-            onError = {
-                sendAction(ShowError(R.string.error_share_link))
-            }
-        )
+    fun shareLink() {
+        publicLink?.url?.let { url ->
+            fileHelper.shareUrlChooser(
+                url = url,
+                onError = {
+                    sendAction(ShowError(R.string.error_share_link))
+                }
+            )
+        }
+    }
+
+    fun copyLink() {
+        publicLink?.url?.let { url ->
+            sendAction(CopyLink(url))
+        }
+    }
+
+    fun onPasswordUpdate() {
+        // TODO: Update state
+    }
+
+    fun onExpirationUpdate() {
+        // TODO: Update state
     }
 }
 
 internal data class PublicLinkViewState(
-    val enabled: Boolean = false,
-    val url: String? = null,
+    val isEnabled: Boolean = false,
+    val isLinkAvailable: Boolean = false,
+    val linkState: PublicLinkState = PublicLinkState.LOADING,
+    val isFolder: Boolean = false,
+    val isExpired: Boolean = false,
+    val settings: PublicLinkSettings? = null,
+)
+
+internal enum class PublicLinkState {
+    READY, LOADING, ERROR
+}
+
+internal data class PublicLinkSettings(
+    val passwordSettings: PublicLinkPassword? = null,
+    val expirationSettings: PublicLinkExpiration? = null,
+)
+
+internal data class PublicLinkPassword(
+    val password: String? = null,
+)
+
+internal data class PublicLinkExpiration(
+    val expiresAt: Long? = null,
 )
 
 sealed interface PublicLinkViewAction
 internal data class ShowError(val message: Int, val closeScreen: Boolean = false) : PublicLinkViewAction
+internal data class CopyLink(val url: String) : PublicLinkViewAction

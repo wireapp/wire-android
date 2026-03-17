485318ca8abbb71c4d9246f577019c6bdc5f14b4
Mohamad Jaara <9083456+MohamadJaara@users.noreply.github.com>
2026-03-17 11:22:29 +0100

    detekt

--- app/src/test/kotlin/com/wire/android/ui/WireActivityViewModelTest.kt
+++ app/src/test/kotlin/com/wire/android/ui/WireActivityViewModelTest.kt
@@ -112,7 +112,6 @@ import kotlinx.coroutines.test.runTest
 import org.junit.jupiter.api.Assertions.assertFalse
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Assertions.assertInstanceOf
-import org.junit.jupiter.api.Assertions.assertFalse
 import org.junit.jupiter.api.Assertions.assertTrue
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.extension.ExtendWith

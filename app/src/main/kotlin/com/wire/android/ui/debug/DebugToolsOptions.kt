f299fc77fcf10f2b4c6bb44d8a36922d31448f26
Jakub Zerko <iot.zerko@gmail.com>
2026-04-03 10:48:24 +0200

    detekt fix

--- app/src/main/kotlin/com/wire/android/ui/debug/DebugToolsOptions.kt
+++ app/src/main/kotlin/com/wire/android/ui/debug/DebugToolsOptions.kt
@@ -25,7 +25,6 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import com.wire.android.BuildConfig
@@ -39,7 +38,6 @@ import com.wire.android.ui.theme.WireTheme
 import com.wire.android.ui.theme.wireColorScheme
 import com.wire.android.ui.theme.wireDimensions
 import com.wire.android.ui.theme.wireTypography
-import com.wire.android.util.extension.isGoogleServicesAvailable
 
 @Preview
 @Composable

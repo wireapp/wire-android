b1d183c6301f60b1742657c3d9357492b262853f
Jakub Zerko <iot.zerko@gmail.com>
2026-05-04 12:07:12 +0200

    typo fix

--- app/src/androidTest/kotlin/com/wire/android/ui/debug/DebugScreenComposeTest.kt
+++ app/src/androidTest/kotlin/com/wire/android/ui/debug/DebugScreenComposeTest.kt
@@ -43,7 +43,6 @@ class DebugScreenComposeTest {
                         onShowFeatureFlags = {},
                         onShowCryptoStats = {},
                         onFlushLogs = { CompletableDeferred(Unit) },
-                        onShowCryptoStats = {}
                     )
             }
         }

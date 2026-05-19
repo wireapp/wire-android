6a3dd0749ee89e4397bf2006748c1de7fa93c6e1
Yamil Medina <yamilmedina@users.noreply.github.com>
2026-05-19 15:51:36 +0200

    chore(benchmark): convert benchmark module to test producer (WPB-24729) (#4821)

---
+++ benchmark/src/main/java/com/wire/benchmark/BaselineGenerator.kt
@@ -0,0 +1,47 @@
+/*
+ * Wire
+ * Copyright (C) 2024 Wire Swiss GmbH
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see http://www.gnu.org/licenses/.
+ */
+package com.wire.benchmark
+
+import androidx.benchmark.macro.junit4.BaselineProfileRule
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.platform.app.InstrumentationRegistry
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@RunWith(AndroidJUnit4::class)
+class BaselineGenerator {
+
+    @get:Rule
+    val baselineProfileRule = BaselineProfileRule()
+
+    private val args get() = InstrumentationRegistry.getArguments()
+    private val targetPackage get() = args.getString("TARGET_PACKAGE", "com.wire")
+    private val email get() = args.getString("EMAIL").orEmpty()
+    private val password get() = args.getString("PASSWORD").orEmpty()
+
+    @Test
+    fun startup() = baselineProfileRule.collect(
+        packageName = targetPackage,
+        includeInStartupProfile = true,
+    ) {
+        pressHome()
+        startActivityAndWait()
+        if (email.isNotEmpty() && password.isNotEmpty()) login(email, password)
+    }
+}
---
+++ benchmark/src/main/java/com/wire/benchmark/LoginExt.kt
@@ -0,0 +1,44 @@
+/*
+ * Wire
+ * Copyright (C) 2026 Wire Swiss GmbH
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see http://www.gnu.org/licenses/.
+ */
+package com.wire.benchmark
+
+import androidx.benchmark.macro.MacrobenchmarkScope
+import androidx.test.uiautomator.By
+import androidx.test.uiautomator.Until
+import kotlin.time.Duration.Companion.seconds
+
+/**
+ * Login to the app using the provided email and password. This function assumes that the app is on the login screen.
+ */
+fun MacrobenchmarkScope.login(email: String, password: String) {
+    device.findObject(By.res("userIdentifierInput"))?.text = email
+    device.findObject(By.res("loginButton"))?.click()
+    device.findObject(By.res("PasswordInput"))?.text = password
+    device.findObject(By.res("LoginNextButton"))?.click()
+    waitForAnalyticsIfPresentAndAgree()
+    device.wait(Until.hasObject(By.text("Conversations")), 30.seconds.inWholeMilliseconds)
+}
+
+/**
+ * Wait for the analytics dialog to appear and click "Agree" if it is present.
+ * This function assumes that the app is on the screen where the analytics dialog may appear.
+ */
+fun MacrobenchmarkScope.waitForAnalyticsIfPresentAndAgree() {
+    device.wait(Until.hasObject(By.text("Agree")), 10.seconds.inWholeMilliseconds)
+    device.findObject(By.text("Agree"))?.click()
+}

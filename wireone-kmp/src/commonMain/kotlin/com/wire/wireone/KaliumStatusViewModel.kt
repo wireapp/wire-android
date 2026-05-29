38b995bd3368f18ffdbe71ba0d31011faa7b8481
Jakub Żerko <iot.zerko@gmail.com>
2026-05-29 13:29:54 +0200

    feat: wireone ios session persistence UI backport [WPB-24959] (#4876)

--- wireone-kmp/src/commonMain/kotlin/com/wire/wireone/KaliumStatusViewModel.kt
+++
@@ -1,7 +0,0 @@
-package com.wire.wireone
-
-internal class KaliumStatusViewModel(
-    private val kaliumProvider: KaliumProvider = createKaliumProvider()
-) {
-    fun statusLine(): String = kaliumProvider.statusLine()
-}
---
+++ wireone-kmp/src/commonMain/kotlin/com/wire/wireone/MicrophonePermission.kt
@@ -0,0 +1,3 @@
+package com.wire.wireone
+
+internal expect suspend fun requestMicrophonePermissionIfNeeded(): Boolean

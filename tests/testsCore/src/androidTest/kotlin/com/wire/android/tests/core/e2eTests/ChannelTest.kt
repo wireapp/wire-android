ad798218f0a2d9ac4506a405640f53df7f1d78ab
emmaoke-w <emmanuel.oke@wire.com>
2026-06-09 10:44:24 +0200

    for testing

--- tests/testsCore/src/androidTest/kotlin/com/wire/android/tests/core/e2eTests/ChannelTest.kt
+++ tests/testsCore/src/androidTest/kotlin/com/wire/android/tests/core/e2eTests/ChannelTest.kt
@@ -166,7 +166,7 @@ class ChannelTest : BaseUiTest() {
 
         step("When I tap on conversation name TestChannel in conversation list") {
             pages.conversationListPage.apply {
-                clickChannelConversation("TestChannel")
+                clickChannelConversation("TestChannelPPP")
             }
         }
 
@@ -405,7 +405,7 @@ class ChannelTest : BaseUiTest() {
 
         step("When I tap on conversation name LeavingChannel in conversation list") {
             pages.conversationListPage.apply {
-                clickChannelConversation("LeavingChannel")
+                clickChannelConversation("LeavingChannelPPP")
             }
         }
 

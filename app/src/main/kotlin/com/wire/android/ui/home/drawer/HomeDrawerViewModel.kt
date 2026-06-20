6c8b358cad1a584f89a1b4ddf02ed1c97c477ff6
Mohamad Jaara <9083456+MohamadJaara@users.noreply.github.com>
2026-06-20 13:52:55 +0200

    feat: adjust padding in message bubble and remove threads from drawer

--- app/src/main/kotlin/com/wire/android/ui/home/drawer/HomeDrawerViewModel.kt
+++ app/src/main/kotlin/com/wire/android/ui/home/drawer/HomeDrawerViewModel.kt
@@ -79,7 +79,6 @@ class HomeDrawerViewModel(
 
                 buildList {
                     add(DrawerUiItem.RegularItem(destination = HomeDestination.Conversations))
-                    add(DrawerUiItem.RegularItem(destination = HomeDestination.Threads))
                     if (shouldShowCells) {
                         add(DrawerUiItem.RegularItem(destination = HomeDestination.Cells))
                     }

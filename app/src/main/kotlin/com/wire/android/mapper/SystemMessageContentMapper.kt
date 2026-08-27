ee45323c348f37ba4d13feafc7b4566a51ccbb9e
ohassine <oussama.has100@gmail.com>
2026-08-27 12:35:18 +0200

    chore: compose stability

--- app/src/main/kotlin/com/wire/android/mapper/SystemMessageContentMapper.kt
+++ app/src/main/kotlin/com/wire/android/mapper/SystemMessageContentMapper.kt
@@ -76,9 +76,6 @@ class SystemMessageContentMapper @Inject constructor(
         is MessageContent.NewConversationWithCellSelfDeleteDisabledMessage ->
             UIMessageContent.SystemMessage.NewConversationWithCellSelfDeleteDisabled
 
-        is MessageContent.CellEditorAccessMessage -> UIMessageContent.SystemMessage.CellsConversationEditorAccess
-        is MessageContent.CellViewerAccessMessage -> UIMessageContent.SystemMessage.CellsConversationViewerAccess
-
         is MessageContent.ConversationAppsEnabledChanged -> mapConversationConversationAppsAccessChanged(
             message.senderUserId,
             content,

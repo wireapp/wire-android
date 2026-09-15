c58904783c9244d396df648766b974e0a94bc1de
ohassine <oussama.has100@gmail.com>
2026-09-15 18:13:29 +0200

    chore: detekt

--- features/cells/src/main/java/com/wire/android/feature/cells/ui/OfflineListing.kt
+++ features/cells/src/main/java/com/wire/android/feature/cells/ui/OfflineListing.kt
@@ -15,6 +15,8 @@
  * You should have received a copy of the GNU General Public License
  * along with this program. If not, see http://www.gnu.org/licenses/.
  */
+@file:Suppress("MatchingDeclarationName")
+
 package com.wire.android.feature.cells.ui
 
 import com.wire.kalium.cells.domain.usecase.offline.OfflineFileInfo
--- features/cells/src/test/kotlin/com/wire/android/feature/cells/ui/CellViewModelTest.kt
+++ features/cells/src/test/kotlin/com/wire/android/feature/cells/ui/CellViewModelTest.kt
@@ -59,7 +59,6 @@ import io.mockk.coEvery
 import io.mockk.coVerify
 import io.mockk.every
 import io.mockk.impl.annotations.MockK
-import io.mockk.mockk
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.MutableStateFlow
